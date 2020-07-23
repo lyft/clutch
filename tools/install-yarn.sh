7bcfeb170f1811cfbbad643c95c690ae8ba16f80
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-07-23 10:44:16 -0500

    better var name

--- tools/install-yarn.sh
+++ tools/install-yarn.sh
@@ -4,7 +4,7 @@ YARN_VERSION="1.22.4"
 ROOT_DEST_DIR="$PWD/frontend"
 DEST_DIR="$ROOT_DEST_DIR/.yarn/releases"
 DEST_FILE="${DEST_DIR}/yarn-${YARN_VERSION}.js"
-WRAPPER_DEST="${PWD}/build/bin/yarn.sh"
+WRAPPER_DEST_FILE="${PWD}/build/bin/yarn.sh"
 
 if [[ ! -d "${ROOT_DEST_DIR}" ]]; then
   echo "Could not find frontend directory. Ensure you're running this script from the root of your project."
@@ -20,7 +20,7 @@ fi
 
 # Install a wrapper script in build/ that executes yarn if it doesn't exist already.
 WRAPPER_SCRIPT="#!/bin/bash\nnode \"${DEST_FILE}\" \"\$@\"\n"
-if [[ ! -f "${WRAPPER_DEST}" || $(< "${WRAPPER_DEST}") != $(printf "%b" "${WRAPPER_SCRIPT}") ]]; then
-  printf "%b" "${WRAPPER_SCRIPT}" > "${WRAPPER_DEST}"
-  chmod +x "${WRAPPER_DEST}"
+if [[ ! -f "${WRAPPER_DEST_FILE}" || $(< "${WRAPPER_DEST_FILE}") != $(printf "%b" "${WRAPPER_SCRIPT}") ]]; then
+  printf "%b" "${WRAPPER_SCRIPT}" > "${WRAPPER_DEST_FILE}"
+  chmod +x "${WRAPPER_DEST_FILE}"
 fi
