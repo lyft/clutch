4bd6e1f56f18721bb1054c07aac796ca24e77157
Derek Schaller <d_a_schaller@yahoo.com>
2020-07-24 10:48:29 -0700

    one last shellcheck fix

--- tools/publish-frontend.sh
+++ tools/publish-frontend.sh
@@ -3,7 +3,7 @@
 VERSION="0.0.0-$(git log -1 --format=%cd --date=format:'%Y%m%d%H%M%S')-$(git rev-parse --short=12 HEAD)"
 PACKAGE=$1
 
-if [[ $(yarn info "@clutch-sh/${PACKAGE}" | grep -q "$VERSION") ]]; then
+if yarn info "@clutch-sh/${PACKAGE}" | grep -q "$VERSION"; then
   echo "Found existing version of ${PACKAGE}@${VERSION}"
   exit 0
 fi
