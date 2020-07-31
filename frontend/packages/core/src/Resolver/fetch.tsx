797faf964caf992a002dfed710ed2cf8c90e95ca
Derek Schaller <d_a_schaller@yahoo.com>
2020-07-31 16:54:36 -0700

    fix lint

--- frontend/packages/core/src/Resolver/fetch.tsx
+++ frontend/packages/core/src/Resolver/fetch.tsx
@@ -45,7 +45,7 @@ const resolveFields = async (
     have: fields,
     limit,
   });
-  return { results: response.data?.results || [], failures: response.data?.partialFailures || []};
+  return { results: response.data?.results || [], failures: response.data?.partialFailures || [] };
 };
 
 const resolveResource = async (
