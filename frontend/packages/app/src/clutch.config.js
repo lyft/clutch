324e2b59913de3bba7acdc95fe5cf927a8e281c3
Mike Cutalo <mcutalo88@gmail.com>
2020-08-04 16:32:12 -0700

    notes for terminate instance

--- frontend/packages/app/src/clutch.config.js
+++ frontend/packages/app/src/clutch.config.js
@@ -4,6 +4,10 @@ module.exports = {
       trending: true,
       componentProps: {
         resolverType: "clutch.aws.ec2.v1.Instance",
+        // notes: [{
+        //   severity: "info",
+        //   text: "This is a test note for terminate instance.",
+        // }, ],
       },
     },
     resizeAutoscalingGroup: {
