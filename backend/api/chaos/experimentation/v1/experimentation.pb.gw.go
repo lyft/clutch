ddc618bdc3b3c1750c5baaecd7fefb7bf6454c6f
Bill Gallagher <bgallagher@lyft.com>
2020-09-10 13:40:21 -0400

    regen protos

--- backend/api/chaos/experimentation/v1/experimentation.pb.gw.go
+++ backend/api/chaos/experimentation/v1/experimentation.pb.gw.go
@@ -186,12 +186,8 @@ func RegisterExperimentsAPIHandlerServer(ctx context.Context, mux *runtime.Serve
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-<<<<<<< HEAD
-		resp, md, err := local_request_ExperimentsAPI_CreateExperiments_0(rctx, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-=======
 		resp, md, err := local_request_ExperimentsAPI_CreateExperiment_0(rctx, inboundMarshaler, server, req, pathParams)
->>>>>>> add start/end time
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
