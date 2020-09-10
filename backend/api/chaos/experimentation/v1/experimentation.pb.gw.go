b698cb22af6a1d32c1c03426210ec259362a3404
Rafal Augustyniak <raugustyniak@lyft.com>
2020-09-10 08:32:23 -0700

    run make api

--- backend/api/chaos/experimentation/v1/experimentation.pb.gw.go
+++ backend/api/chaos/experimentation/v1/experimentation.pb.gw.go
@@ -224,6 +224,8 @@ func RegisterExperimentsAPIHandlerServer(ctx context.Context, mux *runtime.Serve
 	mux.Handle("POST", pattern_ExperimentsAPI_GetExperimentRunDetails_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
 		if err != nil {
@@ -231,6 +233,7 @@ func RegisterExperimentsAPIHandlerServer(ctx context.Context, mux *runtime.Serve
 			return
 		}
 		resp, md, err := local_request_ExperimentsAPI_GetExperimentRunDetails_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
