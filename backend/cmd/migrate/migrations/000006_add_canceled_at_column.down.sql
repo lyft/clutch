2ad1f024f9ecb72e3a34dd17356f6361000a8869
Rafal Augustyniak <raugustyniak@lyft.com>
2020-09-23 18:53:11 -0700

    fixes after merge

--- backend/cmd/migrate/migrations/000006_add_canceled_at_column.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE experiment_run DROP COLUMN cancellation_time;
-ALTER TABLE experiment_run ADD COLUMN scheduled_end_time TIMESTAMP WITH TIME ZONE;
--- backend/cmd/migrate/migrations/000006_add_canceled_at_column.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE experiment_run DROP COLUMN scheduled_end_time;
-ALTER TABLE experiment_run ADD COLUMN cancellation_time TIMESTAMP WITH TIME ZONE;
