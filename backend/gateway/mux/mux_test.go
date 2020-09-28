012c6cf37170108718e02fb1cc6acbd12d6b1d8b
Mike Cutalo <mcutalo88@gmail.com>
2020-09-28 12:12:33 -0700

    Revert "Revert "core / frontend: passthrough host support (#468)" (#481)"
    This reverts commit 515abb6f1b5ad8a21c68314787967e17a72d7f96.

--- backend/gateway/mux/mux_test.go
+++ backend/gateway/mux/mux_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	gatewayv1 "github.com/lyft/clutch/backend/api/config/gateway/v1"
 )
 
 func TestCopyHTTPResponse(t *testing.T) {
@@ -28,4 +30,22 @@ func TestCopyHTTPResponse(t *testing.T) {
 	assert.Equal(t, status, result.StatusCode)
 	assert.Equal(t, headers, rec.Header())
 	assert.Equal(t, body, rec.Body.String())
+}
+
+func TestAssetProviderS3Handler(t *testing.T) {
+	handler := &assetHandler{
+		assetCfg: &gatewayv1.Assets{
+			Provider: &gatewayv1.Assets_S3{
+				S3: &gatewayv1.Assets_S3Provider{
+					Region: "us-east-1",
+					Bucket: "clutch",
+					Key:    "static",
+				},
+			},
+		},
+	}
+
+	// Test that the aws service must be configured to use the S3 handler
+	_, err := handler.assetProviderHandler("clutch.sh/static/main.js")
+	assert.Error(t, err)
 }
