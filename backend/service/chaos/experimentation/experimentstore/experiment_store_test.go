e7f0084ef3c259202586cfc56fb6ff2e1b9bd08e
Rafal Augustyniak <raugustyniak@lyft.com>
2020-09-28 11:30:41 -0700

    fixes after merge

--- backend/service/chaos/experimentation/experimentstore/experiment_store_test.go
+++ backend/service/chaos/experimentation/experimentstore/experiment_store_test.go
@@ -138,63 +138,6 @@ func TestCancelExperimentRun(t *testing.T) {
 	assert.NoError(err)
 }
 
-var getExperimentsTests = []struct {
-	id   string
-	sql  string
-	args []driver.Value
-	rows [][]driver.Value
-	err  error
-}{
-	{
-		id:   "get all experiments",
-		sql:  `SELECT experiment_run.id, details FROM experiment_config, experiment_run WHERE experiment_config.id = experiment_run.experiment_config_id AND ($1 = '' OR $1 = experiment_config.details ->> '@type')`,
-		args: []driver.Value{"upstreamCluster", "downstreamCluster", 1},
-		rows: [][]driver.Value{
-			{
-				1234,
-				`{"@type": "type.googleapis.com/clutch.chaos.serverexperimentation.v1.TestConfig","clusterPair":{"downstreamCluster":"upstreamCluster","upstreamCluster":"downstreamCluster"},"abort":{"percent":100,"httpStatus":401}}`,
-			},
-		},
-	},
-}
-
-func TestGetExperiments(t *testing.T) {
-	t.Parallel()
-
-	for _, test := range getExperimentsTests {
-		test := test
-
-		t.Run(test.id, func(t *testing.T) {
-			t.Parallel()
-			a := assert.New(t)
-
-			db, mock, err := sqlmock.New()
-			a.NoError(err)
-
-			es := &experimentStore{db: db}
-			defer es.Close()
-
-			expected := mock.ExpectQuery(regexp.QuoteMeta(test.sql)).WithArgs("foo", "UNSPECIFIED")
-			if test.err != nil {
-				expected.WillReturnError(test.err)
-			} else {
-				rows := sqlmock.NewRows(experimentColumns)
-				for _, row := range test.rows {
-					rows.AddRow(row...)
-				}
-				expected.WillReturnRows(rows)
-			}
-
-			experiments, err := es.GetExperiments(context.Background(), "foo", experimentation.GetExperimentsRequest_UNSPECIFIED)
-			if test.err != nil {
-				a.Equal(test.err, err)
-			} else {
-				a.NoError(err)
-			}
-		})
-	}
-}
-
 var getExperimentsSQLQuery = `SELECT experiment_run.id, details FROM experiment_config, experiment_run WHERE experiment_config.id = experiment_run.experiment_config_id AND ($1 = '' OR $1 = experiment_config.details ->> '@type')`
 
 func TestGetExperimentsUnmarshalsExperimentConfiguration(t *testing.T) {
@@ -206,7 +149,7 @@ func TestGetExperimentsUnmarshalsExperimentConfiguration(t *testing.T) {
 	es := &experimentStore{db: db}
 	defer es.Close()
 
-	expected := mock.ExpectQuery(regexp.QuoteMeta(getExperimentsSQLQuery)).WithArgs("foo")
+	expected := mock.ExpectQuery(regexp.QuoteMeta(getExperimentsSQLQuery)).WithArgs("foo", "UNSPECIFIED")
 	rows := sqlmock.NewRows(experimentColumns)
 	rows.AddRow([]driver.Value{
 		1234,
@@ -214,7 +157,7 @@ func TestGetExperimentsUnmarshalsExperimentConfiguration(t *testing.T) {
 	}...)
 	expected.WillReturnRows(rows)
 
-	experiments, err := es.GetExperiments(context.Background(), "foo")
+	experiments, err := es.GetExperiments(context.Background(), "foo", experimentation.GetExperimentsRequest_UNSPECIFIED)
 	assert.NoError(err)
 
 	assert.Equal(1, len(experiments))
@@ -222,10 +165,8 @@ func TestGetExperimentsUnmarshalsExperimentConfiguration(t *testing.T) {
 	assert.Equal(uint64(1234), experiment.GetId())
 
 	config := &serverexperimentation.TestConfig{}
-	err2 := ptypes.UnmarshalAny(experiment.GetConfig(), config)
-	if err2 != nil {
-		t.Errorf("setSnapshot failed %v", err2)
-	}
+	err = ptypes.UnmarshalAny(experiment.GetConfig(), config)
+	assert.NoError(err)
 	assert.Nil(config.GetLatency())
 	abort := config.GetAbort()
 	assert.NotNil(abort)
@@ -242,7 +183,7 @@ func TestGetExperimentsFailsIfItReadsExperimentWithMalformedConfiguration(t *tes
 	es := &experimentStore{db: db}
 	defer es.Close()
 
-	expected := mock.ExpectQuery(regexp.QuoteMeta(getExperimentsSQLQuery)).WithArgs("foo")
+	expected := mock.ExpectQuery(regexp.QuoteMeta(getExperimentsSQLQuery)).WithArgs("foo", "UNSPECIFIED")
 	rowsData := [][]driver.Value{
 		{
 			1,
@@ -260,7 +201,7 @@ func TestGetExperimentsFailsIfItReadsExperimentWithMalformedConfiguration(t *tes
 	}
 	expected.WillReturnRows(rows)
 
-	experiments, err := es.GetExperiments(context.Background(), "foo")
+	experiments, err := es.GetExperiments(context.Background(), "foo", experimentation.GetExperimentsRequest_UNSPECIFIED)
 	assert.Nil(experiments)
 	assert.Error(err)
 }
