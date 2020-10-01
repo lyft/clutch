974a919a366559f952b9fa605ddcc6b29b3da0a4
Mike Cutalo <mcutalo88@gmail.com>
2020-10-01 13:20:24 -0700

    remove tests for now

--- backend/service/topology/cache.go
+++ backend/service/topology/cache.go
@@ -23,6 +23,8 @@ func (c *client) acquireTopologyCacheLock() {
 	for {
 		c.log.Debug("trying to acquire advisory lock")
 
+		// Advisory locks only take a arbitrary bigint value
+		// In this case 100 is the lock for topology caching
 		if tryAdvisoryLock(conn, 100) {
 			c.log.Debug("acquired the advisory lock, starting to cache topology now...")
 			c.startTopologyCache()
@@ -35,9 +37,6 @@ func (c *client) acquireTopologyCacheLock() {
 func tryAdvisoryLock(conn *sql.Conn, lockId int) bool {
 	var lock bool
 
-	// Advisory locks only take a arbitrary bigint value
-	// In this case 100 is the lock for topology caching
-	//
 	// Notably we do not use `pg_advisory_lock` as the behavior of this function stack locks.
 	// For each lock invocation requires the same number of unlocks to release the advisory lock.
 	// https://www.postgresql.org/docs/12/functions-admin.html#FUNCTIONS-ADVISORY-LOCKS-TABL
