4ea0733467f35856166e112f27d0196d1c32619b
Scarlett Perry <sperry@lyft.com>
2020-10-12 17:11:21 -0400

    Fix linting error

--- backend/middleware/audit/audit.go
+++ backend/middleware/audit/audit.go
@@ -86,14 +86,14 @@ func (m *mid) eventFromRequest(ctx context.Context, req interface{}, info *grpc.
 	}
 
 	return &auditv1.RequestEvent{
-		Username:        username,
-		ServiceName:     svc,
-		MethodName:      method,
-		Type:            meta.GetAction(info.FullMethod),
-		Resources:       meta.ResourceNames(req.(descriptor.Message)),
+		Username:    username,
+		ServiceName: svc,
+		MethodName:  method,
+		Type:        meta.GetAction(info.FullMethod),
+		Resources:   meta.ResourceNames(req.(descriptor.Message)),
 		RequestMetadata: &auditv1.RequestMetadata{
 			Body: meta.APIMetadata(req),
-		}
+		},
 	}
 }
 
@@ -104,10 +104,10 @@ func (m *mid) eventFromResponse(resp interface{}, err error) *auditv1.RequestEve
 	}
 
 	return &auditv1.RequestEvent{
-		Status:           s.Proto(),
-		Resources:        meta.ResourceNames(resp.(descriptor.Message)),
+		Status:    s.Proto(),
+		Resources: meta.ResourceNames(resp.(descriptor.Message)),
 		ResponseMetadata: &auditv1.ResponseMetadata{
 			Body: meta.APIMetadata(resp),
-		}
+		},
 	}
 }
