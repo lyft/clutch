cfbda4db4e16ebec4a4af3d3876265a30d8d67eb
Rafal Augustyniak <raugustyniak@lyft.com>
2020-10-12 20:55:12 -0700

    Small code clean-up in server-experimentation

--- backend/module/chaos/serverexperimentation/server_experimentation.go
+++ backend/module/chaos/serverexperimentation/server_experimentation.go
@@ -49,23 +49,17 @@ func (s *Service) Register(r module.Registrar) error {
 }
 
 func (s *Service) transform(config *experimentstore.ExperimentConfig) ([]*experimentation.Property, error) {
-	var testConfig = serverexperimentation.TestConfig{}
-	if err := ptypes.UnmarshalAny(config.Config, &testConfig); err != nil {
+	var experimentConfig = serverexperimentation.TestConfig{}
+	if err := ptypes.UnmarshalAny(config.Config, &experimentConfig); err != nil {
 		return []*experimentation.Property{}, err
 	}
 
-	upstream := testConfig.GetClusterPair().GetUpstreamCluster()
-	downstream := testConfig.GetClusterPair().GetDownstreamCluster()
+	upstream := experimentConfig.GetClusterPair().GetUpstreamCluster()
+	downstream := experimentConfig.GetClusterPair().GetDownstreamCluster()
 
-	var description string
-	switch testConfig.GetFault().(type) {
-	case *serverexperimentation.TestConfig_Abort:
-		description = "Abort"
-	case *serverexperimentation.TestConfig_Latency:
-		description = "Latency"
-	default:
-		s.logger.Errorw("GetExperiments: Unable to retrieve experiments", "fault", testConfig.GetFault())
-		return nil, fmt.Errorf("unexpected fault type %v", testConfig.GetFault())
+	faultsDescription, err := experimentConfigToString(&experimentConfig)
+	if err != nil {
+		return nil, err
 	}
 
 	return []*experimentation.Property{
@@ -77,12 +71,30 @@ func (s *Service) transform(config *experimentstore.ExperimentConfig) ([]*experi
 		{
 			Id:    "target",
 			Label: "Target",
-			Value: &experimentation.Property_StringValue{StringValue: fmt.Sprintf("\"%s\" ➡️ \"%s\"", downstream, upstream)},
+			Value: &experimentation.Property_StringValue{StringValue: fmt.Sprintf("%s ➡️ %s", downstream, upstream)},
 		},
 		{
 			Id:    "fault_types",
 			Label: "Fault Types",
-			Value: &experimentation.Property_StringValue{StringValue: description},
+			Value: &experimentation.Property_StringValue{StringValue: faultsDescription},
 		},
 	}, nil
+}
+
+func experimentConfigToString(experiment *serverexperimentation.TestConfig) (string, error) {
+	if experiment == nil {
+		return "", errors.New("experiment is nil")
+	}
+
+	var description string
+	switch experiment.GetFault().(type) {
+	case *serverexperimentation.TestConfig_Abort:
+		description = "Abort"
+	case *serverexperimentation.TestConfig_Latency:
+		description = "Latency"
+	default:
+		return nil, fmt.Errorf("unexpected fault type %v", fault)
+	}
+
+	return description
 }
