2ca7a88adb4c7d84ed50783167cf5b5dda7e4f09
Rafal Augustyniak <raugustyniak@lyft.com>
2020-10-16 18:37:28 -0700

    separate file

---
+++ frontend/workflows/serverexperimentation/src/radio-control.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@material-ui/core";
+
+interface RadioControlItem {
+  label: string;
+  value: string;
+}
+
+interface RadioControlProps {
+  name: string;
+  label: string;
+  items: RadioControlItem[];
+  onChange: (value: string) => void;
+}
+
+const RadioControl: React.FC<RadioControlProps> = ({ name, label, items, onChange }) => {
+  return (
+    <FormControl key={name}>
+      <FormLabel component="legend">Upstream Cluster Type</FormLabel>
+      <RadioGroup
+        aria-label={label}
+        name={name}
+        defaultValue={items[0].value}
+        onChange={e => onChange(e.target.value)}
+      >
+        {items &&
+          items.map(item => {
+            return (
+              <FormControlLabel
+                key={item.value}
+                value={item.value}
+                control={<Radio />}
+                label={item.label}
+              />
+            );
+          })}
+      </RadioGroup>
+    </FormControl>
+  );
+};
+
+export default RadioControl;
--- frontend/workflows/serverexperimentation/src/start-experiment.tsx
+++ frontend/workflows/serverexperimentation/src/start-experiment.tsx
@@ -12,46 +12,9 @@ import {
 import { useDataLayout } from "@clutch-sh/data-layout";
 import type { WizardChild } from "@clutch-sh/wizard";
 import { Wizard, WizardStep } from "@clutch-sh/wizard";
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@material-ui/core";
 import * as yup from "yup";
 
-interface RadioControlItem {
-  label: string;
-  value: string;
-}
-
-interface RadioControlProps {
-  name: string;
-  label: string;
-  items: RadioControlItem[];
-  onChange: (value: string) => void;
-}
-
-const RadioControl: React.FC<RadioControlProps> = ({ name, label, items, onChange }) => {
-  return (
-    <FormControl key={name}>
-      <FormLabel component="legend">Upstream Cluster Type</FormLabel>
-      <RadioGroup
-        aria-label={label}
-        name={name}
-        defaultValue={items[0].value}
-        onChange={e => onChange(e.target.value)}
-      >
-        {items &&
-          items.map(item => {
-            return (
-              <FormControlLabel
-                key={item.value}
-                value={item.value}
-                control={<Radio />}
-                label={item.label}
-              />
-            );
-          })}
-      </RadioGroup>
-    </FormControl>
-  );
-};
+import { RadioControl } from "./radio-control";
 
 const faultInjectionTypeItems = [
   {
