2cac4ccd5be0671b078a0f80e4c39b82b1016cbe
Derek Schaller <d_a_schaller@yahoo.com>
2020-10-20 13:19:27 -0700

    storybook: add hint story

---
+++ frontend/packages/core/src/Feedback/stories/hint.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import type { Meta } from "@storybook/react";
+
+import Hint from "../hint";
+
+export default {
+  title: "Core/Feedback/Hint",
+  component: Hint,
+} as Meta;
+
+const Template = (props: { url: string; text: string }) => (
+  <Hint>
+    {props.url && <img src={props.url} />}
+    <div style={{ padding: "10px" }}>{props.text}</div>
+  </Hint>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: "Some helpful text",
+};
+
+export const EmbeddedImage = Template.bind({});
+EmbeddedImage.args = {
+  url: "https://www.clutch.sh/img/favicon.ico",
+  text: "This is the Clutch logo!",
+};
