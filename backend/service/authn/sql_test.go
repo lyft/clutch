066c888f0d544175d726df5a839c0feb70801816
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-11-05 12:04:40 -0600

    add test

---
+++ backend/service/authn/sql_test.go
@@ -0,0 +1,30 @@
+package authn
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateOrUpdateUser(t *testing.T) {
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	id := "foo@example.com"
+	refreshToken := []byte("abdcdefgzzz")
+
+	r := &repository{db: db}
+	mock.ExpectQuery(createOrUpdateUser).
+		WithArgs(id, refreshToken).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "provider_refresh_token"}).AddRow(id, refreshToken))
+
+	u, err := r.createOrUpdateUser(context.Background(), id, refreshToken)
+	assert.NoError(t, err)
+	assert.Equal(t, id, u.id)
+	assert.Equal(t, refreshToken, u.providerRefreshToken)
+}
