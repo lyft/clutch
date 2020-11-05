f9eb24f37b203c67037fa4071020e5511efa2563
Derek <dschaller@users.noreply.github.com>
2020-11-05 12:29:24 -0800

    frontend: add types for JWT Token (#656)

--- frontend/packages/core/src/AppLayout/user.tsx
+++ frontend/packages/core/src/AppLayout/user.tsx
@@ -33,6 +33,10 @@ const Initials = styled(Typography)`
   `}
 `;
 
+interface JwtToken {
+  sub: string;
+}
+
 const userId = (): string => {
   // Check JWT token for subject and display if available.
   const token = Cookies.get("token");
@@ -41,7 +45,7 @@ const userId = (): string => {
   }
   let subject = "Unknown user";
   try {
-    const decoded = jwtDecode(token);
+    const decoded = jwtDecode(token) as JwtToken;
     if (decoded?.sub) {
       subject = decoded.sub;
     }
