a178edbdc431923b3f5f6e7628dde0ea64ddd568
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-11-19 11:38:10 -0600

    make controls first

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -12,8 +12,7 @@ module.exports = {
     }
   },
   addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-links",
   ],
 }
