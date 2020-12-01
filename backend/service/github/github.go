8f5d1a969b3782d008ac7254745468b5932e7197
Angela Nguyen <3279569+angethuy@users.noreply.github.com>
2020-12-01 14:43:42 -0800

    Update backend/service/github/github.go
    Co-authored-by: Daniel Hochman <danielhochman@users.noreply.github.com>

--- backend/service/github/github.go
+++ backend/service/github/github.go
@@ -69,7 +69,7 @@ type File struct {
 type Client interface {
 	GetFile(ctx context.Context, ref *RemoteRef, path string) (*File, error)
 	CreateBranch(ctx context.Context, req *CreateBranchRequest) error
-	CreatePullRequest(ctx context.Context, ref *RemoteRef, title, base, body string) (*PullRequestInfo, error)
+	CreatePullRequest(ctx context.Context, ref *RemoteRef, base, title, body string) (*PullRequestInfo, error)
 	CreateRepository(ctx context.Context, req *sourcecontrolv1.CreateRepositoryRequest) (*sourcecontrolv1.CreateRepositoryResponse, error)
 	CreateIssueComment(ctx context.Context, ref *RemoteRef, number int, body string) error
 	CompareCommits(ctx context.Context, ref *RemoteRef, compareSHA string) (*scgithubv1.CommitComparison, error)
