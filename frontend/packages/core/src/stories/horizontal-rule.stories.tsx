9b93a3f8e40cf7eb75e9b0aa16760a0643585002
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-12-01 11:21:53 -0600

    lint fix and replace

--- frontend/packages/core/src/stories/horizontal-rule.stories.tsx
+++ frontend/packages/core/src/stories/horizontal-rule.stories.tsx
@@ -1,26 +1,30 @@
 import * as React from "react";
+import ErrorIcon from "@material-ui/icons/Error";
 import type { Meta } from "@storybook/react";
 
 import type { HorizontalRuleProps } from "../horizontal-rule";
-import HorizontalRule from "../horizontal-rule";
-
-import ErrorIcon from "@material-ui/icons/Error";
+import { HorizontalRule } from "../horizontal-rule";
 
 export default {
-    title: "Core/HorizontalRule",
-    component: HorizontalRule,
+  title: "Core/HorizontalRule",
+  component: HorizontalRule,
 } as Meta;
 
 const Template = (props: HorizontalRuleProps) => <HorizontalRule {...props} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
-    children: "OR"
+  children: "OR",
 };
 
 export const WithIcon = Template.bind({});
 WithIcon.args = {
-    children: <><ErrorIcon />&nbsp;Proceed with caution</>
+  children: (
+    <>
+      <ErrorIcon />
+      &nbsp;Proceed with caution
+    </>
+  ),
 };
 
 export const WithoutText = Template.bind({});
