8788b4bfb94b2b2d5eb1394703109189af65debd
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-12-02 14:58:30 -0600

    wip

--- frontend/packages/core/src/accordion.tsx
+++ frontend/packages/core/src/accordion.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import { Accordion as MuiAccordion, AccordionDetails, AccordionActions, AccordionSummary as MuiAccordionSummary } from "@material-ui/core";
+import type {AccordionProps as MuiAccordionProps} from "@material-ui/core";
+import { Accordion as MuiAccordion, AccordionDetails, AccordionActions, AccordionSummary as MuiAccordionSummary, Divider } from "@material-ui/core";
 
-import AddCircleIcon from "@material-ui/icons/AddCircle";
-import RemoveCircleIcon from "@material-ui/icons/RemoveCircle";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
-
-export const Accordion = styled(MuiAccordion)({
+const StyledAccordion = styled(MuiAccordion)({
     borderRadius: "4px",
     boxShadow: "none",
+    border: "1px solid transparent",
+    boxSizing: "content-box",
 
     "&.Mui-expanded": {
         margin: "0",
-        boxShadow: "0px 4px 6px rgba(53, 72, 212, 0.2)"
+        boxShadow: "0px 4px 6px rgba(53, 72, 212, 0.2)",
+        borderColor: "rgba(13, 16, 48, 0.12)",
     },
 
     ".MuiIconButton-root": {
@@ -25,39 +28,53 @@ export const Accordion = styled(MuiAccordion)({
 
     ".MuiAccordionDetails-root": {
         padding: "16px",
+        fontSize: "16px",
     },
 
     ".MuiAccordionActions-root": {
-        padding: "16px",
+        padding: "0",
     },
-
-    ".MuiCollapse-wrapper": {
-        borderRadius: "0 0 4px 4px",
-        border: "1px solid #e1e4f9",
-    }
 });
+
+export interface AccordionProps extends Pick<MuiAccordionProps, "defaultExpanded"> {
+    title?: string;
+    children: React.ReactNode[];
+} 
+
+export const Accordion = ({title, children, ...props}: AccordionProps) => (
+<StyledAccordion {...props}>
+    <StyledAccordionSummary>{title}</StyledAccordionSummary>
+    {children}
+</StyledAccordion>
+)
 
 const AccordionSummaryBase = ({ children, ...props }) => {
     const [expanded, setExpanded] = React.useState(false);
-  
-    const onClick = () => {
-      setExpanded(!expanded);
-    };
-  
-    return (
-      <MuiAccordionSummary
-        {...props}
-        expandIcon={expanded ? <AddCircleIcon /> : <RemoveCircleIcon />}
-        onClick={onClick}
-      >
-        {children}
-      </MuiAccordionSummary>
-    );
-  };
 
-export const AccordionSummary = styled(AccordionSummaryBase)({
+    return (
+        <MuiAccordionSummary
+            {...props}
+            expandIcon={expanded ? <RemoveIcon /> : <AddIcon />}
+            onClick={() => setExpanded(!expanded)}
+        >
+            {children}
+        </MuiAccordionSummary>
+    );
+};
+
+export const StyledAccordionSummary = styled(AccordionSummaryBase)({
     backgroundColor: "#fafafb",
-    borderRadius: "4px 4px 0 0",
+    borderRadius: "4px",
+    color: "#0d1030",
+    height: "48px",
+
+    "&:hover": {
+        backgroundColor: "rgba(13, 16, 48, 0.03)",
+    },
+
+    "&:active": {
+        backgroundColor: "rgba(13, 16, 48, 0.12)",
+    },
 
     ".MuiAccordionSummary-content": {
         margin: "12px 0",
@@ -68,4 +85,6 @@ export const AccordionSummary = styled(AccordionSummaryBase)({
         backgroundColor: "#e1e4f9",
         minHeight: "48px",
     }
-})
+});
+
+export { AccordionActions, AccordionDetails, Divider as AccordionDivider };
