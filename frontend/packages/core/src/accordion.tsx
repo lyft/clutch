feb5d2039920075c794b1f1a2310eff4dd663da2
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-12-02 15:57:28 -0600

    lint

--- frontend/packages/core/src/accordion.tsx
+++ frontend/packages/core/src/accordion.tsx
@@ -1,90 +1,104 @@
 import * as React from "react";
 import styled from "@emotion/styled";
-import type {AccordionProps as MuiAccordionProps} from "@material-ui/core";
-import { Accordion as MuiAccordion, AccordionDetails, AccordionActions, AccordionSummary as MuiAccordionSummary, Divider } from "@material-ui/core";
-
+import type { AccordionProps as MuiAccordionProps } from "@material-ui/core";
+import {
+  Accordion as MuiAccordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionSummary as MuiAccordionSummary,
+  Divider,
+} from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import RemoveIcon from "@material-ui/icons/Remove";
 
 const StyledAccordion = styled(MuiAccordion)({
-    borderRadius: "4px",
-    boxShadow: "none",
-    border: "1px solid transparent",
-    boxSizing: "content-box",
+  borderRadius: "4px",
+  boxShadow: "none",
+  border: "1px solid transparent",
+  boxSizing: "content-box",
 
-    "&.Mui-expanded": {
-        margin: "0",
-        boxShadow: "0px 4px 6px rgba(53, 72, 212, 0.2)",
-        borderColor: "rgba(13, 16, 48, 0.12)",
-    },
+  "&.Mui-expanded": {
+    margin: "0",
+    boxShadow: "0px 4px 6px rgba(53, 72, 212, 0.2)",
+    borderColor: "rgba(13, 16, 48, 0.12)",
+  },
 
-    ".MuiIconButton-root": {
-        color: "rgba(13, 16, 48, 0.38)",
-    },
+  ".MuiIconButton-root": {
+    color: "rgba(13, 16, 48, 0.38)",
+  },
 
-    ".MuiIconButton-root.Mui-expanded": {
-        color: "rgba(13, 16, 48, 0.6)"
-    },
+  ".MuiIconButton-root.Mui-expanded": {
+    color: "rgba(13, 16, 48, 0.6)",
+  },
 
-    ".MuiAccordionDetails-root": {
-        padding: "16px",
-        fontSize: "16px",
-    },
+  ".MuiAccordionDetails-root": {
+    padding: "16px",
+    fontSize: "16px",
+  },
 
-    ".MuiAccordionActions-root": {
-        padding: "0",
-    },
+  ".MuiAccordionActions-root": {
+    padding: "0",
+  },
 });
 
 export interface AccordionProps extends Pick<MuiAccordionProps, "defaultExpanded"> {
-    title?: string;
-    children: React.ReactNode[];
-} 
+  title?: string;
+  collapsible?: boolean;
+  children: React.ReactNode[];
+}
 
-export const Accordion = ({title, children, ...props}: AccordionProps) => (
-<StyledAccordion {...props}>
-    <StyledAccordionSummary>{title}</StyledAccordionSummary>
+export const Accordion = ({
+  title,
+  collapsible,
+  defaultExpanded,
+  children,
+  ...props
+}: AccordionProps) => (
+  <StyledAccordion defaultExpanded={defaultExpanded} {...props}>
+    <StyledAccordionSummary defaultExpanded={defaultExpanded} collapsible={collapsible}>
+      {title}
+    </StyledAccordionSummary>
     {children}
-</StyledAccordion>
-)
+  </StyledAccordion>
+);
 
-const AccordionSummaryBase = ({ children, ...props }) => {
-    const [expanded, setExpanded] = React.useState(false);
+const AccordionSummaryBase = ({ children, collapsible, defaultExpanded, ...props }) => {
+  const [expanded, setExpanded] = React.useState(defaultExpanded);
 
-    return (
-        <MuiAccordionSummary
-            {...props}
-            expandIcon={expanded ? <RemoveIcon /> : <AddIcon />}
-            onClick={() => setExpanded(!expanded)}
-        >
-            {children}
-        </MuiAccordionSummary>
-    );
+  return (
+    <MuiAccordionSummary
+      {...props}
+      expandIcon={collapsible ? expanded ? <RemoveIcon /> : <AddIcon /> : null}
+      onClick={() => setExpanded((v: boolean) => !v)}
+    >
+      {children}
+    </MuiAccordionSummary>
+  );
 };
 
 export const StyledAccordionSummary = styled(AccordionSummaryBase)({
-    backgroundColor: "#fafafb",
-    borderRadius: "4px",
-    color: "#0d1030",
-    height: "48px",
+  backgroundColor: "#fafafb",
+  borderRadius: "4px",
+  color: "#0d1030",
+  height: "48px",
 
-    "&:hover": {
-        backgroundColor: "rgba(13, 16, 48, 0.03)",
-    },
+  "&:hover": {
+    backgroundColor: "rgba(13, 16, 48, 0.03)",
+  },
 
-    "&:active": {
-        backgroundColor: "rgba(13, 16, 48, 0.12)",
-    },
+  "&:active": {
+    backgroundColor: "rgba(13, 16, 48, 0.12)",
+  },
 
-    ".MuiAccordionSummary-content": {
-        margin: "12px 0",
-        fontSize: "16px",
-    },
+  ".MuiAccordionSummary-content": {
+    margin: "12px 0",
+    fontSize: "16px",
+  },
 
-    "&.Mui-expanded": {
-        backgroundColor: "#e1e4f9",
-        minHeight: "48px",
-    }
+  "&.Mui-expanded": {
+    backgroundColor: "#e1e4f9",
+    minHeight: "48px",
+  },
 });
 
 export { AccordionActions, AccordionDetails, Divider as AccordionDivider };
--- frontend/packages/core/src/stories/accordion.stories.tsx
+++ frontend/packages/core/src/stories/accordion.stories.tsx
@@ -5,23 +5,29 @@ import type { AccordionProps } from "../accordion";
 import { Accordion, AccordionActions, AccordionDetails, AccordionDivider } from "../accordion";
 import { Button } from "../button";
 
-
 export default {
-    title: "Core/Accordion",
-    component: Accordion,
+  title: "Core/Accordion",
+  component: Accordion,
 } as Meta;
 
-const Template = (props: AccordionProps) => <Accordion {...props}>
-    <AccordionDetails>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</AccordionDetails>
+const Template = (props: AccordionProps) => (
+  <Accordion {...props}>
+    <AccordionDetails>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </AccordionDetails>
     <AccordionDivider />
     <AccordionActions>
-        <Button text="Okay" />
+      <Button text="Okay" />
     </AccordionActions>
-</Accordion>;
+  </Accordion>
+);
 
 export const Basic = Template.bind({});
 Basic.args = {
-    title: "Hello world!",
-    defaultExpanded: true,
-}
+  title: "Hello world!",
+  defaultExpanded: true,
+};
