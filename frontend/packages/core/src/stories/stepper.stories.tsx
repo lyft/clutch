fbff8494f31f39b843e9f65a4df55208ce0dd234
Derek Schaller <d_a_schaller@yahoo.com>
2020-12-07 11:41:55 -0800

    fix error step

--- frontend/packages/core/src/stories/stepper.stories.tsx
+++ frontend/packages/core/src/stories/stepper.stories.tsx
@@ -54,51 +54,42 @@ const PrimaryTemplate = ({ stepCount, activeStep }: StepperProps & { stepCount: 
   );
 };
 
-const FailureTemplate = () => {
-  const [activeStep, setActiveStep] = React.useState(0);
+const FailureTemplate = ({ failedStep = 2, activeStep }: StepperProps & { failedStep: number }) => {
+  const [curStep, setCurStep] = React.useState(activeStep || 0);
+  const stepCount = 4;
 
   const handleNext = () => {
-    setActiveStep(prevActiveStep => prevActiveStep + 1);
+    setCurStep(prevActiveStep => prevActiveStep + 1);
   };
 
   const handleBack = () => {
-    setActiveStep(prevActiveStep => prevActiveStep - 1);
+    setCurStep(prevActiveStep => prevActiveStep - 1);
   };
 
   const handleReset = () => {
-    setActiveStep(0);
+    setCurStep(0);
   };
 
   return (
     <>
-      <Stepper activeStep={activeStep}>
-        <Step label="Step 1">
-          <Text>First step content</Text>
-        </Step>
-        <Step label="Step 2" error>
-          <Text>Second step content</Text>
-        </Step>
-        <Step label="Step 3">
-          <Text>Third step content</Text>
-        </Step>
-        <Step label="Step 4">
-          <Text>Fourth step content</Text>
-        </Step>
+      <Stepper activeStep={curStep}>
+        {[...Array(stepCount)].map((_, index: number) => (
+          <Step
+            error={curStep === failedStep && index === failedStep - 1}
+            key={index} // eslint-disable-line react/no-array-index-key
+            label={`Step ${index + 1}`}
+          />
+        ))}
       </Stepper>
       <div>
-        <Text>Step {activeStep + 1} content</Text>
+        <Text>Step {curStep + 1} content</Text>
 
-        {activeStep === 2 ? (
+        {curStep === stepCount - 1 || curStep === failedStep ? (
           <Button onClick={handleReset} text="Reset" variant="neutral" />
         ) : (
           <div>
-            <Button
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              text="Back"
-              variant="neutral"
-            />
-            <Button onClick={handleNext} text={activeStep === 3 ? "Finish" : "Next"} />
+            <Button disabled={curStep === 0} onClick={handleBack} text="Back" variant="neutral" />
+            <Button onClick={handleNext} text={curStep === stepCount ? "Finish" : "Next"} />
           </div>
         )}
       </div>
@@ -112,3 +103,6 @@ Primary.args = {
 };
 
 export const Failure = FailureTemplate.bind({});
+Failure.args = {
+  failedStep: 2,
+};
