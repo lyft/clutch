f3053e85283049b43d7ed85ce311df9d8523a2ca
Daniel Hochman <danielhochman@users.noreply.github.com>
2020-12-10 14:44:33 -0600

    ui v2: fix accordion bug (#775)

--- frontend/packages/core/src/accordion.tsx
+++ frontend/packages/core/src/accordion.tsx
@@ -125,11 +125,7 @@ export const Accordion = ({
   };
 
   return (
-    <StyledAccordion
-      defaultExpanded={defaultExpanded}
-      expanded={expanded || defaultExpanded || false}
-      {...props}
-    >
+    <StyledAccordion defaultExpanded={defaultExpanded} expanded={expanded} {...props}>
       <StyledAccordionSummary expanded={expanded} collapsible={collapsible} onClick={handleClick}>
         {title}
       </StyledAccordionSummary>
