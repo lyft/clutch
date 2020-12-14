5c3f8584efb8a457878cb73f6e5e1150c69ef7e2
Scarlett Perry <sperry@lyft.com>
2020-12-14 12:20:44 -0500

    update drawer story

--- frontend/packages/core/src/AppLayout/stories/drawer.stories.tsx
+++ frontend/packages/core/src/AppLayout/stories/drawer.stories.tsx
@@ -1,51 +1,54 @@
-import React from "react";
+import * as React from "react";
 import { MemoryRouter } from "react-router";
+import styled from "@emotion/styled";
+import { Box as MuiBox } from "@material-ui/core";
 import type { Meta } from "@storybook/react";
 
 import { ApplicationContext } from "../../Contexts/app-context";
 import Drawer from "../drawer";
-import { Primary as HeaderStory } from "./header.stories";
 
 export default {
   title: "Core/AppLayout/Drawer",
   component: Drawer,
   decorators: [
-    () => (
+    Sidebar => (
       <MemoryRouter>
-        <Drawer />
+        <Sidebar />
       </MemoryRouter>
     ),
     StoryFn => {
       return (
-        <ApplicationContext.Provider value={{
-          workflows: [
-            {
-              developer: { name: "Lyft", contactUrl: "mailto:hello@clutch.sh" },
-              displayName: "EC2",
-              group: "AWS",
-              path: "ec2",
-              routes: [
-                {
-                  component: () => <></>,
-                  componentProps: { resolverType: "clutch.aws.ec2.v1.Instance" },
-                  description: "Terminate an EC2 instance.",
-                  displayName: "Terminate Instance",
-                  path: "instance/terminate",
-                  requiredConfigProps: ["resolverType"],
-                  trending: true,
-                },
-                {
-                  component: () => <></>,
-                  componentProps: { resolverType: "clutch.aws.ec2.v1.AutoscalingGroup" },
-                  description: "Resize an autoscaling group.",
-                  displayName: "Resize Autoscaling Group",
-                  path: "asg/resize",
-                  requiredConfigProps: ["resolverType"],
-                },
-              ],
-            },
-          ],
-        }}>
+        <ApplicationContext.Provider
+          value={{
+            workflows: [
+              {
+                developer: { name: "Lyft", contactUrl: "mailto:hello@clutch.sh" },
+                displayName: "EC2",
+                group: "AWS",
+                path: "ec2",
+                routes: [
+                  {
+                    component: () => <></>,
+                    componentProps: { resolverType: "clutch.aws.ec2.v1.Instance" },
+                    description: "Terminate an EC2 instance.",
+                    displayName: "Terminate Instance",
+                    path: "instance/terminate",
+                    requiredConfigProps: ["resolverType"],
+                    trending: true,
+                  },
+                  {
+                    component: () => <></>,
+                    componentProps: { resolverType: "clutch.aws.ec2.v1.AutoscalingGroup" },
+                    description: "Resize an autoscaling group.",
+                    displayName: "Resize Autoscaling Group",
+                    path: "asg/resize",
+                    requiredConfigProps: ["resolverType"],
+                  },
+                ],
+              },
+            ],
+          }}
+        >
           <StoryFn />
         </ApplicationContext.Provider>
       );
@@ -53,4 +56,16 @@ export default {
   ],
 } as Meta;
 
-export const Primary = () => <Drawer />;
+const Box = styled(MuiBox)({
+  ".MuiDrawer-root div[class*='MuiDrawer-paper']": {
+    top: 0,
+  },
+});
+
+const Template = () => (
+  <Box>
+    <Drawer />
+  </Box>
+);
+
+export const Primary = Template.bind({});
