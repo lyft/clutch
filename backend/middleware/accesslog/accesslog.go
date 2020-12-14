d56d408aa83879010eb91eae5ee73ece0b1b66f9
Rithu John <rjohn@lyft.com>
2020-12-14 10:51:02 -0800

    address nits

--- backend/middleware/accesslog/accesslog.go
+++ backend/middleware/accesslog/accesslog.go
@@ -1,4 +1,4 @@
-package log
+package accesslog
 
 // <!-- START clutchdoc -->
 // description: Enables access log filtering on responses to filter logs by status code
@@ -25,14 +25,14 @@ const Name = "clutch.middleware.accesslog"
 func New(config *accesslogv1.Config, logger *zap.Logger, scope tally.Scope) (middleware.Middleware, error) {
 	var statusCodes []codes.Code
 	// if no filter is provided default to logging all status codes
-	if config != nil && len(config.StatusCodeFilters) > 0 {
+	if config != nil {
 		for _, filter := range config.StatusCodeFilters {
 			switch t := filter.GetFilterType().(type) {
 			case *accesslogv1.Config_StatusCodeFilter_Equals:
 				statusCode := filter.GetEquals()
 				statusCodes = append(statusCodes, codes.Code(statusCode))
 			default:
-				logger.Error("status code filter not supported", zap.String("type", fmt.Sprintf("%T", t)))
+				return nil, fmt.Errorf("status code filter `%T` not supported", t)
 			}
 		}
 	}
@@ -43,7 +43,8 @@ func New(config *accesslogv1.Config, logger *zap.Logger, scope tally.Scope) (mid
 }
 
 type mid struct {
-	logger      *zap.Logger
+	logger *zap.Logger
+	// TODO(perf): improve lookup efficiency using a lookup table
 	statusCodes []codes.Code
 }
 
--- backend/middleware/accesslog/accesslog_test.go
+++ backend/middleware/accesslog/accesslog_test.go
@@ -1,4 +1,4 @@
-package log
+package accesslog
 
 import (
 	"context"
