ade5bfb445230df39bea90ba86f6cd22f8826373
Daniel Hochman <danielhochman@users.noreply.github.com>
2021-01-19 15:04:34 -0600

    authn: service test coverage (#894)

--- backend/service/authn/authn.go
+++ backend/service/authn/authn.go
@@ -6,18 +6,12 @@ package authn
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
-	"strings"
-	"time"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
-	"github.com/google/uuid"
 	"github.com/uber-go/tally"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
@@ -41,7 +35,13 @@ func New(cfg *any.Any, logger *zap.Logger, scope tally.Scope) (service.Service, 
 	if err := ptypes.UnmarshalAny(cfg, config); err != nil {
 		return nil, err
 	}
-	return NewProvider(config)
+
+	switch t := config.Type.(type) {
+	case *authnv1.Config_Oidc:
+		return NewOIDCProvider(context.Background(), config)
+	default:
+		return nil, fmt.Errorf("authn provider type '%T' not implemented", t)
+	}
 }
 
 // Standardized representation of a user's claims.
@@ -61,232 +61,12 @@ type Provider interface {
 	Exchange(ctx context.Context, code string) (token *oauth2.Token, err error)
 }
 
-type OIDCProvider struct {
-	provider *oidc.Provider
-	verifier *oidc.IDTokenVerifier
-	oauth2   *oauth2.Config
-
-	httpClient *http.Client
-
-	sessionSecret string
-
-	cryptographer *cryptographer
-
-	claimsFromOIDCToken ClaimsFromOIDCTokenFunc
-}
-
-func WithClaimsFromOIDCTokenFunc(p *OIDCProvider, fn ClaimsFromOIDCTokenFunc) *OIDCProvider {
-	ret := *p
-	ret.claimsFromOIDCToken = fn
-	return &ret
-}
-
-func (p *OIDCProvider) GetAuthCodeURL(ctx context.Context, state string) (string, error) {
-	opts := []oauth2.AuthCodeOption{oauth2.AccessTypeOffline}
-	return p.oauth2.AuthCodeURL(state, opts...), nil
-}
-
-func (p *OIDCProvider) ValidateStateNonce(state string) (string, error) {
-	claims := &stateClaims{}
-	_, err := jwt.ParseWithClaims(state, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(p.sessionSecret), nil
-	})
-	if err != nil {
-		return "", err
-	}
-	if err := claims.Valid(); err != nil {
-		return "", err
-	}
-	return claims.RedirectURL, nil
-}
-
-func (p *OIDCProvider) GetStateNonce(redirectURL string) (string, error) {
-	u, err := url.Parse(redirectURL)
-	if err != nil {
-		return "", err
-	}
-	if u.Host != "" {
-		return "", errors.New("only relative redirects are supported")
-	}
-	dest := u.Path
-	if !strings.HasPrefix(dest, "/") {
-		dest = fmt.Sprintf("/%s", dest)
-	}
-
-	claims := &stateClaims{
-		StandardClaims: &jwt.StandardClaims{
-			Subject:   uuid.New().String(), // UUID serves as CSRF token.
-			ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
-			IssuedAt:  time.Now().Unix(),
-		},
-		RedirectURL: dest,
-	}
-	return jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(p.sessionSecret))
-}
-
 type stateClaims struct {
 	*jwt.StandardClaims
 	RedirectURL string `json:"redirect"`
 }
 
-func (p *OIDCProvider) Exchange(ctx context.Context, code string) (*oauth2.Token, error) {
-	// Exchange.
-	ctx = oidc.ClientContext(ctx, p.httpClient)
-	token, err := p.oauth2.Exchange(ctx, code)
-	if err != nil {
-		return nil, err
-	}
-
-	rawIDToken, ok := token.Extra("id_token").(string)
-	if !ok {
-		return nil, errors.New("'id_token' was not present in oauth token")
-	}
-
-	// Verify. This is superfluous since the token was just issued but it can't hurt.
-	idToken, err := p.verifier.Verify(ctx, rawIDToken)
-	if err != nil {
-		return nil, err
-	}
-
-	// Issue token with claims.
-	claims, err := p.claimsFromOIDCToken(ctx, idToken)
-	if err != nil {
-		return nil, err
-	}
-
-	// TODO: Store the encrypted refresh token in the database.
-	if p.cryptographer != nil {
-		if _, err := p.cryptographer.Encrypt([]byte(token.RefreshToken)); err != nil {
-			return nil, err
-		}
-	}
-
-	// Sign and issue token.
-	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(p.sessionSecret))
-	if err != nil {
-		return nil, err
-	}
-	t := &oauth2.Token{
-		AccessToken:  accessToken,
-		Expiry:       time.Unix(claims.ExpiresAt, 0),
-		RefreshToken: "", // TODO: implement refresh_token flow with stateful sessions.
-		TokenType:    "Bearer",
-	}
-	return t, err
-}
-
 // Intermediate claims object for the ID token. Based on what scopes were requested.
 type idClaims struct {
 	Email string `json:"email"`
-}
-
-func oidcTokenToStandardClaims(t *oidc.IDToken) *jwt.StandardClaims {
-	return &jwt.StandardClaims{
-		ExpiresAt: t.Expiry.Unix(),
-		IssuedAt:  t.IssuedAt.Unix(),
-		Issuer:    t.Issuer,
-		Subject:   t.Subject,
-	}
-}
-
-type ClaimsFromOIDCTokenFunc func(ctx context.Context, t *oidc.IDToken) (*Claims, error)
-
-// Extract claims from an OIDC token and return Clutch's standard claims object. This could be configurable at a later
-// date to support subjects with IDs other than email (e.g. GitHub ID).
-func DefaultClaimsFromOIDCToken(ctx context.Context, t *oidc.IDToken) (*Claims, error) {
-	idc := &idClaims{}
-	if err := t.Claims(idc); err != nil {
-		return nil, err
-	}
-	if idc.Email == "" {
-		return nil, errors.New("claims did not deserialize with desired fields")
-	}
-
-	sc := oidcTokenToStandardClaims(t)
-	sc.Subject = idc.Email
-	return &Claims{
-		StandardClaims: sc,
-		Groups:         []string{""},
-	}, nil
-}
-
-func (p *OIDCProvider) Verify(ctx context.Context, rawToken string) (*Claims, error) {
-	claims := &Claims{}
-	_, err := jwt.ParseWithClaims(rawToken, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(p.sessionSecret), nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if err := claims.Valid(); err != nil {
-		return nil, err
-	}
-
-	return claims, nil
-}
-
-// Evaluates what flows the provider claims to support.
-type oidcProviderClaims struct {
-	GrantTypesSupported []string `json:"grant_types_supported"`
-}
-
-func (pc *oidcProviderClaims) Check(grantType string) error {
-	for _, gt := range pc.GrantTypesSupported {
-		if gt == grantType {
-			return nil
-		}
-	}
-	return fmt.Errorf("grant type '%s' not supported by provider. supported: %v", grantType, pc.GrantTypesSupported)
-}
-
-func NewProvider(config *authnv1.Config) (Provider, error) {
-	c := config.GetOidc()
-
-	httpClient := &http.Client{}
-	ctx := oidc.ClientContext(context.Background(), httpClient)
-	provider, err := oidc.NewProvider(ctx, c.Issuer)
-	if err != nil {
-		return nil, err
-	}
-
-	verifier := provider.Verifier(&oidc.Config{
-		ClientID: c.ClientId,
-	})
-
-	oc := &oauth2.Config{
-		ClientID:     c.ClientId,
-		ClientSecret: c.ClientSecret,
-		Endpoint:     provider.Endpoint(),
-		RedirectURL:  c.RedirectUrl,
-		Scopes:       scopes,
-	}
-
-	// Verify the provider implements the same flow we do.
-	pClaims := &oidcProviderClaims{}
-	if err := provider.Claims(&pClaims); err != nil {
-		return nil, err
-	}
-	if err := pClaims.Check("authorization_code"); err != nil {
-		return nil, err
-	}
-
-	p := &OIDCProvider{
-		provider:            provider,
-		verifier:            verifier,
-		oauth2:              oc,
-		httpClient:          httpClient,
-		sessionSecret:       config.SessionSecret,
-		claimsFromOIDCToken: DefaultClaimsFromOIDCToken,
-	}
-
-	if config.Storage != nil {
-		c, err := newCryptographer(config.Storage.EncryptionPassphrase)
-		if err != nil {
-			return nil, err
-		}
-		p.cryptographer = c
-	}
-
-	return p, nil
 }
---
+++ backend/service/authn/oidc_test.go
@@ -0,0 +1,105 @@
+package authn
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+
+	authnv1 "github.com/lyft/clutch/backend/api/config/service/authn/v1"
+	apimock "github.com/lyft/clutch/backend/mock/api"
+	"github.com/lyft/clutch/backend/mock/service/authnmock"
+)
+
+func TestStateNonceRoundTrip(t *testing.T) {
+	p := &OIDCProvider{
+		sessionSecret: "this-is-my-secret",
+	}
+
+	url := "/foo"
+
+	state, err := p.GetStateNonce(url)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, state)
+
+	outUrl, err := p.ValidateStateNonce(state)
+	assert.NoError(t, err)
+	assert.Equal(t, url, outUrl)
+
+	// Check that the same fails if not signed correctly.
+	p2 := &OIDCProvider{
+		sessionSecret: "this-is-a-different-secret",
+	}
+	_, err = p2.ValidateStateNonce(state)
+	assert.Error(t, err)
+}
+
+func TestStateNoncePrependsLeadingSlash(t *testing.T) {
+	p := &OIDCProvider{
+		sessionSecret: "this-is-my-secret",
+	}
+	s, err := p.GetStateNonce("dest/foo")
+	assert.NoError(t, err)
+	u, _ := p.ValidateStateNonce(s)
+	assert.Equal(t, "/dest/foo", u)
+}
+
+func TestStateNonceRejections(t *testing.T) {
+	p := &OIDCProvider{
+		sessionSecret: "this-is-my-secret",
+	}
+
+	tests := []string{
+		"unix:///tmp/redis.sock",
+		"https://google.com",
+		"http://google.com",
+		"123%45%6",
+	}
+
+	for _, tt := range tests {
+		tt := tt // pin!
+		t.Run(tt, func(t *testing.T) {
+			_, err := p.GetStateNonce(tt)
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestNewOIDCProvider(t *testing.T) {
+	cfg := &authnv1.Config{}
+	apimock.FromYAML(`
+session_secret: this_is_my_secret
+oidc:
+  issuer: http://foo.example.com
+  client_id: my_client_id
+  client_secret: my_client_secret
+  redirect_url: "http://localhost:12000/v1/authn/callback"
+`, cfg)
+
+	email := "user@example.com"
+
+	mockprovider := authnmock.NewMockOIDCProviderServer(email)
+	defer mockprovider.Close()
+
+	ctx := context.WithValue(context.Background(), oauth2.HTTPClient, mockprovider.Client())
+	p, err := NewOIDCProvider(ctx, cfg)
+	assert.NoError(t, err)
+	assert.NotNil(t, p)
+
+	authURL, err := p.GetAuthCodeURL(context.Background(), "myState")
+	assert.NoError(t, err)
+	assert.True(t, strings.HasPrefix(authURL, "http://foo.example.com/oauth2/v1/authorize"))
+	assert.True(t, strings.Contains(authURL, "access_type=offline"))
+	assert.True(t, strings.Contains(authURL, "state=myState"))
+
+	token, err := p.Exchange(context.Background(), "aaa")
+	assert.NoError(t, err)
+	assert.NotNil(t, token)
+
+	c, err := p.Verify(context.Background(), token.AccessToken)
+	assert.NoError(t, err)
+	assert.NotNil(t, c)
+	assert.Equal(t, email, c.Subject)
+}
