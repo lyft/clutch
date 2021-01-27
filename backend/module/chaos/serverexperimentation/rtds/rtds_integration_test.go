e65d3431af8002e450a4a78d9ffbad256af47864
Snow Pettersen <snowp@lyft.com>
2021-01-27 15:18:50 -0500

    extract helpers, add TTL test

--- backend/module/chaos/serverexperimentation/rtds/rtds_integration_test.go
+++ backend/module/chaos/serverexperimentation/rtds/rtds_integration_test.go
@@ -2,6 +2,7 @@ package rtds
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -11,9 +12,58 @@ import (
 
 	serverexperimentation "github.com/lyft/clutch/backend/api/chaos/serverexperimentation/v1"
 	"github.com/lyft/clutch/backend/module/chaos/serverexperimentation/rtds/envoy"
+	rtds_testing "github.com/lyft/clutch/backend/module/chaos/serverexperimentation/rtds/testing"
 )
 
-const FaultFilterConfig = `
+func TestEnvoyFaults(t *testing.T) {
+	ts := newTestServer(t, false)
+	defer ts.stop()
+
+	e := startEnvoy(t)
+	defer e.stop()
+
+	code, err := e.MakeSimpleCall()
+	assert.NoError(t, err)
+	assert.Equal(t, 503, code)
+
+	createTestExperiment(t, ts.storer)
+
+	err = awaitExpectedReturnValueForSimpleCall(t, e.Envoy, awaitReturnValueParams{
+		timeout:        2 * time.Second,
+		expectedStatus: 400,
+	})
+	assert.NoError(t, err, "did not see faults enabled")
+}
+
+func TestTTls(t *testing.T) {
+	ts := newTestServer(t, false)
+
+	e := startEnvoy(t)
+	defer e.stop()
+
+	code, err := e.MakeSimpleCall()
+	assert.NoError(t, err)
+	assert.Equal(t, 503, code)
+
+	createTestExperiment(t, ts.storer)
+
+	err = awaitExpectedReturnValueForSimpleCall(t, e.Envoy, awaitReturnValueParams{
+		timeout:        2 * time.Second,
+		expectedStatus: 400,
+	})
+	assert.NoError(t, err, "did not see faults enabled")
+
+	// We know that faults have been applied, now try to kill the server and ensure that we eventually reset the faults.
+	ts.stop()
+
+	err = awaitExpectedReturnValueForSimpleCall(t, e.Envoy, awaitReturnValueParams{
+		timeout:        10 * time.Second,
+		expectedStatus: 503,
+	})
+	assert.NoError(t, err, "did not see faults reverted")
+}
+
+const faultFilterConfig = `
 name: envoy.fault
 typed_config:
   '@type': type.googleapis.com/envoy.extensions.filters.http.fault.v3.HTTPFault
@@ -25,7 +75,7 @@ typed_config:
     percentage: {denominator: HUNDRED, numerator: 0}
 `
 
-const RtdsLayerConfig = `
+const rtdsLayerConfig = `
 name: rtds
 rtds_layer:
   name: rtds
@@ -39,7 +89,7 @@ rtds_layer:
           cluster_name: clutchxds
 `
 
-const RtdsCluster = `
+const rtdsCluster = `
 connect_timeout: 0.250s
 http2_protocol_options: {}
 lb_policy: ROUND_ROBIN
@@ -56,29 +106,36 @@ name: clutchxds
 type: STRICT_DNS
 `
 
-func TestEnvoyFaults(t *testing.T) {
-	ts := newTestServer(t, false)
-	defer ts.stop()
+type rtdsEnvoy struct {
+	stop func()
 
+	envoy.Envoy
+}
+
+func startEnvoy(t *testing.T) *rtdsEnvoy {
 	e := envoy.NewEnvoy(t)
-	err := e.AddHTTPFilter(FaultFilterConfig)
+	err := e.AddHTTPFilter(faultFilterConfig)
 	assert.NoError(t, err)
 
-	err = e.AddCluster(RtdsCluster)
+	err = e.AddCluster(rtdsCluster)
 	assert.NoError(t, err)
 
-	err = e.AddRuntimeLayer(RtdsLayerConfig)
+	err = e.AddRuntimeLayer(rtdsLayerConfig)
 	assert.NoError(t, err)
 
 	err = e.Start()
 	assert.NoError(t, err)
-	defer e.AwaitShutdown()
-	defer e.Stop(t)
 
-	code, err := e.MakeSimpleCall()
-	assert.NoError(t, err)
-	assert.Equal(t, 503, code)
+	return &rtdsEnvoy{
+		stop: func() {
+			e.Stop(t)
+			e.AwaitShutdown()
+		},
+		Envoy: *e,
+	}
+}
 
+func createTestExperiment(t *testing.T, storer *rtds_testing.SimpleStorer) {
 	now := time.Now()
 	config := serverexperimentation.HTTPFaultConfig{
 		Fault: &serverexperimentation.HTTPFaultConfig_AbortFault{
@@ -112,22 +169,30 @@ func TestEnvoyFaults(t *testing.T) {
 	a, err := ptypes.MarshalAny(&config)
 	assert.NoError(t, err)
 
-	_, err = ts.storer.CreateExperiment(context.Background(), a, &now, &now)
+	_, err = storer.CreateExperiment(context.Background(), a, &now, &now)
 	assert.NoError(t, err)
+}
 
-	timeout := time.NewTimer(2 * time.Second)
+type awaitReturnValueParams struct {
+	timeout        time.Duration
+	expectedStatus int
+}
+
+func awaitExpectedReturnValueForSimpleCall(t *testing.T, e envoy.Envoy, params awaitReturnValueParams) error {
+	timeout := time.NewTimer(params.timeout)
 
 	for range time.NewTicker(100 * time.Millisecond).C {
 		select {
 		case <-timeout.C:
-			t.Errorf("timed out waiting for faults to take effect")
-			break
+			return fmt.Errorf("did not receive expected status code before timeout")
 		default:
 		}
 
-		code, err = e.MakeSimpleCall()
-		if err == nil && code == 400 {
-			break
+		code, err := e.MakeSimpleCall()
+		if err == nil && code == params.expectedStatus {
+			return nil
 		}
 	}
+
+	return nil
 }
