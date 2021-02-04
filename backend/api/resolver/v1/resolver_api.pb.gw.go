6fa5c50e792a49e88652f45a1666b74aebc042bc
Mike Cutalo <mcutalo88@gmail.com>
2021-02-04 12:38:14 -0800

    resolver autocomplete protos

--- backend/api/resolver/v1/resolver_api.pb.gw.go
+++ backend/api/resolver/v1/resolver_api.pb.gw.go
@@ -133,6 +133,40 @@ func local_request_ResolverAPI_Resolve_0(ctx context.Context, marshaler runtime.
 
 }
 
+func request_ResolverAPI_AutoComplete_0(ctx context.Context, marshaler runtime.Marshaler, client ResolverAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AutoCompleteRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.AutoComplete(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_ResolverAPI_AutoComplete_0(ctx context.Context, marshaler runtime.Marshaler, server ResolverAPIServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq AutoCompleteRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.AutoComplete(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterResolverAPIHandlerServer registers the http handlers for service ResolverAPI to "mux".
 // UnaryRPC     :call ResolverAPIServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -205,6 +239,29 @@ func RegisterResolverAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		}
 
 		forward_ResolverAPI_Resolve_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_ResolverAPI_AutoComplete_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/clutch.resolver.v1.ResolverAPI/AutoComplete")
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_ResolverAPI_AutoComplete_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ResolverAPI_AutoComplete_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -309,6 +366,26 @@ func RegisterResolverAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_ResolverAPI_AutoComplete_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/clutch.resolver.v1.ResolverAPI/AutoComplete")
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ResolverAPI_AutoComplete_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ResolverAPI_AutoComplete_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -318,6 +395,8 @@ var (
 	pattern_ResolverAPI_Search_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "resolver", "search"}, ""))
 
 	pattern_ResolverAPI_Resolve_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "resolver", "resolve"}, ""))
+
+	pattern_ResolverAPI_AutoComplete_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "resolver", "autocomplete"}, ""))
 )
 
 var (
@@ -326,4 +405,6 @@ var (
 	forward_ResolverAPI_Search_0 = runtime.ForwardResponseMessage
 
 	forward_ResolverAPI_Resolve_0 = runtime.ForwardResponseMessage
+
+	forward_ResolverAPI_AutoComplete_0 = runtime.ForwardResponseMessage
 )
