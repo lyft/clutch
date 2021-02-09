efd9767ab7ec4b8e663414f13b5868c6b976a976
Scarlett Perry <sperry@lyft.com>
2021-02-09 16:40:58 -0500

    log for grpc status

--- backend/gateway/log/log.go
+++ backend/gateway/log/log.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 
 	"go.uber.org/zap"
+	spb "google.golang.org/genproto/googleapis/rpc/status"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
@@ -14,5 +16,25 @@ func ProtoField(key string, m proto.Message) zap.Field {
 	if err != nil {
 		return zap.Any(key, m)
 	}
+	return zap.Any(key, json.RawMessage(b))
+}
+
+// GrpcStatusField takes in an error and returns a zap field that logs as the embedded JSON
+// representation of a gRPC Status message.
+func GrpcStatusField(key string, err error) zap.Field {
+	var m *spb.Status
+	// the second return val is a bool reporting whether it was able to
+	// convert the input error to a Status. If false, FromError returns a new Status with
+	// codes.Unknown and the original input error message.
+	// https: //github.com/grpc/grpc-go/blob/master/status/status.go#L81
+	s, _ := status.FromError(err)
+
+	m = s.Proto()
+
+	b, err := protojson.Marshal(m)
+	if err != nil {
+		return zap.Any(key, m)
+	}
+
 	return zap.Any(key, json.RawMessage(b))
 }
--- backend/gateway/log/log_test.go
+++ backend/gateway/log/log_test.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	healthcheckv1 "github.com/lyft/clutch/backend/api/healthcheck/v1"
 )
@@ -32,4 +37,66 @@ func TestProtoField(t *testing.T) {
 	o := make(map[string]interface{})
 	assert.NoError(t, json.Unmarshal(b.Bytes(), &o))
 	assert.Contains(t, b.String(), `{"@type":"type.googleapis.com/clutch.healthcheck.v1.HealthcheckRequest"}`)
+}
+
+func TestGrpcStatusField(t *testing.T) {
+	// a Status with no detailed appended
+	s1 := status.New(codes.PermissionDenied, "Permission denied")
+	err1 := s1.Err()
+
+	// a Status with details appended
+	s2 := status.New(codes.NotFound, "Resource not found")
+	s2, _ = s2.WithDetails(
+		&errdetails.ResourceInfo{ResourceType: "ConfigMap", Description: "configMap-test-1 not found"},
+		&errdetails.ResourceInfo{ResourceType: "ConfigMap", Description: "configMap-test-2 not found"},
+	)
+	err2 := s2.Err()
+
+	tests := []struct {
+		err             error
+		expectedCode    int
+		expectedMsg     string
+		expectedDetails []string
+	}{
+		{
+			err:          errors.New("I am a regular error message"),
+			expectedMsg:  "I am a regular error message",
+			expectedCode: 2,
+		},
+		{
+			err:          err1,
+			expectedMsg:  "Permission denied",
+			expectedCode: 7,
+		},
+		{
+			err:             err2,
+			expectedCode:    5,
+			expectedMsg:     "Resource not found",
+			expectedDetails: []string{"configMap-test-1 not found", "configMap-test-2 not found"},
+		},
+	}
+
+	for _, test := range tests {
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
+
+		logger := zap.New(
+			zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()), zapcore.AddSync(w), zap.DebugLevel),
+		)
+		logger.Info("test", GrpcStatusField("key", test.err))
+		assert.NoError(t, logger.Sync())
+		assert.NoError(t, w.Flush())
+
+		o := make(map[string]interface{})
+		assert.NoError(t, json.Unmarshal(b.Bytes(), &o))
+
+		assert.Contains(t, b.String(), strconv.Itoa(test.expectedCode))
+		assert.Contains(t, b.String(), test.expectedMsg)
+
+		if test.expectedDetails != nil {
+			for _, detail := range test.expectedDetails {
+				assert.Contains(t, b.String(), detail)
+			}
+		}
+	}
 }
