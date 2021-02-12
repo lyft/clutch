e2ded35fd8be4661f5736bf2baf2f41aaf37b3e2
Derek <dschaller@users.noreply.github.com>
2021-02-12 10:33:51 -0800

    sourcecontrol: add support for dynamic repository options (#1023)

--- backend/api/sourcecontrol/v1/sourcecontrol.pb.gw.go
+++ backend/api/sourcecontrol/v1/sourcecontrol.pb.gw.go
@@ -31,6 +31,40 @@ var _ = runtime.String
 var _ = utilities.NewDoubleArray
 var _ = metadata.Join
 
+func request_SourceControlAPI_GetRepositoryOptions_0(ctx context.Context, marshaler runtime.Marshaler, client SourceControlAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetRepositoryOptionsRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetRepositoryOptions(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_SourceControlAPI_GetRepositoryOptions_0(ctx context.Context, marshaler runtime.Marshaler, server SourceControlAPIServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetRepositoryOptionsRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetRepositoryOptions(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_SourceControlAPI_CreateRepository_0(ctx context.Context, marshaler runtime.Marshaler, client SourceControlAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateRepositoryRequest
 	var metadata runtime.ServerMetadata
@@ -70,6 +104,29 @@ func local_request_SourceControlAPI_CreateRepository_0(ctx context.Context, mars
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterSourceControlAPIHandlerFromEndpoint instead.
 func RegisterSourceControlAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, server SourceControlAPIServer) error {
+
+	mux.Handle("POST", pattern_SourceControlAPI_GetRepositoryOptions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/clutch.sourcecontrol.v1.SourceControlAPI/GetRepositoryOptions")
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_SourceControlAPI_GetRepositoryOptions_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SourceControlAPI_GetRepositoryOptions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
 
 	mux.Handle("POST", pattern_SourceControlAPI_CreateRepository_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
@@ -135,6 +192,26 @@ func RegisterSourceControlAPIHandler(ctx context.Context, mux *runtime.ServeMux,
 // "SourceControlAPIClient" to call the correct interceptors.
 func RegisterSourceControlAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux, client SourceControlAPIClient) error {
 
+	mux.Handle("POST", pattern_SourceControlAPI_GetRepositoryOptions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/clutch.sourcecontrol.v1.SourceControlAPI/GetRepositoryOptions")
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SourceControlAPI_GetRepositoryOptions_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SourceControlAPI_GetRepositoryOptions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_SourceControlAPI_CreateRepository_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -159,9 +236,13 @@ func RegisterSourceControlAPIHandlerClient(ctx context.Context, mux *runtime.Ser
 }
 
 var (
+	pattern_SourceControlAPI_GetRepositoryOptions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "sourcecontrol", "getRepositoryOptions"}, ""))
+
 	pattern_SourceControlAPI_CreateRepository_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "sourcecontrol", "createRepository"}, ""))
 )
 
 var (
+	forward_SourceControlAPI_GetRepositoryOptions_0 = runtime.ForwardResponseMessage
+
 	forward_SourceControlAPI_CreateRepository_0 = runtime.ForwardResponseMessage
 )
