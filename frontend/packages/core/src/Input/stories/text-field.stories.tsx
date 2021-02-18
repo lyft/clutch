0a227ad7f0e98755b1581c2b7adab0ed747ab384
Mike Cutalo <mcutalo88@gmail.com>
2021-02-18 09:02:01 -0800

    storybook

--- frontend/packages/core/src/Input/stories/text-field.stories.tsx
+++ frontend/packages/core/src/Input/stories/text-field.stories.tsx
@@ -50,3 +50,22 @@ WithEndAdornment.args = {
   defaultValue: "Search",
   endAdornment: <SearchIcon />,
 };
+
+const autoComplete = value => {
+  return new Promise((resolve, reject) => {
+    resolve({
+      results: [
+        { id: "clutch", label: "" },
+        { id: "clutch-auto", label: "" },
+        { id: "clutch-autocomplete", label: "" },
+      ],
+    });
+    reject(new Error("Something bad happened"));
+  });
+};
+
+export const Autocomplete = Template.bind({});
+Autocomplete.args = {
+  ...Primary.args,
+  autocompleteCallback: autoComplete,
+};
