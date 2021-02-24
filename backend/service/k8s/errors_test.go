f9bd00bf39326a04e4d7700fd86512f2a7c2c3b2
Daniel Hochman <danielhochman@users.noreply.github.com>
2021-02-24 12:34:18 -0600

    more tests and mappings

--- backend/service/k8s/errors_test.go
+++ backend/service/k8s/errors_test.go
@@ -1,20 +1,23 @@
 package k8s
 
 import (
+	"errors"
 	"testing"
-
-	"github.com/lyft/clutch/backend/middleware/unaryerror"
 
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+
+	"github.com/lyft/clutch/backend/middleware/unaryerror"
 )
 
-func TestConvertError(t *testing.T) {
-	service := &svc{}
+func TestImplementsInterceptorInterface(t *testing.T) {
+	assert.Implements(t, (*unaryerror.Interceptor)(nil), (*svc)(nil))
+}
 
-	assert.Implements(t, (*unaryerror.Interceptor)(nil), service)
+func TestInterceptError(t *testing.T) {
+	service := &svc{}
 
 	err := k8serrors.NewUnauthorized("nice try")
 	newErr := service.InterceptError(err)
@@ -24,4 +27,47 @@ func TestConvertError(t *testing.T) {
 	assert.NotNil(t, s)
 	assert.Equal(t, codes.Unauthenticated, s.Code())
 	assert.Equal(t, "nice try", s.Message())
+}
+
+func TestConvertError(t *testing.T) {
+	testCases := []struct {
+		err          error
+		expectStatus bool
+		expectedCode codes.Code
+	}{
+		{
+			err: errors.New("plain error"),
+		},
+		{
+			err:          k8serrors.NewBadRequest("bad request"),
+			expectStatus: true,
+			expectedCode: codes.FailedPrecondition,
+		},
+		{
+			err:          k8serrors.NewTimeoutError("timeout", 3),
+			expectStatus: true,
+			expectedCode: codes.DeadlineExceeded,
+		},
+		{
+			err:          k8serrors.NewResourceExpired("expired"),
+			expectStatus: true,
+			expectedCode: codes.Unknown,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.err.Error(), func(t *testing.T) {
+			result := ConvertError(tc.err)
+
+			s, ok := status.FromError(result)
+			assert.Equal(t, tc.expectStatus, ok)
+			if tc.expectStatus {
+				assert.Equal(t, tc.expectedCode, s.Code())
+				assert.Contains(t, result.Error(), s.Message())
+			} else {
+				assert.Equal(t, tc.err, result)
+			}
+		})
+	}
 }
