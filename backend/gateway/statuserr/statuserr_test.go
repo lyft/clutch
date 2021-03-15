13d93d68adb965acd83a735ffa5fd4f6adb29cd5
Daniel Hochman <danielhochman@users.noreply.github.com>
2021-03-15 11:36:29 -0500

    dont use expansion its err prone

--- backend/gateway/statuserr/statuserr_test.go
+++ backend/gateway/statuserr/statuserr_test.go
@@ -55,7 +55,7 @@ func TestAllProtoCodesMatch(t *testing.T) {
 	for _, c := range cases {
 		c := c
 		t.Run(c.name, func(t *testing.T) {
-			assert.Equal(t, c.expect, AllProtoCodesMatch(c.code, c.status...))
+			assert.Equal(t, c.expect, AllProtoCodesMatch(c.code, c.status))
 		})
 	}
 }
