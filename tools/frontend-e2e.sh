c4657e82f663d131dfe84dacb0d26a41a2edd66c
Derek Schaller <d_a_schaller@yahoo.com>
2021-03-17 22:24:36 -0700

    test

--- tools/frontend-e2e.sh
+++ tools/frontend-e2e.sh
@@ -10,20 +10,20 @@ STARTUP_WAIT=30
 make backend-dev-mock &
 yarn --cwd frontend workspace @clutch-sh/app start &
 until curl --output /dev/null --silent --head --fail http://localhost:3000; do
-    if [ "$FE_STARTUP_COUNT" -ge "$STARTUP_WAIT" ]; then
-        echo "Error: could not start frontend dev server"
-        exit 1
-    fi;
-    ((FE_STARTUP_COUNT++))
+    # if [ "$FE_STARTUP_COUNT" -ge "$STARTUP_WAIT" ]; then
+    #     echo "Error: could not start frontend dev server"
+    #     exit 1
+    # fi;
+    # ((FE_STARTUP_COUNT++))
     sleep 1
 done
 
 until curl --output /dev/null --silent --fail http://localhost:8080/healthcheck; do
-    if [ "$BE_STARTUP_COUNT" -ge "$STARTUP_WAIT" ]; then
-        echo "Error: could not start backend mock server"
-        exit 1
-    fi;
-    ((BE_STARTUP_COUNT++))
+    # if [ "$BE_STARTUP_COUNT" -ge "$STARTUP_WAIT" ]; then
+    #     echo "Error: could not start backend mock server"
+    #     exit 1
+    # fi;
+    # ((BE_STARTUP_COUNT++))
     sleep 1
 done
 (
