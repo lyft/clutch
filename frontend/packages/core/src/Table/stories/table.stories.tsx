75cefe601f93667e56b6964a7aa4c4f1d21a8fdd
Derek Schaller <d_a_schaller@yahoo.com>
2021-03-22 12:40:07 -0700

    frontend: fix default table cell value in TableRow

--- frontend/packages/core/src/Table/stories/table.stories.tsx
+++ frontend/packages/core/src/Table/stories/table.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import type { Meta } from "@storybook/react";
 
-import type { TableProps } from "../table";
+import type { TableProps, TableRowProps } from "../table";
 import { Table, TableRow } from "../table";
 
 export default {
@@ -9,23 +9,48 @@ export default {
   component: Table,
 } as Meta;
 
-const Template = (props: TableProps) => (
+const Template = ({ row, ...props }: TableProps & { row: React.ReactElement }) => (
   <div style={{ maxHeight: "300px", display: "flex" }}>
     <Table headings={["Column 1", "Column 2"]} {...props}>
-      {[...Array(10)].map((_, index: number) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <TableRow key={index}>
-          <div>Value 1</div>
-          <div>Value 2</div>
-        </TableRow>
-      ))}
+      {[...Array(10)].map((_, index: number) => React.cloneElement(row, { key: index }))}
     </Table>
   </div>
 );
 
+const PrimaryTableRow = (props: TableRowProps) => (
+  <TableRow {...props}>
+    <div>Value 1</div>
+    <div>Value 2</div>
+  </TableRow>
+);
+
+const IncompleteTableRow = (props: TableRowProps) => {
+  let data;
+  return (
+    <TableRow {...props}>
+      <div>Value 1</div>
+      {data}
+    </TableRow>
+  );
+};
+
 export const Primary = Template.bind({});
+Primary.args = {
+  row: <PrimaryTableRow />,
+};
 
 export const StickyHeader = Template.bind({});
 StickyHeader.args = {
   stickyHeader: true,
+  row: <PrimaryTableRow />,
+};
+
+export const MissingCellValue = Template.bind({});
+MissingCellValue.args = {
+  row: <IncompleteTableRow />,
+};
+
+export const DefaultCellValue = Template.bind({});
+DefaultCellValue.args = {
+  row: <IncompleteTableRow defaultCellValue="null" />,
 };
