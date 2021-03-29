f4a20ee10e509948a1849066e4c782adf494d350
Mike Cutalo <mcutalo88@gmail.com>
2021-03-29 11:42:42 -0700

    cleanup

--- backend/resolver/core/core_test.go
+++ backend/resolver/core/core_test.go
@@ -11,15 +11,15 @@ import (
 	"github.com/lyft/clutch/backend/service"
 )
 
-func TestNewAwsResolver(t *testing.T) {
+func TestNewCoreResolver(t *testing.T) {
 	service.Registry["clutch.service.topology"] = topologymock.New()
 
 	log := zaptest.NewLogger(t)
 	scope := tally.NewTestScope("", nil)
 
-	aws, err := New(nil, log, scope)
+	core, err := New(nil, log, scope)
 	assert.NoError(t, err)
-	assert.NotNil(t, aws)
+	assert.NotNil(t, core)
 
 	// Test successful construction without the topology service
 	delete(service.Registry, "clutch.service.topology")
