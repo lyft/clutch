3b312caf6478dcf93d31eba50ef067f440f879cb
Mike Cutalo <mcutalo88@gmail.com>
2021-03-30 10:42:44 -0700

    resolver: autocomplete for core resources (#1198)

---
+++ backend/resolver/core/core_test.go
@@ -0,0 +1,29 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/uber-go/tally"
+	"go.uber.org/zap/zaptest"
+
+	"github.com/lyft/clutch/backend/mock/service/topologymock"
+	"github.com/lyft/clutch/backend/service"
+)
+
+func TestNewCoreResolver(t *testing.T) {
+	service.Registry["clutch.service.topology"] = topologymock.New()
+
+	log := zaptest.NewLogger(t)
+	scope := tally.NewTestScope("", nil)
+
+	core, err := New(nil, log, scope)
+	assert.NoError(t, err)
+	assert.NotNil(t, core)
+
+	// Test successful construction without the topology service
+	delete(service.Registry, "clutch.service.topology")
+	coreNoTopology, err2 := New(nil, log, scope)
+	assert.NoError(t, err2)
+	assert.NotNil(t, coreNoTopology)
+}
