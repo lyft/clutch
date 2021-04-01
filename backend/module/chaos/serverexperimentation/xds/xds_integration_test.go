dc25d9a5c7606f127ab94e6806454d71514a1ee4
Snow Pettersen <snowp@lyft.com>
2021-04-01 14:03:34 -0400

    bump timeouts to reduce flakes

--- backend/module/chaos/serverexperimentation/xds/xds_integration_test.go
+++ backend/module/chaos/serverexperimentation/xds/xds_integration_test.go
@@ -53,7 +53,7 @@ func TestEnvoyFaults(t *testing.T) {
 	createTestExperiment(t, 400, ts.Storer)
 
 	err = awaitExpectedReturnValueForSimpleCall(t, e, awaitReturnValueParams{
-		timeout:        2 * time.Second,
+		timeout:        4 * time.Second,
 		expectedStatus: 400,
 	})
 	assert.NoError(t, err, "did not see faults enabled")
@@ -121,7 +121,7 @@ func TestEnvoyFaultsTimeBasedTermination(t *testing.T) {
 	createTestExperiment(t, 400, ts.Storer)
 
 	err = awaitExpectedReturnValueForSimpleCall(t, e, awaitReturnValueParams{
-		timeout:        3 * time.Second,
+		timeout:        4 * time.Second,
 		expectedStatus: 400,
 	})
 	assert.NoError(t, err, "did not see faults enabled")
@@ -161,7 +161,7 @@ func TestEnvoyECDSFaults(t *testing.T) {
 	experiment := createTestExperiment(t, 404, ts.Storer)
 
 	err = awaitExpectedReturnValueForSimpleCall(t, e, awaitReturnValueParams{
-		timeout:        2 * time.Second,
+		timeout:        4 * time.Second,
 		expectedStatus: 404,
 	})
 	assert.NoError(t, err, "did not see faults enabled")
