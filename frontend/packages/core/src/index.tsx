ba0f49614b2992ffcc579d14d5b489c9eedaf6fe
Andrew Oh (andrewo) <misteroh@users.noreply.github.com>
2021-04-05 17:07:39 -0700

    lint fix

--- frontend/packages/core/src/index.tsx
+++ frontend/packages/core/src/index.tsx
@@ -7,7 +7,13 @@ import RadioGroup from "./Input/radio-group";
 import Select from "./Input/select";
 import Switch from "./Input/switchToggle";
 import TextField from "./Input/text-field";
-import { Accordion, AccordionActions, AccordionDetails, AccordionDivider, AccordionGroup } from "./accordion";
+import {
+  Accordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionDivider,
+  AccordionGroup,
+} from "./accordion";
 import ClutchApp from "./AppProvider";
 import { Button, ButtonGroup, ClipboardButton, IconButton } from "./button";
 import Confirmation from "./confirmation";
