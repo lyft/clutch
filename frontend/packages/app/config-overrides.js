527c046c8c1fc442c06c1342e965c34af0ee2571
Derek Schaller <d_a_schaller@yahoo.com>
2021-04-12 15:47:22 -0700

    frontend: run esbuild on all source

--- frontend/packages/app/config-overrides.js
+++ frontend/packages/app/config-overrides.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
-module.exports = {
-  // The Webpack config to use when compiling your react app for development or production.
-  webpack(config) {
-    config.module.rules.unshift({
+module.exports = function override(config, env) {
+    console.log('override!')
+    let loaders = config.module.rules[1].oneOf;
+    loaders.splice(2, 0, {
       test: /\.(js|mjs|jsx|ts|tsx)$/,
-      include: [path.join(__dirname, "src")],
+      exclude: /.*node_modules.*/,
       use: [
         {
           loader: "esbuild-loader",
@@ -17,20 +17,4 @@ module.exports = {
       ],
     });
     return config;
-  },
-  // The Jest config to use when running your jest tests - note that the normal rewires do not
-  // work here.
-  jest(config) {
-    return config;
-  },
-  // The function to use to create a webpack dev server configuration when running the development
-  // server with 'npm run start' or 'yarn start'.
-  // Example: set the dev server to use a specific certificate in https.
-  devServer(configFunction) {
-    return configFunction;
-  },
-  // The paths config to use when compiling your react app for development or production.
-  paths(paths) {
-    return paths;
-  },
-};
+  };
