68708d6325da96884b0a2018e8672c9e35d511f3
Rafal Augustyniak <raugustyniak@lyft.com>
2021-04-15 15:43:30 -0700

    make api

--- backend/api/authn/v1/authn.pb.gw.go
+++ backend/api/authn/v1/authn.pb.gw.go
@@ -103,18 +103,15 @@ func local_request_AuthnAPI_Callback_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
-var (
-	filter_AuthnAPI_CreateToken_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_AuthnAPI_CreateToken_0(ctx context.Context, marshaler runtime.Marshaler, client AuthnAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateTokenRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AuthnAPI_CreateToken_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -127,10 +124,11 @@ func local_request_AuthnAPI_CreateToken_0(ctx context.Context, marshaler runtime
 	var protoReq CreateTokenRequest
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AuthnAPI_CreateToken_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
