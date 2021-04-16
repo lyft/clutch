ab90f17359c98259aa15aa2f00fb60e6661e2aa0
SHAWNA MONERO <smonero@lyft.com>
2021-04-15 18:20:40 -0700

    chip

---
+++ frontend/packages/core/src/chip.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import styled from "@emotion/styled";
+import type { ChipProps as MuiChipProps } from "@material-ui/core";
+import { Chip as MuiChip } from "@material-ui/core";
+
+type variants = "error" | "unknown" | "pending" | "running" | "succeeded";
+export interface ChipProps extends Pick<MuiChipProps, "label"> {
+  variant: variants;
+}
+
+const CHIP_COLOR_MAP = {
+  error: "#FF8A80",
+  unknown: "#FFCC80",
+  pending: "#FFF59D",
+  running: "#C2C8F2",
+  succeeded: "#69F0AE",
+};
+
+const StyledChip = styled(MuiChip)({
+  height: "20px",
+  fontSize: "12px",
+  lineHeight: "20px",
+  cursor: "inherit",
+  margin: "10px",
+});
+
+const ClutchChip = ({ variant, ...props }: ChipProps) => {
+  const styleOverride = { background: CHIP_COLOR_MAP[variant] };
+  return <StyledChip {...props } style={styleOverride} />;
+};
+
+export default ClutchChip;
