bceca9bc544c1692cc158e7cad667c19766499f2
Snow Pettersen <snowp@lyft.com>
2021-04-28 08:27:36 -0400

    comments

--- backend/internal/test/integration/xds/do_integration_test.sh
+++ backend/internal/test/integration/xds/do_integration_test.sh
@@ -1,9 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# We expect this to be run from the directory containing the script.
 target_dir="$(pwd)"
 
+# Build the Go executable on the host system targeting linux. This is much faster
+# than naively building within the container setup since we get to share the host Go
+# caches.
 export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
 
+# Place the output binaries in $target_dir for use by the Dockerfiles.
 pushd ../../../..
 	pushd internal/test/integration/xds/cmd/envoyconfiggen
 	  go build -o $target_dir/envoyconfiggen main.go
