d3953155c406942c5293c41841f90ee8d9c239a4
SHAWNA MONERO <smonero@lyft.com>
2021-04-28 19:04:28 -0700

    add enableAutocomplete val

--- frontend/packages/core/src/Resolver/input.tsx
+++ frontend/packages/core/src/Resolver/input.tsx
@@ -30,6 +30,7 @@ interface QueryResolverProps {
   inputType: string;
   schemas: clutch.resolver.v1.Schema[];
   submitHandler: any;
+  enableAutocomplete?: boolean;
 }
 
 const autoComplete = async (type: string, search: string): Promise<any> => {
@@ -47,7 +48,7 @@ const autoComplete = async (type: string, search: string): Promise<any> => {
   return { results: response?.data?.results || [] };
 };
 
-const QueryResolver: React.FC<QueryResolverProps> = ({ inputType, schemas, submitHandler }) => {
+const QueryResolver: React.FC<QueryResolverProps> = ({ inputType, schemas, submitHandler, enableAutocomplete = true }) => {
   const validation = useForm({
     mode: "onSubmit",
     reValidateMode: "onSubmit",
@@ -65,8 +66,9 @@ const QueryResolver: React.FC<QueryResolverProps> = ({ inputType, schemas, submi
   };
 
   // If there is at least 1 schema that has the ability to autocomplete we will enable it.
+  // enableAutocomplete's default value is true.  We only use it (set it to false) when we want to override and disable autocomplete at the workflow level rather than the schema level.
   const isAutoCompleteEnabled =
-    schemas.filter(schema => schema?.metadata?.search?.autocompleteEnabled === true).length >= 1;
+    (schemas.filter(schema => schema?.metadata?.search?.autocompleteEnabled === true).length >= 1) && enableAutocomplete;
 
   const error = validation.errors?.query;
   return (
