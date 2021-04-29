6ea61ae6fa51849e8ae1ca23bdec9d22bfa1dd45
Rafal Augustyniak <raugustyniak@lyft.com>
2021-04-29 14:54:36 -0700

    add doc strings

--- backend/internal/test/integration/chaos/do_integration_test.sh
+++ backend/internal/test/integration/chaos/do_integration_test.sh
@@ -12,15 +12,19 @@ export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
 
+# Run integration tests from a provided directory.
+#
+# @arg $1 path to an Envoy config generator file called `main.go` that should be used
+# to generate a config that's passed to an Envoy instance that is spin up as part of
+# the process of running integration tests.
+# @arg $2 path to a directory with integration tests to run.
 run_tests () {
-	envoy_config_gen=$1
-  	tests_directory=$2
 	# Place the output binaries in $target_dir for use by the Dockerfiles.
 	pushd ../../../..
-		pushd $envoy_config_gen
+		pushd $1
 	  		go build -o $target_dir/envoyconfiggen main.go
 		popd
-		pushd $tests_directory
+		pushd $2
 			go test -tags integration_only -c -o $target_dir/testrunner
 		popd
   	popd
