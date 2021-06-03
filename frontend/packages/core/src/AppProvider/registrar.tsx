816432d40bc38072b5950785e8a1587e333a131a
Derek <dschaller@users.noreply.github.com>
2021-06-03 10:12:42 -0700

    frontend: feature flags (#1459)

--- frontend/packages/core/src/AppProvider/registrar.tsx
+++ frontend/packages/core/src/AppProvider/registrar.tsx
@@ -33,18 +33,25 @@ const workflowRoutes = (
   return validRoutes;
 };
 
-const registeredWorkflows = (
+/**
+ * Determine all user registered workflows on the application and apply filters, if any.
+ * @param workflows a map of workflow keys to functions that return their configuration.
+ * @param configuration the user configuration, usually read in from the clutch.config.js file.
+ * @param filters a list of filters to apply to the user registered workflows.
+ * @returns
+ */
+const registeredWorkflows = async (
   workflows: { [key: string]: () => WorkflowConfiguration },
-  configuration: UserConfiguration
-): Workflow[] => {
-  return Object.keys(workflows || [])
+  configuration: UserConfiguration,
+  filters: { (workflows: Workflow[]): Promise<Workflow[]> }[] = []
+): Promise<Workflow[]> => {
+  let validWorkflows = Object.keys(workflows || [])
     .map((workflowId: string) => {
       const workflow = workflows[workflowId]();
       try {
         return { ...workflow, routes: workflowRoutes(workflowId, workflow, configuration) };
       } catch {
-        // n.b. if the routes aren't configured properly we
-        // drop the workflow
+        // n.b. if the routes aren't configured properly we drop the workflow
         /* eslint-disable-next-line no-console */
         console.warn(
           `Skipping registration of ${workflowId || "unknown"} workflow due to invalid config`
@@ -53,6 +60,12 @@ const registeredWorkflows = (
       }
     })
     .filter(workflow => workflow !== null);
+  filters.forEach(f => {
+    f(validWorkflows).then(w => {
+      validWorkflows = w;
+    });
+  });
+  return validWorkflows;
 };
 
 export default registeredWorkflows;
