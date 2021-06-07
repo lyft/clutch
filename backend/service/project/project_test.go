275e06ecf7de8b5424c9813a58d2716d9a7d9915
Mike Cutalo <mcutalo88@gmail.com>
2021-06-07 16:04:26 -0700

    service tests

--- backend/service/project/project_test.go
+++ backend/service/project/project_test.go
@@ -1,1 +1,26 @@
 package project
+
+import (
+	"testing"
+
+	"github.com/lyft/clutch/backend/mock/service/dbmock"
+	"github.com/lyft/clutch/backend/service"
+	"github.com/stretchr/testify/assert"
+	"github.com/uber-go/tally"
+	"go.uber.org/zap/zaptest"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+func TestNew(t *testing.T) {
+	log := zaptest.NewLogger(t)
+	scope := tally.NewTestScope("", nil)
+
+	// Should throw error looking for postgres
+	_, err := New(&anypb.Any{}, log, scope)
+	assert.Error(t, err)
+
+	// Should not error with postgres in the registry
+	service.Registry["clutch.service.db.postgres"] = dbmock.NewMockDB()
+	_, err = New(&anypb.Any{}, log, scope)
+	assert.NoError(t, err)
+}
