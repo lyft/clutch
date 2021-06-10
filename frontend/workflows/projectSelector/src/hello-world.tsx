4a57def65af6c67a5ca393b2fda5f85e0ebeb531
Daniel Hochman <danielhochman@users.noreply.github.com>
2021-06-10 11:27:30 -0500

    adding and removing custom projects

--- frontend/workflows/projectSelector/src/hello-world.tsx
+++ frontend/workflows/projectSelector/src/hello-world.tsx
@@ -2,7 +2,73 @@ import * as React from "react";
 
 import _ from "lodash";
 
-import { Checkbox } from "@clutch-sh/core";
+import { Button, Checkbox, Switch, TextField } from "@clutch-sh/core";
+import { Divider, IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import ClearIcon from "@material-ui/icons/Clear";
+import LayersIcon from "@material-ui/icons/Layers";
+
+enum ActionKind {
+  ADD_PROJECTS,
+  REMOVE_PROJECTS,
+  TOGGLE_PROJECTS,
+  ONLY_PROJECTS,
+}
+
+interface Action {
+  type: ActionKind;
+  payload: Payload;
+}
+
+interface Payload {
+  projects?: string[];
+}
+
+interface State {
+  projects: any;
+  customProjects: any;
+  upstreams: any;
+  downstreams: any;
+}
+
+const selectorReducer = (state: State, action: Action): State => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ActionKind.ADD_PROJECTS:
+      return {
+        ...state,
+        customProjects: {
+          ...state.customProjects,
+          ...Object.fromEntries(payload.projects.map(v => [v, null])),
+        },
+      };
+    case ActionKind.REMOVE_PROJECTS:
+      return {
+        ...state,
+        customProjects: _.omit(state.customProjects, payload.projects),
+      };
+    default:
+      throw new Error(`unknown resolver action: ${type}`);
+  }
+};
+
+enum GroupSelectedStatus {
+  NONE,
+  SOME,
+  ALL,
+}
+
+const determineSelectedStatus = (group: string): GroupSelectedStatus => {};
+
+const initialState: State = {
+  projects: {},
+  customProjects: {},
+  upstreams: {},
+  downstreams: {},
+};
 
 const ProjectSelector = () => {
   // On load, we'll request a list of owned projects and their upstreams and downstreams from the API.
@@ -11,47 +77,90 @@ const ProjectSelector = () => {
   // If a project is unchecked, the upstream and downstreams related to it disappear from the list.
   // If a project is rechecked, the checks were preserved.
 
-  const [projects, setProjects] = React.useState({});
+  const [customProject, setCustomProject] = React.useState("");
+
+  const [state, dispatch] = React.useReducer(selectorReducer, initialState);
 
   React.useEffect(() => {
-    console.log("effect fired");
     // Here we would call the API and load the initial set of projects.
-    setProjects({ users: true, books: true });
   }, []);
 
   const upstreams = ["authors", "thumbnails"];
   const downstreams = ["coffee", "shelves"];
 
   const changeHandler = ({ target }) => {
-    setProjects({ ...projects, [target.name]: target.checked });
+    // setProjects({ ...projects, [target.name]: target.checked });
   };
 
-  console.log("the selected projects were changed", projects);
+  const handleAdd = () => {
+    if (customProject === "") {
+      return;
+    }
+    dispatch({ type: ActionKind.ADD_PROJECTS, payload: { projects: [customProject] } });
+    setCustomProject("");
+  };
 
   return (
     <div>
-      My projects
       <div>
-        {Object.keys(projects).map(key => (
+        <LayersIcon />
+        Dash
+      </div>
+      <div>
+        <TextField
+          placeholder="Add a project"
+          value={customProject}
+          onChange={e => setCustomProject(e.target.value)}
+          onKeyDown={e => e.key === "Enter" && handleAdd()}
+        />
+        <IconButton onClick={handleAdd}>
+          <AddIcon />
+        </IconButton>
+      </div>
+      <div>
+        Projects
+        <Switch />
+      </div>
+      <div>
+        {/* {Object.keys(projects).map(key => (
           <div key={key}>
             <Checkbox name={key} onChange={changeHandler} checked={projects[key]} /> {key}
           </div>
+        ))} */}
+        {Object.keys(state.customProjects).map(key => (
+          <div key={key}>
+            <Checkbox name={key} onChange={changeHandler} checked />
+            {key}
+            <ClearIcon
+              onClick={() =>
+                dispatch({ type: ActionKind.REMOVE_PROJECTS, payload: { projects: [key] } })
+              }
+            />
+          </div>
         ))}
       </div>
-      Upstream
+      <Divider />
+      <div>
+        <ExpandMoreIcon />
+        Downstreams
+        <Switch />
+      </div>
       <div>
         {upstreams.map(v => (
           <div key={v}>
-            {" "}
             <Checkbox /> {v}
           </div>
         ))}
       </div>
-      Downstream
+      <Divider />
+      <div>
+        <ExpandMoreIcon />
+        Upstreams
+        <Switch />
+      </div>
       <div>
         {downstreams.map(v => (
           <div key={v}>
-            {" "}
             <Checkbox /> {v}
           </div>
         ))}
@@ -62,7 +171,7 @@ const ProjectSelector = () => {
 
 const HelloWorld = () => (
   <>
-    <ProjectSelector />{" "}
+    <ProjectSelector />
   </>
 );
 
