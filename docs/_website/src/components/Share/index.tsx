a89405e3833659ac470dc8eebef57a50abbe5d6d
Derek Schaller <d_a_schaller@yahoo.com>
2021-06-23 22:51:27 -0700

    move all window references in browseronly tag

--- docs/_website/src/components/Share/index.tsx
+++ docs/_website/src/components/Share/index.tsx
@@ -36,65 +36,67 @@ interface ShareProps {
 const Share = ({title, authors}: ShareProps) => {
   const [open, setOpen] = React.useState(false);
 
-  const tweet = encodeURI(`https://twitter.com/intent/tweet?text=${title} by ${authors.map(a => a.name).join(", ")} ${window.location.href}`);
   return (
       <BrowserOnly>
-        {() => (
-          <Popover
-            isOpen={open}
-            positions={["bottom"]}
-            padding={10}
-            align={"center"}
-            onClickOutside={() => setOpen(false)}
-            content={({ position, childRect, popoverRect }) => (
-              <ArrowContainer
-                position={position}
-                childRect={childRect}
-                popoverRect={popoverRect}
-                arrowColor="var(--ifm-color-content-secondary)"
-                arrowSize={10}
-                arrowStyle={{ opacity: ".1" }}
-                className="popover-arrow-container"
-                arrowClassName="popover-arrow"
-              >
-                <div className="share-popover-container">
-                  <div className="share-link-list">
-                    <ShareLink
-                      className="fe-twitter"
-                      label="Twitter" color="#1DA1F2"
-                      link={tweet}
-                      onClick={() => setOpen(false)}
-                    />
-                    <ShareLink
-                      className="fe-linkedin"
-                      label="LinkedIn"
-                      color="#0072b1"
-                      link={`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`}
-                      onClick={() => setOpen(false)}
-                    />
-                    <ShareLink
-                      className="fe-link"
-                      label="Copy Link"
-                      color="var(--ifm-color-content-secondary)"
-                      onClick={() => {
-                        var tmp = document.createElement("input"),
-                        href = window.location.href;
-                        document.body.appendChild(tmp);
-                        tmp.value = href;
-                        tmp.select();
-                        document.execCommand("copy");
-                        document.body.removeChild(tmp);
-                        setOpen(false);
-                      }}
-                    />
+        {() => {
+          const tweet = encodeURI(`https://twitter.com/intent/tweet?text=${title} by ${authors.map(a => a.name).join(", ")} ${window.location.href}`);
+          return (
+            <Popover
+              isOpen={open}
+              positions={["bottom"]}
+              padding={10}
+              align={"center"}
+              onClickOutside={() => setOpen(false)}
+              content={({ position, childRect, popoverRect }) => (
+                <ArrowContainer
+                  position={position}
+                  childRect={childRect}
+                  popoverRect={popoverRect}
+                  arrowColor="var(--ifm-color-content-secondary)"
+                  arrowSize={10}
+                  arrowStyle={{ opacity: ".1" }}
+                  className="popover-arrow-container"
+                  arrowClassName="popover-arrow"
+                >
+                  <div className="share-popover-container">
+                    <div className="share-link-list">
+                      <ShareLink
+                        className="fe-twitter"
+                        label="Twitter" color="#1DA1F2"
+                        link={tweet}
+                        onClick={() => setOpen(false)}
+                      />
+                      <ShareLink
+                        className="fe-linkedin"
+                        label="LinkedIn"
+                        color="#0072b1"
+                        link={`https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`}
+                        onClick={() => setOpen(false)}
+                      />
+                      <ShareLink
+                        className="fe-link"
+                        label="Copy Link"
+                        color="var(--ifm-color-content-secondary)"
+                        onClick={() => {
+                          var tmp = document.createElement("input"),
+                          href = window.location.href;
+                          document.body.appendChild(tmp);
+                          tmp.value = href;
+                          tmp.select();
+                          document.execCommand("copy");
+                          document.body.removeChild(tmp);
+                          setOpen(false);
+                        }}
+                      />
+                    </div>
                   </div>
-                </div>
-              </ArrowContainer>
-            )}
-          >
-            <span className={"fe fe-share"} onClick={() => setOpen(o => !o)}/>
-          </Popover>
-        )}
+                </ArrowContainer>
+              )}
+            >
+              <span className={"fe fe-share"} onClick={() => setOpen(o => !o)}/>
+            </Popover>
+          )
+        }}
       </BrowserOnly>
   );
 };
