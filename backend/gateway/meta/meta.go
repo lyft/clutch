d126f303e9bf20dcee2ff820825d13fce87298d5
Mike Cutalo <mcutalo88@gmail.com>
2021-06-28 09:32:13 -0700

    comments

--- backend/gateway/meta/meta.go
+++ backend/gateway/meta/meta.go
@@ -310,14 +310,13 @@ func APIBody(body interface{}) (*anypb.Any, error) {
 		return nil, nil
 	}
 
-	// Deep copy before field redaction so we do not unintentionally remove fields
-	// from the original object that were passed by reference
-	m = proto.Clone(m)
-
 	if IsRedacted(m) {
 		return anypb.New(&apiv1.Redacted{RedactedTypeUrl: TypeURL(m)})
 	}
 
+	// Deep copy before field redaction so we do not unintentionally remove fields
+	// from the original object that were passed by reference
+	m = proto.Clone(m)
 	return anypb.New(ClearLogDisabledFields(m))
 }
 
