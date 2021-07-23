24378e9d6b59d49409c23bb0398026700420ab1d
Mike Cutalo <mcutalo88@gmail.com>
2021-07-23 15:43:52 -0700

    add golang and nodejs check

--- tools/dev-requirement-checks.sh
+++ tools/dev-requirement-checks.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Will be set to false if any of the steps fail
+# 
+did_checks_pass=true
+
 # If were on OSX lets check for brew and coreutils as they are requriments
 # https://clutch.sh/docs/getting-started/local-build/#requirements
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -9,12 +13,25 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # check if coreutils is installed
     if ! brew ls --versions coreutils > /dev/null; then
       echo "coreutils is not installed, this is a required dependency."
-      echo "brew install coreutils"
-      exit
+      echo "install by running [brew install coreutils]"
+      did_checks_pass=false
     fi
   else
       echo "brew is not installed, unable to verify coreutils dependency."
-      echo "Please refer to the development requirments https://clutch.sh/docs/getting-started/local-build/#requirements"
-      exit
+      did_checks_pass=false
   fi
+fi
+
+if ! command -v go -v &> /dev/null; then
+  echo "golang is not installed, this is a required dependency."
+  did_checks_pass=false
+fi
+
+if ! command -v node -v &> /dev/null; then
+  echo "nodejs is not installed, this is a required dependency."
+  did_checks_pass=false
+fi
+
+if [ "$did_checks_pass" = false ] ; then
+  printf "\nPlease refer to the development requirments https://clutch.sh/docs/getting-started/local-build/#requirements"
 fi
