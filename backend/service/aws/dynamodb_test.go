0805032542c620c92282547fbc24ee8d68ac184f
Angela Nguyen <angelan@lyft.com>
2021-08-04 12:15:30 -0700

    test: add tests for dynamodb config

--- backend/service/aws/dynamodb_test.go
+++ backend/service/aws/dynamodb_test.go
@@ -11,7 +11,17 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	dynamodbv1 "github.com/lyft/clutch/backend/api/aws/dynamodb/v1"
+	awsv1 "github.com/lyft/clutch/backend/api/config/service/aws/v1"
 )
+
+var regions = []string{"us-east-1", "us-west-2"}
+
+var cfg = &awsv1.Config{
+	Regions: regions,
+	ClientConfig: &awsv1.ClientConfig{
+		Retries: 10,
+	},
+}
 
 var testDynamodbTable = &types.TableDescription{
 	TableName: aws.String("test-table"),
@@ -110,22 +120,97 @@ func TestDescribeTableWithGsiValid(t *testing.T) {
 	assert.Equal(t, testTableWithGSIOutput, result)
 }
 
-func TestUpdateTableCapacity(t *testing.T) {
+func TestGetScalingLimits(t *testing.T) {
+	ds := getScalingLimits(cfg)
+
+	assert.Equal(t, ds.MaxReadCapacityUnits, AwsMaxRCU, "Max RCU default set")
+	assert.Equal(t, ds.MaxScaleFactor, SafeScaleFactor, "scale factor default set")
+	assert.False(t, ds.EnableOverride)
+}
+func TestUpdateTableCapacityWithDefaultLimits(t *testing.T) {
+	tests := []struct {
+		name     string
+		inputRCU int64
+		inputWCU int64
+		want     string
+	}{
+		{"rcu above max", 100000, 250, "rpc error: code = FailedPrecondition desc = Target read capacity exceeds maximum allowed limits [40000]"},
+		{"wcu above max", 100, 100000, "rpc error: code = FailedPrecondition desc = Target write capacity exceeds maximum allowed limits [40000]"},
+		{"rcu lower than current", 1, 1500, "rpc error: code = FailedPrecondition desc = Target read capacity [1] is lower than current capacity [100]"},
+		{"wcu lower than current", 1500, 1, "rpc error: code = FailedPrecondition desc = Target write capacity [1] is lower than current capacity [200]"},
+		{"rcu change scale too high", 400, 200, "rpc error: code = FailedPrecondition desc = Target read capacity exceeds the scale limit of [2.0]x current capacity"},
+		{"wcu change scale too high", 100, 600, "rpc error: code = FailedPrecondition desc = Target write capacity exceeds the scale limit of [2.0]x current capacity"},
+	}
+
 	m := &mockDynamodb{
 		table: testDynamodbTable,
 	}
-	c := &client{
-		clients: map[string]*regionalClient{"us-east-1": {region: "us-east-1", dynamodb: m}},
+
+	ds := getScalingLimits(cfg)
+
+	d := dynamodbConfig{
+		scalingLimits: scalingLimits{
+			maxReadCapacityUnits:  ds.MaxReadCapacityUnits,
+			maxWriteCapacityUnits: ds.MaxWriteCapacityUnits,
+			maxScaleFactor:        ds.MaxScaleFactor,
+			enableOverride:        ds.EnableOverride,
+		},
 	}
 
-	err := c.UpdateTableCapacity(context.Background(), "us-east-1", "test-table", 150, 250)
-	assert.NoError(t, err)
+	c := &client{
+		clients: map[string]*regionalClient{"us-east-1": {region: "us-east-1", dynamodbCfg: d, dynamodb: m}},
+	}
 
-	err1 := c.UpdateTableCapacity(context.Background(), "us-east-1", "test-table", 50, 100)
-	assert.EqualError(t, err1, "rpc error: code = FailedPrecondition desc = Target read capacity is lower than current capacity.")
+	for _, tt := range tests {
+		tt := tt // capture range variable
+		t.Run(tt.name, func(t *testing.T) {
+			got := c.UpdateTableCapacity(context.Background(), "us-east-1", "test-gsi-table", tt.inputRCU, tt.inputWCU)
+			if got.Error() != tt.want {
+				t.Errorf("\nWant error msg: %s\nGot error msg: %s", tt.want, got)
+			}
+		})
+	}
+}
 
-	err2 := c.UpdateTableCapacity(context.Background(), "us-east-1", "test-table", 1000, 2000)
-	assert.EqualError(t, err2, "rpc error: code = FailedPrecondition desc = Target read capacity exceeds scale limit.")
+func TestUpdateTableCapacityWithCustomLimits(t *testing.T) {
+	tests := []struct {
+		name     string
+		inputRCU int64
+		inputWCU int64
+		want     string
+	}{
+		{"rcu above max", 2000, 250, "rpc error: code = FailedPrecondition desc = Target read capacity exceeds maximum allowed limits [1000]"},
+		{"wcu above max", 100, 3000, "rpc error: code = FailedPrecondition desc = Target write capacity exceeds maximum allowed limits [2000]"},
+		{"rcu lower than current", 1, 1500, "rpc error: code = FailedPrecondition desc = Target read capacity [1] is lower than current capacity [100]"},
+		{"wcu lower than current", 1500, 1, "rpc error: code = FailedPrecondition desc = Target write capacity [1] is lower than current capacity [200]"},
+		{"rcu change scale too high", 900, 200, "rpc error: code = FailedPrecondition desc = Target read capacity exceeds the scale limit of [4.0]x current capacity"},
+		{"wcu change scale too high", 100, 1900, "rpc error: code = FailedPrecondition desc = Target write capacity exceeds the scale limit of [4.0]x current capacity"},
+	}
+
+	m := &mockDynamodb{
+		table: testDynamodbTable,
+	}
+	d := dynamodbConfig{
+		scalingLimits: scalingLimits{
+			maxReadCapacityUnits:  1000,
+			maxWriteCapacityUnits: 2000,
+			maxScaleFactor:        4.0,
+			enableOverride:        false,
+		},
+	}
+	c := &client{
+		clients: map[string]*regionalClient{"us-east-1": {region: "us-east-1", dynamodbCfg: d, dynamodb: m}},
+	}
+
+	for _, tt := range tests {
+		tt := tt // capture range variable
+		t.Run(tt.name, func(t *testing.T) {
+			got := c.UpdateTableCapacity(context.Background(), "us-east-1", "test-gsi-table", tt.inputRCU, tt.inputWCU)
+			if got.Error() != tt.want {
+				t.Errorf("\nWant error msg: %s\nGot error msg: %s", tt.want, got)
+			}
+		})
+	}
 }
 
 type mockDynamodb struct {
