0b5a19cbf3e044c6e5a9b640e29f94103ee6d1de
Angela Nguyen <angelan@lyft.com>
2021-09-02 09:52:49 -0700

    lintiing

--- frontend/workflows/dynamodb/src/update-capacity.tsx
+++ frontend/workflows/dynamodb/src/update-capacity.tsx
@@ -11,10 +11,10 @@ import {
   TableRow,
   useWizardContext,
 } from "@clutch-sh/core";
-import styled from "@emotion/styled";
 import { useDataLayout } from "@clutch-sh/data-layout";
 import type { WizardChild } from "@clutch-sh/wizard";
 import { Wizard, WizardStep } from "@clutch-sh/wizard";
+import styled from "@emotion/styled";
 import _ from "lodash";
 import { number } from "yup";
 
@@ -44,54 +44,52 @@ const TableDetails: React.FC<WizardChild> = () => {
   const table = resourceData.displayValue() as IClutch.aws.dynamodb.v1.Table;
 
   const handleTableCapacityChange = (key: string, value: string) => {
-    const newTableCapacity = {...capacityUpdates.displayValue().table_throughput};
+    const newTableCapacity = { ...capacityUpdates.displayValue().table_throughput };
     newTableCapacity[key] = value;
-    capacityUpdates.updateData("table_throughput", newTableCapacity)
-    console.log(capacityUpdates.displayValue().table_throughput)
+    capacityUpdates.updateData("table_throughput", newTableCapacity);
   };
 
   const handleGsiCapacityChange = (key: string, value: string) => {
-    // big hack to retrieve the capacity type (read or write?) and 
+    // big hack to retrieve the capacity type (read or write?) and
     // the GSI name from a single event attribute
-    const keys = key.split(',')
-    const capacityType = keys[0]
-    const gsiName = keys[1]
+    const keys = key.split(",");
+    const capacityType = keys[0];
+    const gsiName = keys[1];
 
-    const gsiList = capacityUpdates.displayValue().gsi_updates? [...capacityUpdates.displayValue().gsi_updates] : [];
-    const idx = gsiList.findIndex((gsi: {name: string, index_throughput: {}}) => gsi.name === gsiName)
-    if ( idx > -1) { // gsi already in the edits list
-      const gsi = gsiList[idx]
-      gsi["index_throughput"] = {...gsi["index_throughput"], [capacityType] : value}
-      capacityUpdates.updateData("gsi_updates", gsiList)
-    } else { // copy over current capacities 
-      const curr = table.globalSecondaryIndexes.find(gsi => gsi.name === gsiName)
-      const newGsi = { 
-        "name": gsiName, 
-        "index_throughput": { 
-          "read_capacity_units": curr.provisionedThroughput.readCapacityUnits,
-          "write_capacity_units": curr.provisionedThroughput.writeCapacityUnits,
-        }
-      }
-      newGsi.index_throughput = {...newGsi.index_throughput, [capacityType]: value}
-      gsiList.push(newGsi)
-      capacityUpdates.updateData("gsi_updates", gsiList)
+    const gsiList = capacityUpdates.displayValue().gsi_updates
+      ? [...capacityUpdates.displayValue().gsi_updates]
+      : [];
+    const idx = gsiList.findIndex(
+      (gsi: { name: string; indexThroughput: {} }) => gsi.name === gsiName
+    );
+    if (idx > -1) {
+      // gsi already in the edits list
+      const gsi = gsiList[idx];
+      gsi.index_throughput = { ...gsi.index_throughput, [capacityType]: value };
+      capacityUpdates.updateData("gsi_updates", gsiList);
+    } else {
+      // copy over current capacities
+      const curr = table.globalSecondaryIndexes.find(gsi => gsi.name === gsiName);
+      const newGsi = {
+        name: gsiName,
+        index_throughput: {
+          read_capacity_units: curr.provisionedThroughput.readCapacityUnits,
+          write_capacity_units: curr.provisionedThroughput.writeCapacityUnits,
+        },
+      };
+      newGsi.index_throughput = { ...newGsi.index_throughput, [capacityType]: value };
+      gsiList.push(newGsi);
+      capacityUpdates.updateData("gsi_updates", gsiList);
     }
   };
 
   return (
     <WizardStep error={resourceData.error} isLoading={resourceData.isLoading}>
       <Container>
-        <Table
-          columns={[
-            'Name',
-            'Type',
-            'Status',
-            'Provisioned Capacities',
-          ]}
-        >
+        <Table columns={["Name", "Type", "Status", "Provisioned Capacities"]}>
           <TableRow key={table.name}>
             {table.name}
-            {"Table"}
+            Table
             {table.status}
             <MetadataTable
               onUpdate={handleTableCapacityChange}
@@ -103,8 +101,8 @@ const TableDetails: React.FC<WizardChild> = () => {
                     type: "number",
                     key: "read",
                     validation: number()
-                    .integer("must be a number")
-                    .min(table.provisionedThroughput.readCapacityUnits),
+                      .integer("must be a number")
+                      .min(table.provisionedThroughput.readCapacityUnits),
                   },
                 },
                 {
@@ -117,53 +115,53 @@ const TableDetails: React.FC<WizardChild> = () => {
                       .integer()
                       .min(table.provisionedThroughput.writeCapacityUnits),
                   },
-                }]}>
-              </MetadataTable>
+                },
+              ]}
+            />
           </TableRow>
           {table.globalSecondaryIndexes.map(gsi => (
-          <TableRow key={gsi.name}>
-            {gsi.name}
-            {"Index"}
-            {gsi.status}
-            <MetadataTable
-              onUpdate={handleGsiCapacityChange}
-              data={[
-                {
-                  name: "read",
-                  value: gsi.provisionedThroughput.readCapacityUnits,
-                  input: {
-                    type: "number",
-                    key: ["read_capacity_units", gsi.name],
-                    validation: number()
-                    .integer()
-                    .min(gsi.provisionedThroughput.readCapacityUnits),
+            <TableRow key={gsi.name}>
+              {gsi.name}
+              Index
+              {gsi.status}
+              <MetadataTable
+                onUpdate={handleGsiCapacityChange}
+                data={[
+                  {
+                    name: "read",
+                    value: gsi.provisionedThroughput.readCapacityUnits,
+                    input: {
+                      type: "number",
+                      key: ["read_capacity_units", gsi.name],
+                      validation: number()
+                        .integer()
+                        .min(gsi.provisionedThroughput.readCapacityUnits),
+                    },
                   },
-                },
-                {
-                  name: "write",
-                  value: gsi.provisionedThroughput.writeCapacityUnits,
-                  input: {
-                    type: "number",
-                    key: ["write_capacity_units", gsi.name],
-                    validation: number()
-                      .integer()
-                      .min(gsi.provisionedThroughput.writeCapacityUnits),
+                  {
+                    name: "write",
+                    value: gsi.provisionedThroughput.writeCapacityUnits,
+                    input: {
+                      type: "number",
+                      key: ["write_capacity_units", gsi.name],
+                      validation: number()
+                        .integer()
+                        .min(gsi.provisionedThroughput.writeCapacityUnits),
+                    },
                   },
-                }]}>
-              </MetadataTable>
-          </TableRow>
-      ))}
-        
+                ]}
+              />
+            </TableRow>
+          ))}
         </Table>
       </Container>
-
 
       {/* TO DO: conditionally render the override checkbox depending on workflow config prop */}
       <CheckboxPanel
         header="To override the safety limits for scaling, check the box below."
         onChange={state => capacityUpdates.updateData("ignore_maximums", state)}
         options={{
-          'Override maximum limits': false,
+          "Override maximum limits": false,
         }}
       />
 
@@ -177,52 +175,59 @@ const TableDetails: React.FC<WizardChild> = () => {
 
 const Confirm: React.FC<WizardChild> = () => {
   const updateCapacityOutput = useDataLayout("updateCapacityOutput");
-  let scalingResults = updateCapacityOutput.displayValue()?.data?.table
+  const scalingResults = updateCapacityOutput.displayValue()?.data?.table;
   let statusList = [];
   if (!_.isEmpty(scalingResults)) {
-    statusList.push(scalingResults)
-    statusList = statusList.concat(scalingResults.globalSecondaryIndexes)
-  } 
+    statusList.push(scalingResults);
+    statusList = statusList.concat(scalingResults.globalSecondaryIndexes);
+  }
 
   return (
     <WizardStep error={updateCapacityOutput.error} isLoading={updateCapacityOutput.isLoading}>
       <Table columns={["Name", "Status"]}>
-      {statusList.map((s, index: number) => (
+        {statusList.map((s, index: number) => (
           <TableRow key={s.name + index}>
             {s.name}
             {s.status}
           </TableRow>
-      ))}
+        ))}
       </Table>
     </WizardStep>
   );
 };
 
-const UpdateCapacity: React.FC<WorkflowProps> = ({resolverType}) => {
+const UpdateCapacity: React.FC<WorkflowProps> = ({ resolverType }) => {
   const dataLayout = {
     resourceData: {},
     capacityUpdates: {},
     updateCapacityOutput: {
       deps: ["resourceData", "capacityUpdates"],
-      hydrator: (resourceData, capacityUpdates)=> {
-        let tableArgs = {
+      hydrator: (resourceData, capacityUpdates) => {
+        const tableArgs = {
           table_name: resourceData.name,
           region: resourceData.region,
           // ignore_maximums: capacityUpdates?.ignore_maximums? capacityUpdates.ignore_maximums : false,
-        }
+        };
 
-        let changeArgs: {}
+        let changeArgs: {};
         if (capacityUpdates.table_throughput) {
-          changeArgs = {...changeArgs, table_throughput: {
-            read_capacity_units: capacityUpdates.table_throughput["read"]? capacityUpdates.table_throughput["read"] : resourceData.provisionedThroughput.readCapacityUnits,
-            write_capacity_units: capacityUpdates.table_throughput["write"]? capacityUpdates.table_throughput["write"] : resourceData.provisionedThroughput.writeCapacityUnits,
-          }}
+          changeArgs = {
+            ...changeArgs,
+            table_throughput: {
+              read_capacity_units: capacityUpdates.table_throughput.read
+                ? capacityUpdates.table_throughput.read
+                : resourceData.provisionedThroughput.readCapacityUnits,
+              write_capacity_units: capacityUpdates.table_throughput.write
+                ? capacityUpdates.table_throughput.write
+                : resourceData.provisionedThroughput.writeCapacityUnits,
+            },
+          };
         }
         if (Array.isArray(capacityUpdates.gsi_updates)) {
-          changeArgs = {...changeArgs, gsi_updates: [...capacityUpdates.gsi_updates]}
+          changeArgs = { ...changeArgs, gsi_updates: [...capacityUpdates.gsi_updates] };
         }
         return client
-          .post("/v1/aws/dynamodb/updateCapacity", {...tableArgs, ...changeArgs})
+          .post("/v1/aws/dynamodb/updateCapacity", { ...tableArgs, ...changeArgs })
           .then(resp => {
             return resp;
           });
