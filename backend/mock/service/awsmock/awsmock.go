8c42d4733d1333cc9eecd8f43f1576515c4d83fd
Faris Mohamed <farism@lyft.com>
2021-10-06 17:54:52 -0400

    remove duplicated imports from autolint fix

--- backend/mock/service/awsmock/awsmock.go
+++ backend/mock/service/awsmock/awsmock.go
@@ -6,9 +6,6 @@ import (
 	"io"
 	"math/rand"
 
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/smithy-go/middleware"
-
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
