edd8ae0b0acedcd19a3b48a1b0df4acdc42c41d3
SHAWNA MONERO <smonero@lyft.com>
2021-11-01 13:40:33 -0700

    q mark for checking current

--- frontend/packages/core/src/popper.tsx
+++ frontend/packages/core/src/popper.tsx
@@ -102,7 +102,7 @@ const Popper = ({
   id,
 }: PopperProps) => (
   <Collapse in={open} timeout="auto" unmountOnExit>
-    <StyledPopper open={open} anchorEl={anchorRef.current} transition placement={placement}>
+    <StyledPopper open={open} anchorEl={anchorRef?.current} transition placement={placement}>
       <Paper>
         <ClickAwayListener onClickAway={onClickAway}>
           <List component="div" disablePadding id={id}>
