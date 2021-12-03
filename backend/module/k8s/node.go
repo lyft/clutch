cdc7d7bd7bae94ef7e21281d3ef57263e5da6aaa
Ashley Kasim <akasim@lyft.com>
2021-12-02 16:09:44 -0800

    k8s: implement backend api's for node (#1907)

--- backend/module/k8s/node.go
+++ backend/module/k8s/node.go
@@ -2,15 +2,22 @@ package k8s
 
 import (
 	"context"
-	"errors"
 
 	k8sapiv1 "github.com/lyft/clutch/backend/api/k8s/v1"
 )
 
 func (a *k8sAPI) DescribeNode(ctx context.Context, req *k8sapiv1.DescribeNodeRequest) (*k8sapiv1.DescribeNodeResponse, error) {
-	return nil, errors.New("not implemented")
+	node, err := a.k8s.DescribeNode(ctx, req.Clientset, req.Cluster, req.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &k8sapiv1.DescribeNodeResponse{Node: node}, nil
 }
 
 func (a *k8sAPI) UpdateNode(ctx context.Context, req *k8sapiv1.UpdateNodeRequest) (*k8sapiv1.UpdateNodeResponse, error) {
-	return nil, errors.New("not implemented")
+	err := a.k8s.UpdateNode(ctx, req.Clientset, req.Cluster, req.Name, req.Unschedulable)
+	if err != nil {
+		return nil, err
+	}
+	return &k8sapiv1.UpdateNodeResponse{}, nil
 }
