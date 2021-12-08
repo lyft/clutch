5fbd3ad25abbe084012c2302e3030b81a4f6c4cb
Josh Slaughter <jslaughter@lyft.com>
2021-12-08 14:40:37 -0800

    Addressing feedback: Modifying Emoji component to ease maintenance

--- frontend/packages/core/src/Assets/emojis/index.tsx
+++ frontend/packages/core/src/Assets/emojis/index.tsx
@@ -6,24 +6,25 @@ import HappyEmoji from "./happy";
 import NeutralEmoji from "./neutral";
 import SadEmoji from "./sad";
 
-export type EmojiType = "HAPPY" | "NEUTRAL" | "SAD";
+const EmojiTypes = {
+  HAPPY: ({ ...props }) => <HappyEmoji {...props} />,
+  NEUTRAL: ({ ...props }) => <NeutralEmoji {...props} />,
+  SAD: ({ ...props }) => <SadEmoji {...props} />,
+};
+
+export type EmojiType = keyof typeof EmojiTypes;
 
 interface EmojiProps extends SVGProps {
   type: EmojiType;
 }
 
-const Emoji = ({ type, ...props }: EmojiProps) => {
-  switch (type) {
-    case "HAPPY":
-      return <HappyEmoji {...props} />;
-    case "NEUTRAL":
-      return <NeutralEmoji {...props} />;
-    case "SAD":
-      return <SadEmoji {...props} />;
-    default:
-      return null;
-  }
-};
+/**
+ * Shorthand component to return an Emoji based on a given input type
+ *
+ * @param type the given type of emoji to return from EmojiType
+ * @returns the given emoji extended with any additional properties passed in
+ */
+const Emoji = ({ type, ...props }: EmojiProps) => EmojiTypes[type](props);
 
 export default Emoji;
 
