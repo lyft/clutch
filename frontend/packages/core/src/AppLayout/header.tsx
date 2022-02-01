70d6e1719006b0cc2a74bc7c613f14e96bb31f2a
Josh Slaughter <8338893+jdslaugh@users.noreply.github.com>
2022-02-01 11:05:13 -0800

    frontend: NPS Header Component (#1964)
    * frontend: nps feedback components

--- frontend/packages/core/src/AppLayout/header.tsx
+++ frontend/packages/core/src/AppLayout/header.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
 import { AppBar as MuiAppBar, Box, Grid, Toolbar, Typography } from "@material-ui/core";
 
+import { FeatureOn, SimpleFeatureFlag } from "../flags";
+import { NPSHeader } from "../NPS";
+
 import Logo from "./logo";
 import Notifications from "./notifications";
 import SearchField from "./search";
@@ -44,6 +47,11 @@ const Header: React.FC = () => {
               <SearchField />
             </Box>
             {showNotifications && <Notifications />}
+            <SimpleFeatureFlag feature="npsHeader">
+              <FeatureOn>
+                <NPSHeader />
+              </FeatureOn>
+            </SimpleFeatureFlag>
             <UserInformation />
           </Grid>
         </Toolbar>
