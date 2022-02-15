085f0ac753707f3c026a85faac08adeb332f56a2
Josh Slaughter <jslaughter@lyft.com>
2022-02-15 14:48:45 -0800

    Extending from alertProps instead of mui

--- frontend/packages/core/src/Feedback/warning.tsx
+++ frontend/packages/core/src/Feedback/warning.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Snackbar } from "@material-ui/core";
-import type { AlertProps as MuiAlertProps } from "@material-ui/lab";
 
+import type { AlertProps } from "./alert";
 import { Alert } from "./alert";
 
-export interface WarningProps extends Pick<MuiAlertProps, "severity" | "action" | "onClose"> {
+export interface WarningProps extends AlertProps {
   message: any;
-  title?: React.ReactNode;
   onClose?: () => void;
 }
 
-const Warning: React.FC<WarningProps> = ({ message, title, onClose, severity = "warning" }) => {
+const Warning: React.FC<WarningProps> = ({ message, onClose, severity = "warning", ...props }) => {
   const [open, setOpen] = React.useState(true);
 
   const onDismiss = () => {
@@ -28,7 +27,7 @@ const Warning: React.FC<WarningProps> = ({ message, title, onClose, severity = "
       onClose={() => setOpen(false)}
       anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
     >
-      <Alert elevation={6} variant="filled" onClose={onDismiss} title={title} severity={severity}>
+      <Alert elevation={6} variant="filled" onClose={onDismiss} severity={severity} {...props}>
         {message}
       </Alert>
     </Snackbar>
