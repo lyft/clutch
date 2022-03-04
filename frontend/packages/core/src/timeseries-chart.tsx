6b9fe1db67ffdfb9e96ad3d3af1d8b15f38b34c4
SHAWNA MONERO <smonero@lyft.com>
2022-03-04 08:43:15 -0800

    wrapper pt1

---
+++ frontend/packages/core/src/timeseries-chart.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+  
+
+  const TimeseriesChart = (data) => {
+
+
+
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" type="number" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      );
+  };
+  
+  export default TimeseriesChart;
+  
