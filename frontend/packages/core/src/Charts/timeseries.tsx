a442aabc0ba91861f426a2d2b2655dffd86f5cf6
SHAWNA MONERO <smonero@lyft.com>
2022-03-06 20:09:18 -0800

    update

--- frontend/packages/core/src/Charts/timeseries.tsx
+++ frontend/packages/core/src/Charts/timeseries.tsx
@@ -11,11 +11,25 @@ import {
   YAxis,
 } from "recharts";
 
-export interface ReferenceLineProps {
-  axis: "x" | "y";
+
+const tickFormatterFunc = (timeStamp) => {
+  const date = new Date(timeStamp);
+  return date.toLocaleTimeString();
+}
+
+export type ReferenceLineAxis = "x" | "y";
+/*
+  For reference lines (dashed lines), you can set the `axis` property to "x" or "y" to denote which axis 
+  the line is on. These can be useful for showing limits or thresholds on graphs.
+  There is already a type called `ReferenceLineProps` in Recharts so we avoid that name
+*/
+export interface TimeseriesReferenceLineProps {
+  axis: ReferenceLineAxis;
   coordinate: number;
+  color: string;
   label?: string;
 }
+
 export interface LineProps {
   dataKey: string;
   color: string;
@@ -27,8 +41,11 @@ export interface TimeseriesChartProps {
   xAxisDataKey?: string;
   yAxisDataKey?: string;
   lines: LineProps[];
-  refLines?: ReferenceLineProps[];
-  // TODO: add ref dots, ref areas, zoom enabled, auto colors, legend enabled, cartesian grid options
+  refLines?: TimeseriesReferenceLineProps[];
+  singleLineMode?: boolean; // if false, the Y Axis will be based off the max and min of all data combined 
+  // The assumption is that multiple lines would want to share the same Y Axis.
+  // TODO: add ref dots, ref areas, zoom enabled, auto colors, legend enabled, cartesian grid options, 
+  // tooltip options, activeDot options, dark mode / styling, 
 }
 
 /*
@@ -38,17 +55,10 @@ export interface TimeseriesChartProps {
   Timeseries Chart).
   
   *** VERY IMPORTANT ***
-  It is very important that the data is sorted in ascending order by the XAxis. Otherwise the lines will go
-  backwards and have problems.
+  The data will be internally sorted by the XAxis dataKey when using single line mode. When having multiple lines
+  (singleLineMode is false) then the user is responsible for sorting and grouping the data appropriately. 
+  If they do not sort and group it, there can be discontinuities in the lines.
   *** END VERY IMPORTANT ***
-
-  Suggested data format:
-  {
-    lineName: string
-    timestamp: number
-    value: number
-  }
-  For reference lines, you can set the `axis` property to "x" or "y" to denote which axis the line is on.
 */
 const TimeseriesChart = ({
   data,
@@ -56,7 +66,11 @@ const TimeseriesChart = ({
   yAxisDataKey,
   lines,
   refLines,
+  singleLineMode = true,
 }: TimeseriesChartProps) => {
+  if (singleLineMode) {
+    data.sort((a, b) => a[xAxisDataKey] - b[xAxisDataKey]);
+  } 
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart data={data}>
@@ -66,8 +80,9 @@ const TimeseriesChart = ({
           type="number"
           scale="time"
           domain={["dataMin - 1000", "dataMax + 1000"]}
+          tickFormatter={tickFormatterFunc}
         />
-        <YAxis dataKey={yAxisDataKey} domain={["dataMin", "dataMax"]} />
+        {singleLineMode ? <YAxis dataKey={yAxisDataKey} domain={["dataMin", "dataMax"]} type="number" /> : <YAxis type="number" /> }
         <Tooltip />
         <Legend />
         {lines
@@ -78,7 +93,7 @@ const TimeseriesChart = ({
                   type="linear"
                   dataKey={line.dataKey}
                   stroke={line.color}
-                  animationDuration={line.animationDuration !== null ? line.animationDuration : 500}
+                  animationDuration={line.animationDuration !== null ? line.animationDuration : null}
                 />
               );
             })
@@ -90,12 +105,14 @@ const TimeseriesChart = ({
                   key={index.toString() + refLine.axis + refLine.coordinate.toString()}
                   x={refLine.coordinate}
                   label={refLine.label}
+                  stroke={refLine.color}
                 />
               ) : (
                 <ReferenceLine
                   key={index.toString() + refLine.axis + refLine.coordinate.toString()}
                   y={refLine.coordinate}
                   label={refLine.label}
+                  stroke={refLine.color}
                 />
               );
             })
---
+++ frontend/packages/core/src/stories/timeseries.stories.tsx
@@ -0,0 +1,171 @@
+import * as React from "react";
+import { styled } from "@clutch-sh/core";
+import type { Meta } from "@storybook/react";
+
+import TimeseriesChart, { TimeseriesReferenceLineProps } from "../Charts/timeseries";
+
+export default {
+  title: "Core/TimeseriesChart",
+  component: TimeseriesChart,
+} as Meta;
+
+const ChartContainer = styled("div")({
+  width: 900,
+  height: 200,
+});
+
+export const Primary = () => {
+  const mockDataSingleLine = [
+    {
+      lineName: "line1",
+      timestamp: 1546300800000,
+      value: 5,
+
+    },
+    {
+      lineName: "line1",
+      timestamp: 1546300900000,
+      value: 20,
+    },
+    {
+      lineName: "line1",
+      timestamp: 1546301000000,
+      value: 30,
+    },
+    {
+      lineName: "line2",
+      timestamp: 1546300700000,
+      value: 5,
+
+    },
+    {
+      lineName: "line2",
+      timestamp: 1546300600000,
+      value: 20,
+    },
+    {
+      lineName: "line2",
+      timestamp: 1546301800000,
+      value: 30,
+    },
+  ];
+
+  const mockDataMultiLine = [
+    {
+      lineName: "line1",
+      timestamp: 1546301800,
+      value2: 15,
+    },
+    {
+      lineName: "line1",
+      timestamp: 1546301900,
+      value2: 20,
+    },
+    {
+      lineName: "line1",
+      timestamp: 1546302000,
+      value2: 80,
+    },
+    {
+      lineName: "line2",
+      timestamp: 1546301820,
+      value: 10,
+    },
+    {
+      lineName: "line2",
+      timestamp: 1546301930,
+      value: 20,
+    },
+    {
+      lineName: "line2",
+      timestamp: 1546302040,
+      value: 40,
+    },
+  ];
+
+  const mockData3 = [
+    {
+      lineName: "line1",
+      timestamp: 1546300800000,
+      value: 30,
+    },
+    {
+      lineName: "line1",
+      timestamp: 1546300900000,
+      value: 25,
+    },
+    {
+      lineName: "line1",
+      timestamp: 1546301000000,
+      value: 10,
+    },
+  ];
+
+  const mockLines = [
+    {
+      dataKey: "value",
+      color: "red",
+      animationDuration: 0,
+    },
+  ]
+
+  const mockLines2 = [
+    {
+      dataKey: "value",
+      color: "pink",
+      animationDuration: 2000,
+    },
+  ]
+
+  const mockMultipleLines = [
+    {
+      dataKey: "value",
+      color: "red",
+      animationDuration: 0,
+    },
+    {
+      dataKey: "value2",
+      color: "blue",
+    }
+  ];
+
+  const mockRefLines: TimeseriesReferenceLineProps[] = [
+    {
+      axis: "x",
+      coordinate: 1546300850000,
+      label: "ref line 1",
+      color: "green",
+    },
+    {
+      axis: "y",
+      coordinate: 17,
+      label: "ref line 2",
+      color: "red",
+    }
+  ];
+
+  return (
+    <ChartContainer>
+      <TimeseriesChart
+        data={mockDataSingleLine}
+        xAxisDataKey="timestamp"
+        yAxisDataKey="value"
+        lines={mockLines}
+      /> 
+      <TimeseriesChart
+        data={mockDataMultiLine}
+        xAxisDataKey="timestamp"
+        yAxisDataKey="value"
+        lines={mockMultipleLines}
+        singleLineMode={false}
+      />
+      <TimeseriesChart
+        data={mockData3}
+        xAxisDataKey="timestamp"
+        yAxisDataKey="value"
+        lines={mockLines2}
+        refLines={mockRefLines}
+      />
+    </ChartContainer>
+  );
+};
