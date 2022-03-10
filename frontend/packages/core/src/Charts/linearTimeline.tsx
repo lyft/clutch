efcf4d1f3dc74ed841baa6cc62010a7837e91bfd
SHAWNA MONERO <smonero@lyft.com>
2022-03-09 20:41:12 -0800

    fix

--- frontend/packages/core/src/Charts/linearTimeline.tsx
+++ frontend/packages/core/src/Charts/linearTimeline.tsx
@@ -2,64 +2,62 @@ import React from "react";
 import {
   CartesianGrid,
   Legend,
-  ScatterChart,
-  Scatter,
   ResponsiveContainer,
+  Scatter,
+  ScatterChart,
   Tooltip,
   XAxis,
   YAxis,
 } from "recharts";
-import { calculateTicks, calculateDomainEdges, localTimeFormatter } from "./helpers";
+
+import { calculateDomainEdges, calculateTicks, localTimeFormatter } from "./helpers";
 
 export interface LinearTimelineDataPoint {
-    timestamp: number;
-    // more to come
-  }
+  timestamp: number;
+  // more to come
+}
 
-  // Note that shape can be a set of shapes denoted by preset strings ("circle", "square", etc.)
-  // or a custom SVG element
-  export interface LinearTimelineDataPoints {
-    points: LinearTimelineDataPoint[];
-    shape?: any;
-    color?: string;
-  }
-  
-  export interface LinearTimelineData {
-    [lane: string]: LinearTimelineDataPoints;
-    // ...
-  }
+// Note that shape can be a set of shapes denoted by preset strings ("circle", "square", etc.)
+// or a custom SVG element
+export interface LinearTimelineDataPoints {
+  points: LinearTimelineDataPoint[];
+  shape?: any;
+  color?: string;
+}
 
-  export interface LinearTimelineProps {
-    data: LinearTimelineData;
-    xAxisDataKey: string;
-    regularIntervalTicks?: boolean;
-    tickFormatterFunc?: (timestamp: number) => string;
-    enableLegend?: boolean;
+export interface LinearTimelineData {
+  [lane: string]: LinearTimelineDataPoints;
+  // ...
+}
 
-  }
+export interface LinearTimelineProps {
+  data: LinearTimelineData;
+  xAxisDataKey: string;
+  regularIntervalTicks?: boolean;
+  tickFormatterFunc?: (timestamp: number) => string;
+  legend?: boolean;
+}
 
 const LinearTimeline = ({
   data,
   xAxisDataKey = "timestamp",
   regularIntervalTicks = true,
   tickFormatterFunc = localTimeFormatter,
-  enableLegend = true,
+  legend = true,
 }: LinearTimelineProps) => {
-  const combinedData = Object.keys(data).reduce((acc, lane) => {;
+  const combinedData = Object.keys(data).reduce((acc, lane) => {
     return [...acc, ...data[lane].points];
   }, []);
-  const [xAxisDomainMin, xAxisDomainMax] = calculateDomainEdges(
-    combinedData,
-    xAxisDataKey,
-    .2
-  );
+  const [xAxisDomainMin, xAxisDomainMax] = calculateDomainEdges(combinedData, xAxisDataKey, 0.2);
   let ticks = [];
   if (regularIntervalTicks) {
     ticks = calculateTicks(combinedData, xAxisDataKey);
   }
 
-
+  // Because we can't rely on using "category" for the Y-Axis, we need to assign Ids (based off the index)
+  const mappingOfLaneIdsToNames = {};
   const dataWithIds = Object.keys(data).map((key, index) => {
+    mappingOfLaneIdsToNames[index] = key;
     const thePoints = data[key].points;
     const pointsWithId = thePoints.map(point => {
       return {
@@ -67,33 +65,46 @@ const LinearTimeline = ({
         laneID: index,
       };
     });
-    data[key].points = pointsWithId;
     return {
-      ...data[key],
+      points: pointsWithId,
+      shape: data[key].shape,
+      color: data[key].color,
       laneID: index,
     };
-  })
+  });
+  const formatIdsToNames = (value: string) => {
+    return <span>{mappingOfLaneIdsToNames[value]}</span>;
+  };
+
+  // TODO: Allow for proper styling and make things less hacky than "payload[0]"
+  const formatTooltip = ({ active, payload, label }) => {
+    if (active) {
+      return <div>{localTimeFormatter(payload[0].value)}</div>;
+    }
+
+    return null;
+  };
 
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <ScatterChart >
+      <ScatterChart>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis
           dataKey={xAxisDataKey}
           type="number"
           domain={[xAxisDomainMin, xAxisDomainMax]}
-          tickFormatter={tickFormatterFunc || null}
+          tickFormatter={tickFormatterFunc}
           allowDataOverflow
           ticks={regularIntervalTicks ? ticks : null}
         />
         {/* Note due to https://github.com/recharts/recharts/issues/2563 we cannot use a "category" type scatter plot
             To get around this we do a workaround of numbering each lane and hiding the numbers from the user */}
-        <YAxis dataKey={"laneID"} type="number" padding={{bottom: 30, top: 30}} hide={true} />
-        <Tooltip />
-        {enableLegend ? <Legend /> : null}
-        {Object.keys(dataWithIds).map((lane) => {
-          const points = dataWithIds[lane].points;
-          console.log(points);
+        <YAxis dataKey="laneID" type="number" padding={{ bottom: 30, top: 30 }} hide />
+        <Tooltip content={formatTooltip} />
+        {/* TODO: Use the Z-Axis for a "zoom" amount to enlarge or shrink icon size */}
+        {legend ? <Legend iconSize={18} formatter={formatIdsToNames} /> : null}
+        {Object.keys(dataWithIds).map(lane => {
+          const { points } = dataWithIds[lane];
           return (
             <Scatter
               key={lane}
@@ -101,6 +112,7 @@ const LinearTimeline = ({
               name={lane}
               shape={dataWithIds[lane].shape ?? "circle"}
               fill={dataWithIds[lane].color ?? "null"}
+              legendType={dataWithIds[lane].shape ?? "circle"}
             />
           );
         })}
--- frontend/packages/core/src/stories/linearTimeline.stories.tsx
+++ frontend/packages/core/src/stories/linearTimeline.stories.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { styled } from "@clutch-sh/core";
 import type { Meta } from "@storybook/react";
 
-import LinearTimeline from "../Charts/linearTimeline"
 import type { LinearTimelineData } from "../Charts/linearTimeline";
+import LinearTimeline from "../Charts/linearTimeline";
 
 export default {
   title: "Core/LinearTimeline",
@@ -17,28 +17,31 @@ const ChartContainer = styled("div")({
 
 export const Primary = () => {
   const mockData: LinearTimelineData = {
-    "deploys": { points: [ { timestamp: 1588884888 } ], shape: "cross", color: "purple" },
-    "k8s events": { points: [ { timestamp: 1588985888 } ], shape: "wye", color: "pink" },
-    "explosions": { points: [ { timestamp: 1588788888 }, { timestamp: 1589708888 }, 
-      { timestamp: 1589608088 }, 
-      { timestamp: 1589618088 },
-      { timestamp: 1589628088 },
-      { timestamp: 1589638088 },
-      { timestamp: 1589648088 },
-      { timestamp: 1589658088 },
-      { timestamp: 1589668088 },
-      { timestamp: 1589678088 },
-      { timestamp: 1589688088 },
-      { timestamp: 1589698088 },
-      { timestamp: 1589607088 },
-    ], shape: "star", color: "green" },
+    deploys: { points: [{ timestamp: 1588884888 }], shape: "cross", color: "purple" },
+    "k8s events": { points: [{ timestamp: 1588985888 }], shape: "triangle", color: "blue" },
+    explosions: {
+      points: [
+        { timestamp: 1588788888 },
+        { timestamp: 1589708888 },
+        { timestamp: 1589608088 },
+        { timestamp: 1589618088 },
+        { timestamp: 1589828088 },
+        { timestamp: 1589138088 },
+        { timestamp: 1589248088 },
+        { timestamp: 1589358088 },
+        { timestamp: 1589468088 },
+        { timestamp: 1589508088 },
+        { timestamp: 1589688088 },
+        { timestamp: 1589798088 },
+        { timestamp: 1589807088 },
+      ],
+      shape: "star",
+      color: "green",
+    },
   };
   return (
     <ChartContainer>
-      <LinearTimeline
-        data={mockData}
-        xAxisDataKey="timestamp"
-      />
+      <LinearTimeline data={mockData} xAxisDataKey="timestamp" />
     </ChartContainer>
   );
 };
