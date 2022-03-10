105e18fa831e0982da8d5ccf996f51f1aac62cb2
Josh Slaughter <8338893+jdslaugh@users.noreply.github.com>
2022-03-10 11:36:34 -0800

    feature: Add Project Catalog Search (#2091)
    * feature: Add Project Catalog Search

---
+++ frontend/workflows/projectCatalog/src/language-icon.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { faGolang, faJava, faJs, faPython, faRust } from "@fortawesome/free-brands-svg-icons";
+import { faGear, faTerminal } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+
+interface LanguageIconProps extends Pick<FontAwesomeIconProps, "size"> {
+  language: string;
+}
+
+const LanguageIcon = ({ language, size = "lg", ...props }: LanguageIconProps) => {
+  let icon;
+  switch (language) {
+    case "python":
+      icon = faPython;
+      break;
+    case "go":
+      icon = faGolang;
+      break;
+    case "bash":
+      icon = faTerminal;
+      break;
+    case "javascript":
+      icon = faJs;
+      break;
+    case "java":
+      icon = faJava;
+      break;
+    case "rust":
+      icon = faRust;
+      break;
+    default:
+      icon = faGear;
+  }
+
+  return <FontAwesomeIcon icon={icon} size={size} {...props} />;
+};
+
+export default LanguageIcon;
