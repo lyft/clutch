3d107c261ba27568ba245b2b008ed700a4a5b9af
Daniel Hochman <danielhochman@users.noreply.github.com>
2022-03-18 11:41:20 -0500

    regen api

--- backend/api/aws/kinesis/v1/kinesis.pb.gw.go
+++ backend/api/aws/kinesis/v1/kinesis.pb.gw.go
@@ -111,12 +111,13 @@ func RegisterKinesisAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/GetStream", runtime.WithHTTPPathPattern("/v1/aws/kinesis/getStream"))
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/GetStream", runtime.WithHTTPPathPattern("/v1/aws/kinesis/getStream"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_KinesisAPI_GetStream_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_KinesisAPI_GetStream_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -134,12 +135,13 @@ func RegisterKinesisAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/UpdateShardCount", runtime.WithHTTPPathPattern("/v1/aws/kinesis/updateShardCount"))
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/UpdateShardCount", runtime.WithHTTPPathPattern("/v1/aws/kinesis/updateShardCount"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_KinesisAPI_UpdateShardCount_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_KinesisAPI_UpdateShardCount_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -196,12 +198,13 @@ func RegisterKinesisAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/GetStream", runtime.WithHTTPPathPattern("/v1/aws/kinesis/getStream"))
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/GetStream", runtime.WithHTTPPathPattern("/v1/aws/kinesis/getStream"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_KinesisAPI_GetStream_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_KinesisAPI_GetStream_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -216,12 +219,13 @@ func RegisterKinesisAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/UpdateShardCount", runtime.WithHTTPPathPattern("/v1/aws/kinesis/updateShardCount"))
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/clutch.aws.kinesis.v1.KinesisAPI/UpdateShardCount", runtime.WithHTTPPathPattern("/v1/aws/kinesis/updateShardCount"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_KinesisAPI_UpdateShardCount_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_KinesisAPI_UpdateShardCount_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
