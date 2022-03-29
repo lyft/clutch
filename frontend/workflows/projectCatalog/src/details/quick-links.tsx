586aa2645b06335a53216d9cb2e1adde0c5ce38f
SHAWNA MONERO <smonero@lyft.com>
2022-03-29 15:32:41 -0700

    f

--- frontend/workflows/projectCatalog/src/details/quick-links.tsx
+++ frontend/workflows/projectCatalog/src/details/quick-links.tsx
@@ -11,13 +11,14 @@ import {
   Typography,
 } from "@clutch-sh/core";
 
+const ICON_SIZE = "32px";
 // If only a single link, then no popper is necessary
 const QuickLink = ({ link, linkGroupName, linkGroupImage }) => (
   <Grid item key={link.name}>
     <Tooltip title={linkGroupName}>
       <TooltipContainer>
         <Link href={link.url}>
-          <img width="32px" height="32px" src={linkGroupImage} alt={link.name} />
+          <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={link.name} />
         </Link>
       </TooltipContainer>
     </Tooltip>
@@ -33,67 +34,73 @@ const QuickLinkGroup = ({ linkGroupName, linkGroupImage, links }) => {
     <Grid item key={linkGroupName}>
       <Tooltip title={linkGroupName}>
         <TooltipContainer>
+          {/* 
+            We are disabling lint here until we come up with a better solution than putting the onClic() inside the <img> 
+
+            error  Visible, non-interactive elements with click handlers must have at least one keyboard listener  jsx-a11y/click-events-have-key-events
+            error  Non-interactive elements should not be assigned mouse or keyboard event listeners               jsx-a11y/no-noninteractive-element-interactions
+
+            TODO: don't disable it.
+          */}
           {/* eslint-disable */}
           <img
-            width="32px"
-            height="32px"
+            width={ICON_SIZE}
+            height={ICON_SIZE}
             src={linkGroupImage}
             alt={linkGroupName}
             onClick={() => setOpen(true)}
             ref={anchorRef}
           />
           <Popper open={open} anchorRef={anchorRef} onClickAway={() => setOpen(false)}>
-            {links.map(link => {
-              return (
-                <PopperItem key={link.name}>
-                  <Link href={link.url}>
-                    <Typography color="inherit" variant="body4">
-                      {link.name}
-                    </Typography>
-                  </Link>
-                </PopperItem>
-              );
-            })}
+            {links.map(link => (
+              <PopperItem key={link.name}>
+                <Link href={link.url}>
+                  <Typography color="inherit" variant="body4">
+                    {link.name}
+                  </Typography>
+                </Link>
+              </PopperItem>
+            ))}
           </Popper>
-          </TooltipContainer>
+        </TooltipContainer>
       </Tooltip>
     </Grid>
   );
 };
-export interface QuickLinksCardInput {
+export interface QuickLinksProps {
   linkGroups: IClutch.core.project.v1.ILinkGroup[];
 }
 
-const QuickLinksCard = ({ linkGroups }: QuickLinksCardInput) => (
-    <Card>
-      <Grid
-        container
-        item
-        direction="column"
-        alignItems="center"
-        spacing={1}
-        style={{ padding: "7px 0" }}
-      >
-        {linkGroups?.map(linkGroup => {
-          if (linkGroup.links?.length === 1) {
-            return (
-              <QuickLink
-                link={linkGroup.links[0]}
-                linkGroupName={linkGroup.name}
-                linkGroupImage={linkGroup.imagePath}
-              />
-            );
-          }
+const QuickLinksCard = ({ linkGroups }: QuickLinksProps) => (
+  <Card>
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      spacing={1}
+      style={{ padding: "10px 0" }}
+    >
+      {linkGroups?.map(linkGroup => {
+        if (linkGroup.links?.length === 1) {
           return (
-            <QuickLinkGroup
+            <QuickLink
+              link={linkGroup.links[0]}
               linkGroupName={linkGroup.name}
               linkGroupImage={linkGroup.imagePath}
-              links={linkGroup.links}
             />
           );
-        })}
-      </Grid>
-    </Card>
+        }
+        return (
+          <QuickLinkGroup
+            linkGroupName={linkGroup.name}
+            linkGroupImage={linkGroup.imagePath}
+            links={linkGroup.links}
+          />
+        );
+      })}
+    </Grid>
+  </Card>
 );
 
 export default QuickLinksCard;
