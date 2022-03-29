230f90e7abbb236c45f5ec6112c28b346d125922
Josh Slaughter <jslaughter@lyft.com>
2022-03-29 10:04:28 -0700

    Adjusting info resolver

--- frontend/workflows/projectCatalog/src/details/info/resolver.tsx
+++ frontend/workflows/projectCatalog/src/details/info/resolver.tsx
@@ -2,7 +2,35 @@ import type { clutch as IClutch } from "@clutch-sh/api";
 import { client } from "@clutch-sh/core";
 import { faGithub, faSlack } from "@fortawesome/free-brands-svg-icons";
 
-import type { ProjectInfo } from "./types";
+import type { ProjectInfo, ProjectRepository } from "./types";
+
+const getRepoData = (repo: string): ProjectRepository => {
+  const repoInfo: any = {};
+
+  const [base, splitProject] = repo.split(":");
+  const project = splitProject.replace(".git", "");
+  const [, manager] = base.split("@");
+
+  // nested project
+  if (project.indexOf("/") > 0) {
+    repoInfo.name = project.split("/").pop() || project;
+  } else {
+    repoInfo.name = project;
+  }
+
+  if (manager) {
+    repoInfo.url = `https://${manager}/${project}`;
+
+    switch (manager.toLowerCase()) {
+      case "github.com":
+        repoInfo.icon = faGithub;
+        break;
+      default: // no icon
+    }
+  }
+
+  return repoInfo as ProjectRepository;
+};
 
 const massageProjectInfo = (project: IClutch.core.project.v1.IProject = {}): ProjectInfo => {
   const messengerLink = (project?.linkGroups || []).find(lg => lg.name === "Slack");
@@ -13,12 +41,7 @@ const massageProjectInfo = (project: IClutch.core.project.v1.IProject = {}): Pro
     disabled: project?.data?.disabled as boolean,
     description: project?.data?.description as string,
     serviceIds: (project?.data?.pagerduty_service_ids || []) as string[],
-    repository: {
-      name: (project?.data?.repository as string).split("/")[1].split(".git")[0],
-      repo: project?.data?.repository as string,
-      url: "",
-      icon: faGithub,
-    },
+    repository: getRepoData(project?.data?.repository as string),
     messenger: {
       text: project?.data?.slack as string,
       icon: faSlack,
@@ -35,10 +58,12 @@ const massageProjectInfo = (project: IClutch.core.project.v1.IProject = {}): Pro
 };
 
 const fetchProject = (project: string): Promise<ProjectInfo> =>
-  client.post("/v1/project/getProjects", { projects: [project] }).then(resp => {
-    const { results = {} } = resp.data as IClutch.project.v1.GetProjectsResponse;
+  client
+    .post("/v1/project/getProjects", { projects: [project], exclude_dependencies: true })
+    .then(resp => {
+      const { results = {} } = resp.data as IClutch.project.v1.GetProjectsResponse;
 
-    return massageProjectInfo(results[project] ? results[project].project ?? {} : {});
-  });
+      return massageProjectInfo(results[project] ? results[project].project ?? {} : {});
+    });
 
 export default fetchProject;
