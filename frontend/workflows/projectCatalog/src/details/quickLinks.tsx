51f538fb3a6cafc97b296fef44d817fe05312c03
SHAWNA MONERO <smonero@lyft.com>
2022-03-29 09:55:16 -0700

    first attempt at qlinks

---
+++ frontend/workflows/projectCatalog/src/details/quickLinks.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import type { clutch as IClutch } from "@clutch-sh/api";
+import { Card, Grid } from "@clutch-sh/core";
+
+const QuickLinks = (project: IClutch.core.project.v1.IProject) => {
+  return (
+    <Card>
+      <Grid
+        container
+        item
+        direction="column"
+        alignItems="center"
+        spacing={1}
+        style={{ padding: "7px 0" }}
+      >
+        {project?.linkGroups?.map(linkGroup => {
+          return linkGroup.links?.map(link => {
+            return (
+              <Grid item key={link.id}>
+                <Link
+                  to={link.url}
+                  style={{
+                    textDecoration: "none",
+                    color: "inherit",
+                  }}
+                >
+                  <img src={link.imagePath} alt={link.name} />
+                </Link>
+              </Grid>
+            );
+          });
+        })}
+      </Grid>
+    </Card>
+  );
+};
+
+export default QuickLinks;
