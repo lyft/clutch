f81a4a0467dd9b3c17776130d5948705dd3fc98d
Shawna Monero <66325812+smonero@users.noreply.github.com>
2022-03-30 13:38:39 -0700

    FE: Project Catalog: Quicklinks with images (#2151)

---
+++ frontend/workflows/projectCatalog/src/details/quick-links.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import type { clutch as IClutch } from "@clutch-sh/api";
+import {
+  Card,
+  Grid,
+  Link,
+  Popper,
+  PopperItem,
+  Tooltip,
+  TooltipContainer,
+  Typography,
+} from "@clutch-sh/core";
+
+const ICON_SIZE = "32px";
+// If only a single link, then no popper is necessary
+const QuickLink = ({ link, linkGroupName, linkGroupImage }) => (
+  <Grid item key={link.name}>
+    <Tooltip title={linkGroupName}>
+      <TooltipContainer>
+        <Link href={link.url}>
+          <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={link.name} />
+        </Link>
+      </TooltipContainer>
+    </Tooltip>
+  </Grid>
+);
+
+interface QuickLinkGroupProps {
+  linkGroupName: string;
+  linkGroupImage: string;
+  links: IClutch.core.project.v1.ILink[];
+}
+// Have a popper in the case of multiple links per group
+const QuickLinkGroup = ({ linkGroupName, linkGroupImage, links }: QuickLinkGroupProps) => {
+  const anchorRef = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Grid item key={linkGroupName}>
+      <Tooltip title={linkGroupName}>
+        <TooltipContainer>
+          <button
+            type="button"
+            style={{ padding: 0, background: "transparent", border: "0", cursor: "pointer" }}
+            ref={anchorRef}
+            onClick={() => setOpen(true)}
+          >
+            <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={linkGroupName} />
+          </button>
+          <Popper open={open} anchorRef={anchorRef} onClickAway={() => setOpen(false)}>
+            {links.map(link => (
+              <PopperItem key={link.name}>
+                <Link href={link.url}>
+                  <Typography color="inherit" variant="body4">
+                    {link.name}
+                  </Typography>
+                </Link>
+              </PopperItem>
+            ))}
+          </Popper>
+        </TooltipContainer>
+      </Tooltip>
+    </Grid>
+  );
+};
+export interface QuickLinksProps {
+  linkGroups: IClutch.core.project.v1.ILinkGroup[];
+}
+
+const QuickLinksCard = ({ linkGroups }: QuickLinksProps) => (
+  <Card>
+    <Grid
+      container
+      item
+      direction="column"
+      alignItems="center"
+      spacing={1}
+      style={{ padding: "8px" }}
+    >
+      {linkGroups?.map(linkGroup => {
+        if (linkGroup.links?.length === 1) {
+          return (
+            <QuickLink
+              link={linkGroup.links[0]}
+              linkGroupName={linkGroup.name}
+              linkGroupImage={linkGroup.imagePath}
+            />
+          );
+        }
+        return (
+          <QuickLinkGroup
+            linkGroupName={linkGroup.name}
+            linkGroupImage={linkGroup.imagePath}
+            links={linkGroup.links}
+          />
+        );
+      })}
+    </Grid>
+  </Card>
+);
+
+export default QuickLinksCard;
