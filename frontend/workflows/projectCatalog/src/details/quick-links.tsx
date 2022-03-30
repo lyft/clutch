0005a92f0d290ef85afc0f558180a2565d9335a0
Josh Slaughter <jslaughter@lyft.com>
2022-03-30 14:16:58 -0700

    Fixing build issues

--- frontend/workflows/projectCatalog/src/details/quick-links.tsx
+++ frontend/workflows/projectCatalog/src/details/quick-links.tsx
@@ -11,23 +11,53 @@ import {
   Typography,
 } from "@clutch-sh/core";
 
+interface LinkGroupProps {
+  linkGroupName: string;
+  linkGroupImage: string;
+}
+
+interface QuickLinkProps extends LinkGroupProps {
+  link: IClutch.core.project.v1.ILink;
+}
+
+interface QuickLinkTooltip {
+  key: string;
+  name: string;
+  children: React.ReactNode;
+}
+
 const ICON_SIZE = "32px";
-// If only a single link, then no popper is necessary
-const QuickLink = ({ link, linkGroupName, linkGroupImage }) => (
-  <Grid item key={link.name}>
-    <Tooltip title={linkGroupName}>
-      <TooltipContainer>
-        <Link href={link.url}>
-          <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={link.name} />
-        </Link>
-      </TooltipContainer>
+
+const QuickLinkTooltipContainer = ({ key, name, children }: QuickLinkTooltip) => (
+  <Grid item key={key}>
+    <Tooltip title={name}>
+      <TooltipContainer>{children}</TooltipContainer>
     </Tooltip>
   </Grid>
 );
 
-interface QuickLinkGroupProps {
-  linkGroupName: string;
-  linkGroupImage: string;
+// If only a single link, then no popper is necessary
+const QuickLink = ({ link, linkGroupName, linkGroupImage }: QuickLinkProps) => {
+  const container = (
+    <Link href={link.url ?? undefined}>
+      <img
+        width={ICON_SIZE}
+        height={ICON_SIZE}
+        src={linkGroupImage}
+        alt={link.name ?? "Quick Link"}
+      />
+    </Link>
+  );
+  return linkGroupName ? (
+    <QuickLinkTooltipContainer key={link.name ?? ""} name={linkGroupName}>
+      {container}
+    </QuickLinkTooltipContainer>
+  ) : (
+    container
+  );
+};
+
+interface QuickLinkGroupProps extends LinkGroupProps {
   links: IClutch.core.project.v1.ILink[];
 }
 // Have a popper in the case of multiple links per group
@@ -35,32 +65,36 @@ const QuickLinkGroup = ({ linkGroupName, linkGroupImage, links }: QuickLinkGroup
   const anchorRef = React.useRef(null);
   const [open, setOpen] = React.useState(false);
 
-  return (
-    <Grid item key={linkGroupName}>
-      <Tooltip title={linkGroupName}>
-        <TooltipContainer>
-          <button
-            type="button"
-            style={{ padding: 0, background: "transparent", border: "0", cursor: "pointer" }}
-            ref={anchorRef}
-            onClick={() => setOpen(true)}
-          >
-            <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={linkGroupName} />
-          </button>
-          <Popper open={open} anchorRef={anchorRef} onClickAway={() => setOpen(false)}>
-            {links.map(link => (
-              <PopperItem key={link.name}>
-                <Link href={link.url}>
-                  <Typography color="inherit" variant="body4">
-                    {link.name}
-                  </Typography>
-                </Link>
-              </PopperItem>
-            ))}
-          </Popper>
-        </TooltipContainer>
-      </Tooltip>
-    </Grid>
+  const container = (
+    <>
+      <button
+        type="button"
+        style={{ padding: 0, background: "transparent", border: "0", cursor: "pointer" }}
+        ref={anchorRef}
+        onClick={() => setOpen(true)}
+      >
+        <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={linkGroupName} />
+      </button>
+      <Popper open={open} anchorRef={anchorRef} onClickAway={() => setOpen(false)}>
+        {links.map(link => (
+          <PopperItem key={link.name}>
+            <Link href={link.url ?? undefined}>
+              <Typography color="inherit" variant="body4">
+                {link.name}
+              </Typography>
+            </Link>
+          </PopperItem>
+        ))}
+      </Popper>
+    </>
+  );
+
+  return linkGroupName ? (
+    <QuickLinkTooltipContainer key={linkGroupName} name={linkGroupName}>
+      {container}
+    </QuickLinkTooltipContainer>
+  ) : (
+    container
   );
 };
 export interface QuickLinksProps {
@@ -77,21 +111,21 @@ const QuickLinksCard = ({ linkGroups }: QuickLinksProps) => (
       spacing={1}
       style={{ padding: "8px" }}
     >
-      {linkGroups?.map(linkGroup => {
+      {(linkGroups || []).map(linkGroup => {
         if (linkGroup.links?.length === 1) {
           return (
             <QuickLink
               link={linkGroup.links[0]}
-              linkGroupName={linkGroup.name}
-              linkGroupImage={linkGroup.imagePath}
+              linkGroupName={linkGroup.name ?? ""}
+              linkGroupImage={linkGroup.imagePath ?? ""}
             />
           );
         }
         return (
           <QuickLinkGroup
-            linkGroupName={linkGroup.name}
-            linkGroupImage={linkGroup.imagePath}
-            links={linkGroup.links}
+            linkGroupName={linkGroup.name ?? ""}
+            linkGroupImage={linkGroup.imagePath ?? ""}
+            links={linkGroup?.links ?? []}
           />
         );
       })}
