8ee8d5387fa6659c22ddcd0ad1b664761681d80c
Jun Gan <jungan@lyft.com>
2022-03-31 14:21:47 -0700

    github: set commit time and auto stage modified and deleted files

--- backend/service/github/github.go
+++ backend/service/github/github.go
@@ -299,8 +299,8 @@ type CreateBranchRequest struct {
 	SingleBranch bool
 }
 
-func commitOptionsFromClaims(ctx context.Context) *git.CommitOptions {
-	ret := &git.CommitOptions{Author: &object.Signature{}}
+func commitOptionsFromClaims(ctx context.Context, commitTime time.Time) *git.CommitOptions {
+	ret := &git.CommitOptions{Author: &object.Signature{When: commitTime}, All: true}
 
 	subject := "Anonymous User" // Used if auth is disabled or it's the actual anonymous user.
 	if claims, err := authn.ClaimsFromContext(ctx); err == nil && claims.Subject != authn.AnonymousSubject {
@@ -357,7 +357,7 @@ func (s *svc) CreateBranch(ctx context.Context, req *CreateBranchRequest) error 
 		}
 	}
 
-	opts := commitOptionsFromClaims(ctx)
+	opts := commitOptionsFromClaims(ctx, time.Now())
 	if _, err := wt.Commit(req.CommitMessage, opts); err != nil {
 		return err
 	}
