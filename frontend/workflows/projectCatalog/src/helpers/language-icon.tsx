ce0881a4e1b8c4bb5e1b70ab74f8d6e33923c834
Daniel Hochman <danielhochman@users.noreply.github.com>
2022-04-05 22:24:35 -0500

    frontend: add language title and plural support to project catalog details (#2168)

--- frontend/workflows/projectCatalog/src/helpers/language-icon.tsx
+++ frontend/workflows/projectCatalog/src/helpers/language-icon.tsx
@@ -9,30 +9,38 @@ interface LanguageIconProps extends Pick<FontAwesomeIconProps, "size"> {
 
 const LanguageIcon = ({ language, size = "lg", ...props }: LanguageIconProps) => {
   let icon;
+  let title;
   switch (language) {
     case "python":
       icon = faPython;
+      title = "Python";
       break;
     case "go":
       icon = faGolang;
+      title = "Golang";
       break;
     case "bash":
       icon = faTerminal;
+      title = "Shell";
       break;
     case "javascript":
       icon = faJs;
+      title = "JavaScript";
       break;
     case "java":
       icon = faJava;
+      title = "Java";
       break;
     case "rust":
       icon = faRust;
+      title = "Rust";
       break;
     default:
       icon = faGear;
+      title = "None detected";
   }
 
-  return <FontAwesomeIcon icon={icon} size={size} {...props} />;
+  return <FontAwesomeIcon title={title} icon={icon} size={size} {...props} />;
 };
 
 export default LanguageIcon;
