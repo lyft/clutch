403739eb0135bc1366d90a9b7f2ac1b9b43bc3c6
SHAWNA MONERO <smonero@lyft.com>
2022-04-11 20:25:05 -0700

    fix

---
+++ frontend/workflows/projectSelector/src/project-links.tsx
@@ -0,0 +1,152 @@
+import React from "react";
+import type { clutch as IClutch } from "@clutch-sh/api";
+import { Grid, Link, Popper, Tooltip, TooltipContainer, Typography } from "@clutch-sh/core";
+import styled from "@emotion/styled";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+interface LinkGroupProps {
+  linkGroupName: string;
+  linkGroupImage: string;
+}
+
+interface QuickLinkProps extends LinkGroupProps {
+  link: IClutch.core.project.v1.ILink;
+}
+
+interface QuickLinkContainerProps {
+  key: string | null | undefined;
+  name: string;
+  children: React.ReactNode;
+}
+
+const ICON_SIZE = "22px";
+
+const QuickLinkContainer = ({ key, name, children }: QuickLinkContainerProps) => {
+  const container = (
+    <Tooltip title={name}>
+      <TooltipContainer>{children}</TooltipContainer>
+    </Tooltip>
+  );
+
+  return (
+    <Grid item key={key ?? ""}>
+      {name ? container : children}
+    </Grid>
+  );
+};
+
+const StyledMoreVertIcon = styled.span({
+  width: "12px",
+  ".MuiIconButton-root": {
+    padding: "6px",
+    color: "rgba(13, 16, 48, 0.38)",
+  },
+  ".MuiIconButton-root:hover": {
+    backgroundColor: "rgb(245, 246, 253)",
+  },
+  ".MuiIconButton-root:active": {
+    backgroundColor: "rgba(0,0,0, 0.1)",
+  },
+});
+
+const QuickLink = ({ link, linkGroupName, linkGroupImage }: QuickLinkProps) =>
+  link?.url ? (
+    <QuickLinkContainer key={link.name} name={linkGroupName}>
+      <Link href={link.url}>
+        <img
+          width={ICON_SIZE}
+          height={ICON_SIZE}
+          src={linkGroupImage}
+          alt={link.name ?? `Quick Link to ${link.url}`}
+        />
+        <span style={{ padding: "8px" }}>{linkGroupName}</span>
+      </Link>
+    </QuickLinkContainer>
+  ) : null;
+
+interface QuickLinkGroupProps extends LinkGroupProps {
+  links: IClutch.core.project.v1.ILink[];
+}
+// Have a popper in the case of multiple links per group
+const QuickLinkGroup = ({ linkGroupName, linkGroupImage, links }: QuickLinkGroupProps) => {
+  const [validLinks, setValidLinks] = React.useState<IClutch.core.project.v1.ILink[]>([]);
+
+  React.useEffect(() => {
+    if (links) {
+      setValidLinks(links.filter(link => link?.url && link.url.length > 0));
+    }
+  }, [links]);
+
+  return (
+    <QuickLinkContainer key={linkGroupName} name={linkGroupName}>
+      <img width={ICON_SIZE} height={ICON_SIZE} src={linkGroupImage} alt={linkGroupName} />
+      <span style={{ padding: "8px" }}>{linkGroupName}</span>
+      <div>
+        {validLinks.map(link => {
+          return (
+            link?.url && (
+              <Link href={link.url}>
+                <Typography color="inherit" variant="body4">
+                  {link.name}
+                </Typography>
+              </Link>
+            )
+          );
+        })}
+      </div>
+    </QuickLinkContainer>
+  );
+};
+
+const ExpandedLinks = ({ linkGroups }) => (
+  <Grid
+    container
+    item
+    direction="column"
+    alignItems="center"
+    spacing={1}
+    style={{ padding: "8px" }}
+  >
+    {(linkGroups || []).map(linkGroup => {
+      if (linkGroup.links?.length === 1) {
+        return (
+          <QuickLink
+            link={linkGroup.links[0]}
+            linkGroupName={linkGroup.name ?? ""}
+            linkGroupImage={linkGroup.imagePath ?? ""}
+          />
+        );
+      }
+      return (
+        <QuickLinkGroup
+          linkGroupName={linkGroup.name ?? ""}
+          linkGroupImage={linkGroup.imagePath ?? ""}
+          links={linkGroup?.links ?? []}
+        />
+      );
+    })}
+  </Grid>
+);
+
+const ProjectLinks = ({ linkGroups }) => {
+  const anchorRef = React.useRef(null);
+  const [open, setOpen] = React.useState(false);
+
+  if (!linkGroups) {
+    return null;
+  }
+
+  return (
+    <StyledMoreVertIcon>
+      <IconButton ref={anchorRef} onClick={() => setOpen(true)}>
+        <MoreVertIcon />
+        <Popper open={open} anchorRef={anchorRef} onClickAway={() => setOpen(false)}>
+          <ExpandedLinks linkGroups={linkGroups} />
+        </Popper>
+      </IconButton>
+    </StyledMoreVertIcon>
+  );
+};
+
+export default ProjectLinks;
