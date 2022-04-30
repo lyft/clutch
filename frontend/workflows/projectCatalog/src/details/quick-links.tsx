2b86bdd2604a4b3acd44e3e46ef5f692662a1fb2
SHAWNA MONERO <smonero@lyft.com>
2022-04-29 22:54:08 -0700

    fix alignment

--- frontend/workflows/projectCatalog/src/details/quick-links.tsx
+++ frontend/workflows/projectCatalog/src/details/quick-links.tsx
@@ -78,7 +78,7 @@ const QuickLinkGroup = ({ linkGroupName, linkGroupImage, links }: QuickLinkGroup
     <QuickLinkContainer key={linkGroupName} name={linkGroupName}>
       <button
         type="button"
-        style={{ padding: 0, background: "transparent", border: "0", cursor: "pointer" }}
+        style={{ padding: 0, background: "transparent", border: "0", cursor: "pointer", display: "flex" }}
         ref={anchorRef}
         onClick={() => setOpen(true)}
       >
