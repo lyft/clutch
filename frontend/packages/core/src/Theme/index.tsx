93e3c0bfdbc1027bbabd4c2f81f550a08ee48a6f
Josh Slaughter <jslaughter@lyft.com>
2022-05-31 13:56:12 -0700

    Running codemods against Theme

--- frontend/packages/core/src/Theme/index.tsx
+++ frontend/packages/core/src/Theme/index.tsx
@@ -1,7 +1,4 @@
-// Need a function that can return colors even outside of the theme where folks can reference them through imports
-// ideally this would be something like a function per color where we return based on
-// the current mode.
-import { fade } from "@material-ui/core/styles/colorManipulator";
+import { alpha } from "@mui/material/styles";
 
 import { STATE_OPACITY } from "./colors";
 import ThemeProvider from "./theme";
@@ -9,7 +6,7 @@ import type { ComponentState } from "./types";
 
 // Return the appropriate color for a specified state and color.
 const stateColor = (state: ComponentState, color: string) => {
-  return fade(color, STATE_OPACITY[state]);
+  return alpha(color, STATE_OPACITY[state]);
 };
 
 // TODO: Use the ThemeProvider once the Theme component can be replaced
