00bd524846adeb1d84b096ce87c6cec24a507a50
Josh Slaughter <jslaughter@lyft.com>
2022-05-31 12:28:48 -0700

    Running codemods against wizard

--- frontend/packages/wizard/src/wizard.tsx
+++ frontend/packages/wizard/src/wizard.tsx
@@ -16,7 +16,7 @@ import {
 } from "@clutch-sh/core";
 import type { ManagerLayout } from "@clutch-sh/data-layout";
 import { DataLayoutContext, useDataLayoutManager } from "@clutch-sh/data-layout";
-import { Container as MuiContainer, Grid, Paper as MuiPaper, Typography } from "@material-ui/core";
+import { Container as MuiContainer, Grid, Paper as MuiPaper, Typography } from "@mui/material";
 
 import { useWizardState, WizardAction } from "./state";
 import type { WizardStepProps } from "./step";
@@ -131,12 +131,12 @@ const Wizard = ({ heading, width = "default", dataLayout, children }: WizardProp
         <DataLayoutContext.Provider value={dataLayoutManager}>
           {/* eslint-disable-next-line react/jsx-no-constructed-context-values */}
           <WizardContext.Provider value={() => context(child)}>
-            <Grid container direction="column" justify="center" alignItems="center">
+            <Grid container direction="column" justifyContent="center" alignItems="center">
               {child}
             </Grid>
           </WizardContext.Provider>
         </DataLayoutContext.Provider>
-        <Grid container justify="center">
+        <Grid container justifyContent="center">
           {((state.activeStep === lastStepIndex && !isLoading) || hasError) && (
             <>
               <SimpleFeatureFlag feature="npsWizard">
@@ -175,7 +175,7 @@ const Wizard = ({ heading, width = "default", dataLayout, children }: WizardProp
       <Grid
         container
         direction="column"
-        justify="center"
+        justifyContent="center"
         alignItems="stretch"
         style={{ display: "inline" }}
       >
