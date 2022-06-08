25bfe1d24d85b9e185ed93c6073ca5cd9b863a3a
SHAWNA MONERO <smonero@lyft.com>
2022-06-07 17:33:39 -0700

    lint

--- frontend/packages/core/src/Resolver/input.tsx
+++ frontend/packages/core/src/Resolver/input.tsx
@@ -43,7 +43,7 @@ const autoComplete = async (type: string, search: string): Promise<any> => {
 
   const response = await client.post("/v1/resolver/autocomplete", {
     want: `type.googleapis.com/${type}`,
-    search: search,
+    search,
     caseInsensitive: true,
   });
 
