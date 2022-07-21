633ab19aeff94b4a3325c461fc03abfd2b2289f9
Josh Slaughter <8338893+jdslaugh@users.noreply.github.com>
2022-07-21 03:01:02 -0700

    muiv5 - Breaking Apart - Wizard Changes (#2304)

--- frontend/packages/wizard/src/step.tsx
+++ frontend/packages/wizard/src/step.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import type { ClutchError } from "@clutch-sh/core";
-import { Error, Loadable, styled, useWizardContext } from "@clutch-sh/core";
-import { Grid as MuiGrid } from "@material-ui/core";
+import { Error, Grid as ClutchGrid, Loadable, styled, useWizardContext } from "@clutch-sh/core";
 
-const Grid = styled(MuiGrid)({
+const Grid = styled(ClutchGrid)({
   width: "100%",
   "> *": {
     margin: "8px 0",
@@ -29,7 +28,7 @@ const WizardStep: React.FC<WizardStepProps> = ({ isLoading, error, children }) =
     return <Loadable isLoading={isLoading}>{children}</Loadable>;
   }
   return (
-    <Grid container justify="center" direction="column" alignItems="stretch">
+    <Grid container justifyContent="center" direction="column" alignItems="stretch">
       {hasError ? <Error subject={error} /> : children}
     </Grid>
   );
