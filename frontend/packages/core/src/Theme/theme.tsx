ee7bf0c8befab0465ab22ece8b1b45e18ac845b8
Josh Slaughter <8338893+jdslaugh@users.noreply.github.com>
2022-08-02 13:58:48 -0700

    muiv5 - Breaking Apart - Core/Layout Changes (#2307)
    * muiv5 - Breaking Apart - Core/Layout Changes

--- frontend/packages/core/src/Theme/theme.tsx
+++ frontend/packages/core/src/Theme/theme.tsx
@@ -1,40 +1,52 @@
 import React from "react";
 import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
-import type { Theme as MuiTheme } from "@material-ui/core";
-import { createMuiTheme, CssBaseline, MuiThemeProvider } from "@material-ui/core";
-import { StylesProvider } from "@material-ui/styles";
+import type { Theme as MuiTheme } from "@mui/material";
+import {
+  adaptV4Theme,
+  createTheme as createMuiTheme,
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider as MuiThemeProvider,
+} from "@mui/material";
+import { StylesProvider } from "@mui/styles";
 
 import { clutchColors } from "./colors";
 import palette from "./palette";
 import type { ThemeVariant } from "./types";
 
+declare module "@mui/styles/defaultTheme" {
+  interface DefaultTheme extends MuiTheme {}
+}
+
 // Create a Material UI theme is propagated to all children.
 const createTheme = (variant: ThemeVariant): MuiTheme => {
-  return createMuiTheme({
-    // inject in custom colors
-    ...clutchColors(variant),
-    palette: palette(variant),
-    transitions: {
-      // https://material-ui.com/getting-started/faq/#how-can-i-disable-transitions-globally
-      create: () => "none",
-    },
-    props: {
-      MuiButtonBase: {
-        // https://material-ui.com/getting-started/faq/#how-can-i-disable-the-ripple-effect-globally
-        disableRipple: true,
+  return createMuiTheme(
+    adaptV4Theme({
+      // inject in custom colors
+      ...clutchColors(variant),
+      palette: palette(variant),
+      transitions: {
+        // https://material-ui.com/getting-started/faq/#how-can-i-disable-transitions-globally
+        create: () => "none",
       },
-    },
-    overrides: {
-      MuiAccordion: {
-        root: {
-          "&$expanded": {
-            // remove the additional margin rule when expanded so the original margin is used.
-            margin: null,
+      props: {
+        MuiButtonBase: {
+          // https://material-ui.com/getting-started/faq/#how-can-i-disable-the-ripple-effect-globally
+          disableRipple: true,
+        },
+      },
+      overrides: {
+        MuiAccordion: {
+          root: {
+            "&$expanded": {
+              // remove the additional margin rule when expanded so the original margin is used.
+              margin: null,
+            },
           },
         },
       },
-    },
-  });
+    })
+  );
 };
 
 interface ThemeProps {
@@ -43,12 +55,14 @@ interface ThemeProps {
 }
 
 const ThemeProvider = ({ children, variant = "light" }: ThemeProps) => (
-  <MuiThemeProvider theme={createTheme(variant)}>
-    <EmotionThemeProvider theme={createTheme(variant)}>
-      <CssBaseline />
-      <StylesProvider injectFirst>{children}</StylesProvider>
-    </EmotionThemeProvider>
-  </MuiThemeProvider>
+  <StyledEngineProvider injectFirst>
+    <MuiThemeProvider theme={createTheme(variant)}>
+      <EmotionThemeProvider theme={createTheme(variant)}>
+        <CssBaseline />
+        <StylesProvider injectFirst>{children}</StylesProvider>
+      </EmotionThemeProvider>
+    </MuiThemeProvider>
+  </StyledEngineProvider>
 );
 
 // Note that ThemeProvider can't be used until the Theme component can be replaced.
