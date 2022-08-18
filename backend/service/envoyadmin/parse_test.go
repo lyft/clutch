429acf3ff81867dc80ae2478faf9d7f3ed166b21
Derek <dschaller@users.noreply.github.com>
2022-08-17 21:04:54 -0700

    backend: fix nil pointer in envoy admin parser (#2346)

---
+++ backend/service/envoyadmin/parse_test.go
@@ -0,0 +1,36 @@
+package envoyadmin
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	envoytriagev1 "github.com/lyft/clutch/backend/api/envoytriage/v1"
+)
+
+func TestNodeMetadataFromResponse(t *testing.T) {
+	testCases := []struct {
+		resp     []byte
+		metadata *envoytriagev1.NodeMetadata
+		hasErr   bool
+	}{
+		// resp has no command line options
+		{
+			resp:     []byte("{}"),
+			metadata: &envoytriagev1.NodeMetadata{},
+			hasErr:   false,
+		},
+	}
+
+	for _, test := range testCases {
+		metadata, err := nodeMetadataFromResponse(test.resp)
+		if test.hasErr {
+			assert.Error(t, err)
+			assert.Nil(t, metadata)
+		} else {
+			assert.NoError(t, err)
+			assert.NotNil(t, metadata)
+			assert.Equal(t, test.metadata, metadata)
+		}
+	}
+}
