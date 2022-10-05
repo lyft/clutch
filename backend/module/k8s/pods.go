72a5ff5735e95f78fd8711592d4a552532eb7acd
Yiwen Gao <ygao@lyft.com>
2022-10-04 18:13:27 -0700

    Add partial failures to module

--- backend/module/k8s/pods.go
+++ backend/module/k8s/pods.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	k8sapiv1 "github.com/lyft/clutch/backend/api/k8s/v1"
+	"google.golang.org/genproto/googleapis/rpc/status"
 )
 
 func (a *k8sAPI) DescribePod(ctx context.Context, req *k8sapiv1.DescribePodRequest) (*k8sapiv1.DescribePodResponse, error) {
@@ -24,10 +25,17 @@ func (a *k8sAPI) DeletePod(ctx context.Context, req *k8sapiv1.DeletePodRequest) 
 
 func (a *k8sAPI) ListPods(ctx context.Context, req *k8sapiv1.ListPodsRequest) (*k8sapiv1.ListPodsResponse, error) {
 	pods, err := a.k8s.ListPods(ctx, req.Clientset, req.Cluster, req.Namespace, req.Options)
-	if err != nil {
+	// If there's a complete failure, return it directly
+	// If there are only partial failures, set it on the response
+	if len(pods) == 0 && err != nil {
 		return nil, err
 	}
-	return &k8sapiv1.ListPodsResponse{Pods: pods}, nil
+
+	result := &k8sapiv1.ListPodsResponse{Pods: pods}
+	if err != nil {
+		result.PartialFailures = []*status.Status{{Message: err.Error()}}
+	}
+	return result, nil
 }
 
 func (a *k8sAPI) UpdatePod(ctx context.Context, req *k8sapiv1.UpdatePodRequest) (*k8sapiv1.UpdatePodResponse, error) {
