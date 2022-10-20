61c0d83a71c044848c2b74d6c907924d6a41ee9c
francisco souza <108725+fsouza@users.noreply.github.com>
2022-10-19 22:52:52 -0400

    tools/golangci-lint: upgrade to 1.47.3 and compile it
    To avoid issues, golangci-lint should be compile with the same version
    of the Go.

--- tools/golangci-lint.sh
+++ tools/golangci-lint.sh
@@ -6,13 +6,8 @@ BUILD_ROOT="${REPO_ROOT}/build"
 BUILD_BIN="${BUILD_ROOT}/bin"
 
 NAME=golangci-lint
-RELEASE=v1.42.1
-OSX_RELEASE_256=9c0042e91218dc1dd4eb7b54e29c7331eff081b3ac3f88b0d5df89b976fcd45c
-LINUX_RELEASE_256=214b093c15863430c4b66dd39df677dab6e38fc873ded147e331740d50eea51f
-
-ARCH=amd64
-
-RELEASE_BINARY="${BUILD_BIN}/${NAME}-${RELEASE}"
+RELEASE=v1.47.3
+RELEASE_BINARY="${BUILD_BIN}/${NAME}"
 
 main() {
   cd "${REPO_ROOT}/backend"
@@ -26,31 +21,7 @@ ensure_binary() {
     echo "info: Downloading ${NAME} ${RELEASE} to build environment"
 
     mkdir -p "${BUILD_BIN}"
-
-    case "${OSTYPE}" in
-      "darwin"*) os_type="darwin"; sum="${OSX_RELEASE_256}" ;;
-      "linux"*) os_type="linux"; sum="${LINUX_RELEASE_256}" ;;
-      *) echo "error: Unsupported OS '${OSTYPE}' for shellcheck install, please install manually" && exit 1 ;;
-    esac
-
-    release_archive="/tmp/${NAME}-${RELEASE}.tar.gz"
-
-    URL="https://github.com/golangci/golangci-lint/releases/download/${RELEASE}/golangci-lint-${RELEASE:1}-${os_type}-${ARCH}.tar.gz"
-    curl -sSL -o "${release_archive}" "${URL}"
-    echo ${sum} ${release_archive} | sha256sum --check --quiet -
-
-    release_tmp_dir="/tmp/${NAME}-${RELEASE}"
-    mkdir -p "${release_tmp_dir}"
-    tar -xzf "${release_archive}" --strip=1 -C "${release_tmp_dir}"
-
-    if [[ ! -f "${RELEASE_BINARY}" ]]; then
-      find "${BUILD_BIN}" -maxdepth 0 -regex '.*/'${NAME}'-[A-Za-z0-9\.]+$' -exec rm {} \;  # cleanup older versions
-      mv "${release_tmp_dir}/${NAME}" "${RELEASE_BINARY}"
-    fi
-
-    # Cleanup stale resources.
-    rm "${release_archive}"
-    rm -rf "${release_tmp_dir}"
+    env GOBIN="${BUILD_BIN}" go install github.com/golangci/golangci-lint/cmd/golangci-lint@"${RELEASE}"
   fi
 }
 
