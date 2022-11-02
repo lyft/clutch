1c92467ee108e87d2a51d26c975d038b67653d22
Derek Schaller <d_a_schaller@yahoo.com>
2022-11-02 14:32:08 -0700

    api: audit api - add pagination and individual event api

--- backend/api/audit/v1/audit_grpc.pb.go
+++ backend/api/audit/v1/audit_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type AuditAPIClient interface {
 	GetEvents(ctx context.Context, in *GetEventsRequest, opts ...grpc.CallOption) (*GetEventsResponse, error)
+	GetEvent(ctx context.Context, in *GetEventRequest, opts ...grpc.CallOption) (*GetEventResponse, error)
 }
 
 type auditAPIClient struct {
@@ -42,11 +43,21 @@ func (c *auditAPIClient) GetEvents(ctx context.Context, in *GetEventsRequest, op
 	return out, nil
 }
 
+func (c *auditAPIClient) GetEvent(ctx context.Context, in *GetEventRequest, opts ...grpc.CallOption) (*GetEventResponse, error) {
+	out := new(GetEventResponse)
+	err := c.cc.Invoke(ctx, "/clutch.audit.v1.AuditAPI/GetEvent", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuditAPIServer is the server API for AuditAPI service.
 // All implementations should embed UnimplementedAuditAPIServer
 // for forward compatibility
 type AuditAPIServer interface {
 	GetEvents(context.Context, *GetEventsRequest) (*GetEventsResponse, error)
+	GetEvent(context.Context, *GetEventRequest) (*GetEventResponse, error)
 }
 
 // UnimplementedAuditAPIServer should be embedded to have forward compatible implementations.
@@ -55,6 +66,9 @@ type UnimplementedAuditAPIServer struct {
 
 func (UnimplementedAuditAPIServer) GetEvents(context.Context, *GetEventsRequest) (*GetEventsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetEvents not implemented")
+}
+func (UnimplementedAuditAPIServer) GetEvent(context.Context, *GetEventRequest) (*GetEventResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetEvent not implemented")
 }
 
 // UnsafeAuditAPIServer may be embedded to opt out of forward compatibility for this service.
@@ -86,6 +100,24 @@ func _AuditAPI_GetEvents_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+func _AuditAPI_GetEvent_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetEventRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuditAPIServer).GetEvent(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/clutch.audit.v1.AuditAPI/GetEvent",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuditAPIServer).GetEvent(ctx, req.(*GetEventRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // AuditAPI_ServiceDesc is the grpc.ServiceDesc for AuditAPI service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -96,6 +128,10 @@ var AuditAPI_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetEvents",
 			Handler:    _AuditAPI_GetEvents_Handler,
+		},
+		{
+			MethodName: "GetEvent",
+			Handler:    _AuditAPI_GetEvent_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
