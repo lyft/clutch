bef7c261ac97409083f0abedf0f43b9c4a8126be
Derek Schaller <d_a_schaller@yahoo.com>
2022-11-16 03:00:44 +0800

    update test

--- frontend/packages/core/src/Input/tests/date-time.test.tsx
+++ frontend/packages/core/src/Input/tests/date-time.test.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
+import { DateTimePicker as MuiDateTimePicker } from "@mui/x-date-pickers";
+import type { ReactWrapper } from "enzyme";
 import { mount } from "enzyme";
 
+import type { DateTimePickerProps } from "../date-time";
 import DateTimePicker from "../date-time";
 
 describe("DateTimePicker", () => {
   describe("TextField", () => {
-    let component;
+    const onChange = jest.fn();
+    let component: ReactWrapper<DateTimePickerProps>;
     beforeAll(() => {
-      component = mount(<DateTimePicker value={new Date()} onChange={() => {}} />);
+      component = mount(<DateTimePicker value={new Date()} onChange={onChange} />);
     });
 
     it("has padding", () => {
@@ -16,6 +20,28 @@ describe("DateTimePicker", () => {
       expect(getComputedStyle(adornedInput.getDOMNode()).getPropertyValue("padding-right")).toBe(
         "14px"
       );
+    });
+
+    describe("onChange callback", () => {
+      beforeEach(() => {
+        onChange.mockReset();
+      });
+
+      it("is called when valid value", () => {
+        component = mount(<DateTimePicker value={new Date()} onChange={onChange} />);
+        const input = component.find("input");
+        expect(input).toHaveLength(1);
+        input.simulate("change", { target: { value: "11/16/2023 02:55 AM" } });
+        expect(onChange).toHaveBeenCalled();
+      });
+
+      it("is not called with invalid value", () => {
+        component = mount(<DateTimePicker value={new Date()} onChange={onChange} />);
+        const input = component.find("input");
+        expect(input).toHaveLength(1);
+        input.simulate("change", { target: { value: "invalid" } });
+        expect(onChange).not.toHaveBeenCalled();
+      });
     });
   });
 
@@ -33,22 +59,22 @@ describe("DateTimePicker", () => {
 
     it("value", () => {
       expect(component).toHaveLength(1);
-      const input = component.find("ForwardRef(DateTimePicker)");
+      const input = component.find(MuiDateTimePicker);
       expect(input.props().value).toBe(date);
     });
 
     it("onChange", () => {
-      const input = component.find("ForwardRef(DateTimePicker)");
-      expect(input.props().onChange).toBe(onChange);
+      const input = component.find(MuiDateTimePicker);
+      expect(input.props().onChange).toBeDefined();
     });
 
     it("label", () => {
-      const outline = component.find("ForwardRef(DateTimePicker)");
+      const outline = component.find(MuiDateTimePicker);
       expect(outline.props().label).toBe("testing");
     });
 
     it("disabled", () => {
-      const outline = component.find("ForwardRef(DateTimePicker)");
+      const outline = component.find(MuiDateTimePicker);
       expect(outline.props().disabled).toBe(false);
     });
   });
