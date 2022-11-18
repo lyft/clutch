b47212a16467a10f510515d936a43d5f28128785
Josh Slaughter <jslaughter@lyft.com>
2022-11-18 09:38:14 -0800

    Fixing unit tests

--- frontend/packages/core/src/tests/timeago.test.tsx
+++ frontend/packages/core/src/tests/timeago.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
+import "@testing-library/jest-dom";
+
 import TimeAgo from "../timeago";
 
 const getTimestamp = (days, date = new Date()) => {
