e4e7781585506823c6fab4ad6487534a527914ed
Jorge Cortés <jorgeenriquecortes@lyft.com>
2023-01-04 16:56:04 -0600

    frontend: hoist accordion stories

--- frontend/packages/core/src/stories/panel.stories.tsx
+++ frontend/packages/core/src/stories/panel.stories.tsx
@@ -2,27 +2,21 @@ import React from "react";
 import type { Meta } from "@storybook/react";
 
 import type { AccordionProps } from "../panel";
-import Accordion from "../panel";
+import AccordionComponent from "../panel";
 
 export default {
   title: "Core/Accordion",
-  component: Accordion,
+  component: AccordionComponent,
 } as Meta;
 
 const Template = (props: AccordionProps) => (
-  <Accordion {...props}>
+  <AccordionComponent {...props}>
     <img alt="clutch logo" src="https://clutch.sh/img/navigation/logo.svg" height="100px" />
-  </Accordion>
+  </AccordionComponent>
 );
 
 export const Primary = Template.bind({});
 Primary.args = {
   heading: "Check this out!",
   summary: "This is an expansion panel.",
-};
-
-export const Expanded = Template.bind({});
-Expanded.args = {
-  ...Primary.args,
-  expanded: true,
 };
