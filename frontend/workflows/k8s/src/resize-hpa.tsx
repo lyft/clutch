e8951cdbd7ba9f939b7bc1596ae90bf4164c865a
Shawna Monero <smonero@lyft.com>
2023-01-12 11:57:01 -0800

    change text based off suggestions

--- frontend/workflows/k8s/src/resize-hpa.tsx
+++ frontend/workflows/k8s/src/resize-hpa.tsx
@@ -44,11 +44,11 @@ const HPADetails: React.FC<WizardChild> = () => {
     hpaData.updateData(key, value);
   };
 
-  const notes = ({
+  const notes = [{
     text:
       "The HPA should take just a few minutes to scale in either direction. The HPA min/max size may fluctuate as it's scaling for a few minutes; this is normal and due to variable factors, like the Lyft Rebalancer.",
     severity: "info",
-  });
+  }];
 
   const currentHpaData = useDataLayout("currentHpaData");
 
