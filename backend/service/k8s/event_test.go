8ab1be1ceda1c0e235b7cb4467e92b5be5e3fabb
Shawna Monero <66325812+smonero@users.noreply.github.com>
2023-06-26 14:50:47 -0700

    fix field selector generation (#2701)

--- backend/service/k8s/event_test.go
+++ backend/service/k8s/event_test.go
@@ -82,43 +82,43 @@ func TestListEvents(t *testing.T) {
 	}
 }
 
-func TestConvertTypesToFieldSelector(t *testing.T) {
+func TestConvertTypesToFieldSelectors(t *testing.T) {
 	testCases := []struct {
 		name     string
 		types    []k8sapiv1.EventType
-		expected string
+		expected []string
 	}{
 		{
 			name:     "empty",
 			types:    []k8sapiv1.EventType{},
-			expected: "",
+			expected: []string{},
 		},
 		{
 			name:     "single",
 			types:    []k8sapiv1.EventType{k8sapiv1.EventType_WARNING},
-			expected: "type=Warning",
+			expected: []string{"type=Warning"},
 		},
 		{
 			name:     "multiple",
 			types:    []k8sapiv1.EventType{k8sapiv1.EventType_WARNING, k8sapiv1.EventType_NORMAL},
-			expected: "type=Normal,type=Warning",
+			expected: []string{"type=Normal", "type=Warning"},
 		},
 		{
 			name:     "duplicate",
 			types:    []k8sapiv1.EventType{k8sapiv1.EventType_WARNING, k8sapiv1.EventType_WARNING},
-			expected: "type=Warning",
+			expected: []string{"type=Warning"},
 		},
 		{
 			name:     "all",
 			types:    []k8sapiv1.EventType{k8sapiv1.EventType_WARNING, k8sapiv1.EventType_NORMAL, k8sapiv1.EventType_ERROR},
-			expected: "type=Normal,type=Warning,type=Error",
+			expected: []string{"type=Error", "type=Normal", "type=Warning"},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual := convertTypesToFieldSelector(tc.types)
-			assert.Equal(t, tc.expected, actual)
+			actual := convertTypesToFieldSelectors(tc.types)
+			assert.ElementsMatch(t, tc.expected, actual)
 		})
 	}
 }
