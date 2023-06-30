dec75f25450f3b0c91ad51302871a8cf4c982772
Josh Slaughter <jslaughter@lyft.com>
2023-06-30 10:31:30 -0700

    Fixing Cypress tests

--- frontend/packages/app/cypress/integration/navigation_spec.js
+++ frontend/packages/app/cypress/integration/navigation_spec.js
@@ -2,6 +2,20 @@ const DRAWER = "drawer";
 const WORKFLOW_GROUP = "workflowGroup";
 const WORKFLOW_GROUP_ITEM = "workflowGroupItem";
 
+const ifElementExists = (selector, attempt = 0) => {
+  if (attempt === 5) {
+    return null;
+  }
+
+  if (Cypress.$(selector).length === 0) {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(100, { log: false });
+    return ifElementExists(selector, attempt + 1);
+  }
+
+  return cy.get(selector, { log: false });
+};
+
 describe("Navigation drawer", () => {
   before(() => {
     cy.visit("localhost:3000");
@@ -17,8 +31,9 @@ describe("Navigation drawer", () => {
   it("displays and hides routes", () => {
     cy.element(WORKFLOW_GROUP).each((_, idx) => {
       cy.element(WORKFLOW_GROUP).eq(idx).click();
-      if (cy.element(WORKFLOW_GROUP_ITEM)) {
-        cy.element(WORKFLOW_GROUP_ITEM).each(link => {
+      const element = ifElementExists(WORKFLOW_GROUP_ITEM);
+      if (element) {
+        element.each(link => {
           cy.wrap(link).should("have.attr", "href");
         });
         cy.element(WORKFLOW_GROUP).eq(idx).click();
@@ -31,18 +46,21 @@ describe("Navigation drawer", () => {
     it("can route correctly", () => {
       return cy.element(WORKFLOW_GROUP).each((_, groupIdx) => {
         cy.element(WORKFLOW_GROUP).eq(groupIdx).click();
-        cy.element(WORKFLOW_GROUP_ITEM).each((__, linkIdx) => {
-          cy.element(WORKFLOW_GROUP_ITEM).eq(linkIdx).should("be.visible");
-          cy.element(WORKFLOW_GROUP_ITEM)
-            .eq(linkIdx)
-            .should("have.attr", "href")
-            .then(href => {
-              cy.element(WORKFLOW_GROUP_ITEM).eq(linkIdx).click();
-              cy.url().should("include", href);
-            });
-          cy.element(WORKFLOW_GROUP).eq(groupIdx).click();
-          // TODO: validate header of workflow here when it's landed
-        });
+        const element = ifElementExists(WORKFLOW_GROUP_ITEM);
+        if (element) {
+          cy.element(WORKFLOW_GROUP_ITEM).each((__, linkIdx) => {
+            cy.element(WORKFLOW_GROUP_ITEM).eq(linkIdx).should("be.visible");
+            cy.element(WORKFLOW_GROUP_ITEM)
+              .eq(linkIdx)
+              .should("have.attr", "href")
+              .then(href => {
+                cy.element(WORKFLOW_GROUP_ITEM).eq(linkIdx).click();
+                cy.url().should("include", href);
+              });
+            cy.element(WORKFLOW_GROUP).eq(groupIdx).click();
+            // TODO: validate header of workflow here when it's landed
+          });
+        }
       });
     });
   });
