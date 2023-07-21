bda8e2df3c75adbe28682c013644343c9c628ee8
Josh Slaughter <jslaughter@lyft.com>
2023-07-21 13:09:07 -0700

    Fixing unit tests

--- frontend/packages/core/src/Input/tests/time-picker.test.tsx
+++ frontend/packages/core/src/Input/tests/time-picker.test.tsx
@@ -41,16 +41,11 @@ test("onChange is not called when invalid value", () => {
 
 test("sets passed value correctly", () => {
   const date = new Date();
-  const formattedDMY = new Intl.DateTimeFormat("en-US", {
-    month: "2-digit",
-    day: "2-digit",
-    year: "numeric",
-  }).format(date);
   const formattedTime = new Intl.DateTimeFormat("en-US", {
     hour: "2-digit",
     minute: "2-digit",
   }).format(date);
-  const formattedDate = `${formattedDMY} ${formattedTime}`;
+  const formattedDate = `${formattedTime}`;
   render(<TimePicker value={date} onChange={onChange} />);
 
   expect(screen.getByPlaceholderText("hh:mm (a|p)m")).toHaveValue(formattedDate);
