fd45aa283a3d629ca6bd05db75dc18080df9cdfa
Roier <roierrodriguez@lyft.com>
2023-07-26 13:55:58 -0600

    frontend: Adding bugsnag

--- frontend/packages/app/src/index.jsx
+++ frontend/packages/app/src/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
 import { ClutchApp } from "@clutch-sh/core";
 
 import registeredWorkflows from "./workflows";
@@ -8,7 +10,16 @@ import "./index.css";
 
 const config = require("./clutch.config");
 
-ReactDOM.render(
-  <ClutchApp availableWorkflows={registeredWorkflows} configuration={config} />,
-  document.getElementById("root")
-);
+let root = <ClutchApp availableWorkflows={registeredWorkflows} configuration={config} />;
+
+if (process.env.REACT_APP_CREDENTIALS_BUGSNAG_API_TOKEN) {
+  Bugsnag.start({
+    apiKey: process.env.REACT_APP_CREDENTIALS_BUGSNAG_API_TOKEN,
+    plugins: [new BugsnagPluginReact()],
+    releaseStage: process.env.APPLICATION_ENV,
+  });
+  const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
+  root = <ErrorBoundary>{root}</ErrorBoundary>;
+}
+
+ReactDOM.render(root, document.getElementById("root"));
