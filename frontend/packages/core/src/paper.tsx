19f1729de8c2353bb2fc398b0b9743ab68f27d90
Josh Slaughter <jslaughter@lyft.com>
2024-02-21 15:36:55 -0800

    frontend: Adjusting Wizard to allow for styling

--- frontend/packages/core/src/paper.tsx
+++ frontend/packages/core/src/paper.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 import type { PaperProps as MuiPaperProps, Theme } from "@mui/material";
 import { alpha, Paper as MuiPaper } from "@mui/material";
 
-export interface PaperProps extends Pick<MuiPaperProps, "children"> {}
+export interface PaperProps extends Pick<MuiPaperProps, "children" | "className"> {}
 
 const StyledPaper = styled(MuiPaper)(({ theme }: { theme: Theme }) => ({
   boxShadow: `0px 4px 6px ${alpha(theme.palette.primary[600], 0.2)}`,
@@ -14,8 +14,8 @@ const StyledPaper = styled(MuiPaper)(({ theme }: { theme: Theme }) => ({
   minHeight: "inherit",
 }));
 
-const Paper = ({ children, ...props }: PaperProps) => (
-  <StyledPaper {...props}>{children}</StyledPaper>
+const Paper = ({ children, className }: PaperProps) => (
+  <StyledPaper className={className}>{children}</StyledPaper>
 );
 
 export default Paper;
