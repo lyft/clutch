2aaf5bf1eab3ea8c286c8e7f1e50f15b22f879b8
Josh Slaughter <jslaughter@lyft.com>
2024-04-11 12:53:13 -0700

    Exporting some interfaces

--- frontend/packages/core/src/index.tsx
+++ frontend/packages/core/src/index.tsx
@@ -63,6 +63,7 @@ export { useTheme } from "./AppProvider/themes";
 
 export { css as EMOTION_CSS, keyframes as EMOTION_KEYFRAMES } from "@emotion/react";
 
+export type { QLink as QuickLink, LinkGroup as QuickLinkGroup } from "./quick-links";
 export type { BaseWorkflowProps, WorkflowConfiguration } from "./AppProvider/workflow";
 export type { ButtonProps } from "./button";
 export type { CardHeaderSummaryProps } from "./card";
