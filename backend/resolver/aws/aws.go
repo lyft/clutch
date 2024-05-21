ac47609d763259eed5d67d3350cde85d16f5636b
Tolu Jimoh <tolujimoh@lyft.com>
2024-05-21 17:10:24 -0400

    aws: implement topology cache for aws s3 and iam resources

--- backend/resolver/aws/aws.go
+++ backend/resolver/aws/aws.go
@@ -17,7 +17,9 @@ import (
 
 	dynamodbv1api "github.com/lyft/clutch/backend/api/aws/dynamodb/v1"
 	ec2v1api "github.com/lyft/clutch/backend/api/aws/ec2/v1"
+	iamv1api "github.com/lyft/clutch/backend/api/aws/iam/v1"
 	kinesisv1api "github.com/lyft/clutch/backend/api/aws/kinesis/v1"
+	s3v1api "github.com/lyft/clutch/backend/api/aws/s3/v1"
 	awsv1resolver "github.com/lyft/clutch/backend/api/resolver/aws/v1"
 	resolverv1 "github.com/lyft/clutch/backend/api/resolver/v1"
 	"github.com/lyft/clutch/backend/gateway/meta"
@@ -34,6 +36,9 @@ var typeURLInstance = meta.TypeURL((*ec2v1api.Instance)(nil))
 var typeURLAutoscalingGroup = meta.TypeURL((*ec2v1api.AutoscalingGroup)(nil))
 var typeURLKinesisStream = meta.TypeURL((*kinesisv1api.Stream)(nil))
 var typeURLDynamodbTable = meta.TypeURL((*dynamodbv1api.Table)(nil))
+var typeURLS3Bucket = meta.TypeURL((*s3v1api.Bucket)(nil))
+var typeURLS3AccessPoint = meta.TypeURL((*s3v1api.AccessPoint)(nil))
+var typeURLIAMRole = meta.TypeURL((*iamv1api.Role)(nil))
 
 var typeSchemas = resolver.TypeURLToSchemaMessagesMap{
 	typeURLInstance: {
@@ -47,6 +52,15 @@ var typeSchemas = resolver.TypeURLToSchemaMessagesMap{
 	},
 	typeURLDynamodbTable: {
 		(*awsv1resolver.DynamodbTableName)(nil),
+	},
+	typeURLS3Bucket: {
+		(*awsv1resolver.S3BucketName)(nil),
+	},
+	typeURLS3AccessPoint: {
+		(*awsv1resolver.S3AccessPointName)(nil),
+	},
+	typeURLIAMRole: {
+		(*awsv1resolver.IAMRoleName)(nil),
 	},
 }
 
@@ -156,6 +170,15 @@ func (r *res) Resolve(ctx context.Context, wantTypeURL string, input proto.Messa
 	case typeURLDynamodbTable:
 		return r.resolveDynamodbTableForInput(ctx, input)
 
+	case typeURLS3Bucket:
+		return r.resolveS3BucketForInput(ctx, input)
+
+	case typeURLS3AccessPoint:
+		return r.resolveS3AccessPointForInput(ctx, input)
+
+	case typeURLIAMRole:
+		return r.resolveIAMRoleForInput(ctx, input)
+
 	default:
 		return nil, status.Errorf(codes.Internal, "resolver for '%s' not implemented", wantTypeURL)
 	}
@@ -209,6 +232,42 @@ func (r *res) Search(ctx context.Context, typeURL, query string, limit uint32) (
 		}
 
 		return r.dynamodbResults(ctx, resolver.OptionAll, resolver.OptionAll, query, limit)
+
+	case typeURLS3Bucket:
+		patternValues, ok, err := meta.ExtractPatternValuesFromString((*s3v1api.Bucket)(nil), query)
+		if err != nil {
+			return nil, err
+		}
+
+		if ok {
+			return r.s3BucketResults(ctx, patternValues["account"], patternValues["region"], patternValues["name"], limit)
+		}
+
+		return r.s3BucketResults(ctx, resolver.OptionAll, resolver.OptionAll, query, limit)
+
+	case typeURLS3AccessPoint:
+		patternValues, ok, err := meta.ExtractPatternValuesFromString((*s3v1api.AccessPoint)(nil), query)
+		if err != nil {
+			return nil, err
+		}
+
+		if ok {
+			return r.s3AccessPointResults(ctx, patternValues["account"], patternValues["region"], patternValues["name"], limit)
+		}
+
+		return r.s3AccessPointResults(ctx, resolver.OptionAll, resolver.OptionAll, query, limit)
+
+	case typeURLIAMRole:
+		patternValues, ok, err := meta.ExtractPatternValuesFromString((*iamv1api.Role)(nil), query)
+		if err != nil {
+			return nil, err
+		}
+
+		if ok {
+			return r.iamRoleResults(ctx, patternValues["account"], patternValues["region"], patternValues["name"], limit)
+		}
+
+		return r.iamRoleResults(ctx, resolver.OptionAll, resolver.OptionAll, query, limit)
 
 	default:
 		return nil, status.Errorf(codes.Internal, "resolver search for '%s' not implemented", typeURL)
