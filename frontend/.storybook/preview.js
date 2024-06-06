642ed2c04d6e66556d749ee6b38d676b53a582b7
Jorge Cortés <jorgeenriquecortes@lyft.com>
2024-06-06 16:11:14 -0600

    use selected theme for background

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,25 @@
+import { DecoratorHelpers } from "@storybook/addon-themes";
 import { withClutchTheme } from "./withClutchTheme.decorator";
+import { Global, css } from "@emotion/react";
+import { clutchColors } from "../packages/core/src/Theme/colors";
+
+const { pluckThemeFromContext } = DecoratorHelpers;
+
+const withGlobalStyles = (Story, context) => {
+  const selectedTheme = pluckThemeFromContext(context);
+  const GlobalStyles = css`
+    body {
+      background: ${clutchColors(selectedTheme).neutral["50"]};
+    }
+  `;
+
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <Story {...context} />
+    </>
+  );
+};
 
 export const decorators = [
   withClutchTheme({
@@ -8,19 +29,20 @@ export const decorators = [
     },
     defaultTheme: "light",
   }),
+  withGlobalStyles,
 ];
 
 export const parameters = {
   backgrounds: {
-    default: "clutch",
+    default: "light",
     values: [
       {
-        name: "clutch",
-        value: "#f9fafe",
+        name: "light",
+        value: clutchColors("light").neutral["50"],
       },
       {
-        name: "light",
-        value: "#ffffff",
+        name: "dark",
+        value: clutchColors("dark").neutral["50"],
       },
     ],
   },
