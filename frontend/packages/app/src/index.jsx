6a586a3999b86cb1a5781faa3fb5985c4230939e
Lucía Echenique Álvarez <luciaechenique@lyft.com>
2024-08-26 15:29:40 -0600

    small update

--- frontend/packages/app/src/index.jsx
+++ frontend/packages/app/src/index.jsx
@@ -8,44 +8,7 @@ import "./index.css";
 
 const config = require("./clutch.config");
 
-const banners = {
-  header: {
-    message: "this is message header",
-    linkText: "infra docs",
-    link: "https://infradocs.lyft.net/index.html",
-    severity: "error",
-  },
-  perWorkflow: {
-    Envoy: {
-      title: "this is a per workout banner fro envoy",
-      message: "this is message banner fro envoy",
-      linkText: "infra docs",
-      link: "https://infradocs.lyft.net/index.html",
-      severity: "success",
-    },
-    K8s: {
-      title: "this is a per workout banner for k8s",
-      message: "this is message banner for k8s",
-      linkText: "infra docs",
-      link: "https://infradocs.lyft.net/index.html",
-      severity: "warning",
-    },
-  },
-  multiWorkflow: {
-    title: "multi title",
-    message: "message for multi workflow",
-    workflows: ["EC2", "Envoy"],
-    severity: "info",
-    linkText: "infra docs",
-    link: "https://infradocs.lyft.net/index.html",
-  },
-};
-
 ReactDOM.render(
-  <ClutchApp
-    availableWorkflows={registeredWorkflows}
-    configuration={config}
-    appConfiguration={{ banners }}
-  />,
+  <ClutchApp availableWorkflows={registeredWorkflows} configuration={config} />,
   document.getElementById("root")
 );
