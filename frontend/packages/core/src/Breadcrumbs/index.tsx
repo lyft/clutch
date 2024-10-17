3860de3037b462d2475cd200c7dc27b91ddfd49c
Orlando Valverde <orlandojesigvalverde@lyft.com>
2024-10-17 14:18:36 -0600

    Fix items stated in code review

--- frontend/packages/core/src/Breadcrumbs/index.tsx
+++ frontend/packages/core/src/Breadcrumbs/index.tsx
@@ -5,28 +5,26 @@ import { alpha } from "@mui/system";
 import styled from "../styled";
 
 import Breadcrumb from "./breadcrumb";
-import { BreadcrumbEntry } from "./types";
+import type { BreadcrumbEntry } from "./types";
 
 interface BreadcrumbsProps {
   entries: BreadcrumbEntry[];
 }
 
 const StyledBreadcrumbs = styled(MuiBreadcrumbs)(({ theme }: { theme: Theme }) => ({
-  margin: "8px 0px",
+  margin: theme.spacing(theme.clutch.spacing.sm, theme.clutch.spacing.none),
   "& .MuiBreadcrumbs-separator": {
     color: alpha(theme.colors.neutral[900], 0.6),
   },
 }));
 
-const Breadcrumbs = ({ entries }: BreadcrumbsProps) => {
-  return (
-    <StyledBreadcrumbs>
-      {entries.map((entry: BreadcrumbEntry) => (
-        <Breadcrumb key={entry.label} {...entry} />
-      ))}
-    </StyledBreadcrumbs>
-  );
-};
+const Breadcrumbs = ({ entries }: BreadcrumbsProps) => (
+  <StyledBreadcrumbs>
+    {entries.map((entry: BreadcrumbEntry) => (
+      <Breadcrumb key={entry.label} {...entry} />
+    ))}
+  </StyledBreadcrumbs>
+);
 
 export * from "./types";
 
