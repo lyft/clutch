56d96b2086761eb92fa3fa151f64364dc8727a2d
Orlando Valverde <4467518+septum@users.noreply.github.com>
2024-10-18 12:12:40 -0600

    frontend: Add breadcrumbs component (#3145)

---
+++ frontend/packages/core/src/Breadcrumbs/breadcrumb.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { Link } from "../link";
+import styled from "../styled";
+import { Typography } from "../typography";
+
+import type { BreadcrumbEntry } from "./types";
+
+const StyledTypography = styled(Typography)({
+  fontWeight: 500,
+});
+
+const Breadcrumb = ({ label, url }: BreadcrumbEntry) => {
+  return url ? (
+    <Link href={url} target="_self" whiteSpace="nowrap">
+      <StyledTypography variant="caption2" color="inherit">
+        {label}
+      </StyledTypography>
+    </Link>
+  ) : (
+    <StyledTypography variant="caption2">{label}</StyledTypography>
+  );
+};
+
+export default Breadcrumb;
